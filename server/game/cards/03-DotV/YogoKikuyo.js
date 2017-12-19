b9990eae33f671496490f96da9fa6126d0a52280
artem <artemmikhalitsin@gmail.com>
2017-12-19 15:07:09 -0500

    Now works

--- server/game/cards/03-DotV/YogoKikuyo.js
+++ server/game/cards/03-DotV/YogoKikuyo.js
@@ -5,12 +5,10 @@ class YogoKikuyo extends DrawCard {
         this.interrupt({
             title: 'Cancel a spell',
             when: {
-                onCardAbilityInitiated: (event) => {
-                  let result = this.game.currentConflict && event.card.type === 'event' && event.card.hasTrait('spell')
-                  console.log(result);
-                  return result;
-                }
+                onCardAbilityInitiated: event => this.game.currentConflict && event.card.type === 'event' &&
+                                          event.card.hasTrait('spell') && this.controller.canPutIntoPlay(this)
             },
+            location: 'hand',
             canCancel: true,
             handler: context => {
                 this.game.addMessage('{0} uses {1} to put it into play and cancel the effects of {2}', this.controller, this, context.event.card);
