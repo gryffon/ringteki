17a0ae8a1240c05acb7cff6d4fc689730dfb8205
artem <artemmikhalitsin@gmail.com>
2017-12-19 21:19:16 -0500

    now uses putIntoPlay cost

--- server/game/cards/03-DotV/YogoKikuyo.js
+++ server/game/cards/03-DotV/YogoKikuyo.js
@@ -1,20 +1,19 @@
 const DrawCard = require('../../drawcard.js');
 
 class YogoKikuyo extends DrawCard {
-    setupCardAbilities() {
+    setupCardAbilities(ability) {
         this.interrupt({
             title: 'Cancel a spell',
             when: {
                 onCardAbilityInitiated: event => this.game.currentConflict && event.card.type === 'event' &&
-                                          event.card.hasTrait('spell') && event.card.controller === this.controller.opponent &&
-                                          this.controller.canPutIntoPlay(this)
+                                          event.card.hasTrait('spell') && event.card.controller === this.controller.opponent
 
             },
+            cost: ability.costs.putSelfIntoPlay(),
             location: 'hand',
             canCancel: true,
             handler: context => {
                 this.game.addMessage('{0} uses {1} to put it into play and cancel the effects of {2}', this.controller, this, context.event.card);
-                this.controller.putIntoPlay(this);
                 context.cancel();
             }
         });
