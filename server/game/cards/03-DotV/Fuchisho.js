ee7134eea81498ea3dfac108e422385f0fea62f3
artem <artemmikhalitsin@gmail.com>
2017-12-19 21:49:47 -0500

    Renamed pack folder

--- server/game/cards/03-DotV/Fuchisho.js
+++ server/game/cards/03-DotV/Fuchisho.js
@@ -16,7 +16,7 @@ class Fushicho extends DrawCard {
             handler: context => {
                 this.game.addMessage('{0} uses {1} to resurrect {2}', this.controller, this, context.target);
                 this.controller.putIntoPlay(context.target);
-                context.target.modifyFate(2); // 1 fate gets removed as part of the phase - is there a better way to do this?
+                context.target.fate = 1; // 1 fate gets removed as part of the phase - is there a better way to do this?
             }
         }
         );
