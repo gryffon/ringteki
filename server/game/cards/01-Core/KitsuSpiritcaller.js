e5fa1957df6e2fc90237b19e55d4504b651d9ece
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-14 07:01:31 +0000

    Kitsu Spiritcaller bugfix

--- server/game/cards/01-Core/KitsuSpiritcaller.js
+++ server/game/cards/01-Core/KitsuSpiritcaller.js
@@ -14,8 +14,8 @@ class KitsuSpiritcaller extends DrawCard {
             },
             handler: context => {
                 this.game.addMessage('{0} bows {1} to call {2} back from the dead until the end of the conflict', this.controller, this, context.target);
-                this.game.applyGameAction(context, { putIntoConflict: context.target });
-                this.delayedEffect({
+                let event = this.game.applyGameAction(context, { putIntoConflict: context.target })[0];
+                event.addThenEvent(this.game.getEvent('unnamedEvent', {}, () => this.delayedEffect({
                     match: context.target,
                     trigger: 'onConflictFinished',
                     context: context,
@@ -25,7 +25,7 @@ class KitsuSpiritcaller extends DrawCard {
                         events[0].options.bottom = true;
                         return events;
                     }
-                });
+                })));
             }
         });
     }
