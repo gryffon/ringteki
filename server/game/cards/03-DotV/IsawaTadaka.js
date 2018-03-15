feba990cc7021cd7db057f0d12b16ea0be2065bb
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-15 18:51:22 +0000

    Isawa Tadaka

---
+++ server/game/cards/03-DotV/IsawaTadaka.js
@@ -0,0 +1,16 @@
+const DrawCard = require('../../drawcard.js');
+
+class IsawaTadaka extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            targetType: 'player',
+            targetController: 'any',
+            match: player => this.game.rings.earth.isConsideredClaimed(player),
+            effect: ability.effects.cannotPlay(context => context && context.source.type === 'event' && context.player.conflictDiscardPile.any(card => card.name === context.source.name))
+        });
+    }
+}
+
+IsawaTadaka.id = 'isawa-tadaka'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = IsawaTadaka;
