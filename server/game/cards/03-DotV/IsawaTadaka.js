51ac635a228d73573c04c72ed6b23606b4754c32
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-15 22:02:00 +0000

    bugfix

--- server/game/cards/03-DotV/IsawaTadaka.js
+++ server/game/cards/03-DotV/IsawaTadaka.js
@@ -5,7 +5,7 @@ class IsawaTadaka extends DrawCard {
         this.persistentEffect({
             targetType: 'player',
             targetController: 'any',
-            match: player => this.game.rings.earth.isConsideredClaimed(player),
+            match: player => !this.game.rings.earth.isConsideredClaimed(player),
             effect: ability.effects.cannotPlay(context => context && context.source.type === 'event' && context.player.conflictDiscardPile.any(card => card.name === context.source.name))
         });
     }
