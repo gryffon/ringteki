25901a81b7b81258e3baeeca705d23ee22374b0d
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-19 23:28:11 +0000

    Fix typo in GuardianKami.js

--- server/game/cards/03-DotV/GuardianKami.js
+++ server/game/cards/03-DotV/GuardianKami.js
@@ -4,7 +4,7 @@ class GuardianKami extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Resolve ring effect',
-            cost: ability.cost.sacrificeSelf(),
+            cost: ability.costs.sacrificeSelf(),
             condition: context => context.source.isDefending(),
             handler: context => {
                 this.game.addMessage('{0} sacrifices {1} to resolve the ring effect', context.player, context.source);
