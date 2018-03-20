90b375bcbf5217fa6260a6cdf47040d6faeb3e91
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-20 02:23:39 +0000

    Fire Elemental Guard

--- server/game/cards/03-DotV/FireElementalGuard.js
+++ server/game/cards/03-DotV/FireElementalGuard.js
@@ -1,7 +1,34 @@
 const DrawCard = require('../../drawcard.js');
+const EventRegistrar = require('../../eventregistrar.js');
 
 class FireElementalGuard extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities() {
+        this.spellsPlayedThisConflict = 0;
+        this.eventRegistrar = new EventRegistrar(this.game, this);
+        this.eventRegistrar.register(['onConflictFinished', 'onCardPlayed']);
+        this.action({
+            title: 'Discard an attachment',
+            condition: () => this.spellsPlayedThisConflict > 2,
+            methods: ['onConflictFinished', 'onCardPlayed'],
+            target: {
+                cardType: 'attachment',
+                gameAction: 'discardFromPlay',
+            },
+            handler: context => {
+                this.game.addMessage('{0} uses {1} to discard {2}', context.player, context.source, context.target);
+                this.game.applyGameAction(context, { discardFromPlay: context.target });
+            }
+        });
+    }
+
+    onConflictFinished() {
+        this.spellsPlayedThisConflict = 0;
+    }
+
+    onCardPlayed(event) {
+        if(this.game.currentConflict && event.card.hasTrait('spell')) {
+            this.spellsPlayedThisConflict += 1;
+        }
     }
 }
 
