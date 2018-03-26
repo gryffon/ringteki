bc9d71186f059794e38119b9a7bc6b8f0a2eff20
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-03-25 22:39:22 -0400

    Clarity of Purpose added. Cannot be bowed effect still being worked on.

--- server/game/cards/03-DotV/ClarityOfPurpose.js
+++ server/game/cards/03-DotV/ClarityOfPurpose.js
@@ -2,6 +2,41 @@ const DrawCard = require('../../drawcard.js');
 
 class ClarityOfPurpose extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.action({
+            title: 'Opponent\'s cards cannot bow the target character and it does not bow as a result of political conflicts',
+            condition: () => this.game.currentConflict && this.controller.opponent,
+            target: {
+                activePromptTitle: 'Choose a character',
+                cardType: 'character',
+                cardCondition: card => card.controller === this.controller
+            },
+            handler: context => {
+                let msg = '{0} uses {1} on {2} to prevent opponents\' actions from bowing it';
+                if (this.game.currentConflict.conflictType === 'political') {
+                    msg += ' and to prevent it bowing as a result of the political conflict\'s resolution';
+                }
+
+                this.game.addMessage(msg, this.controller, this, context.target);
+
+                this.untilEndOfConflict(ability => ({
+                    match: context.target,
+                    condition: () => this.controller !== context.target.controller,
+                    effect: [
+                        ability.effects.cannotBeBowed()
+                    ]
+                }));
+
+                if (this.game.currentConflict.conflictType === 'political') {
+                    this.untilEndOfConflict(ability => ({
+                        match: context.target,
+                        effect: [
+                            ability.effects.doesNotBowAsAttacker(),
+                            ability.effects.doesNotBowAsDefender()
+                        ]
+                    }));
+                }
+            }
+        });
     }
 }
 
