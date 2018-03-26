f0952c4861aa8d2474a8bb1a7d95c4e4bfe5f23a
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-03-26 07:38:58 -0400

    Now prevents bowing from opponents cards.

--- server/game/cards/03-DotV/ClarityOfPurpose.js
+++ server/game/cards/03-DotV/ClarityOfPurpose.js
@@ -1,32 +1,28 @@
 const DrawCard = require('../../drawcard.js');
 
 class ClarityOfPurpose extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities() {
         this.action({
             title: 'Opponent\'s cards cannot bow the target character and it does not bow as a result of political conflicts',
             condition: () => this.game.currentConflict && this.controller.opponent,
             target: {
                 activePromptTitle: 'Choose a character',
                 cardType: 'character',
-                cardCondition: card => card.controller === this.controller
+                cardCondition: card => card.controller === this.controller && card.location === 'play area'
             },
             handler: context => {
                 let msg = '{0} uses {1} on {2} to prevent opponents\' actions from bowing it';
-                if (this.game.currentConflict.conflictType === 'political') {
+                if(this.game.currentConflict.conflictType === 'political') {
                     msg += ' and to prevent it bowing as a result of the conflict\'s resolution';
                 }
-
                 this.game.addMessage(msg, this.controller, this, context.target);
-
                 this.untilEndOfConflict(ability => ({
                     match: context.target,
-                    condition: () => this.controller !== context.target.controller,
                     effect: [
-                        ability.effects.cannotBeBowed()
+                        ability.effects.cannotBeBowed(context => context && context.source.type !== 'ring' && context.source.controller === this.controller.opponent)
                     ]
                 }));
-
-                if (this.game.currentConflict.conflictType === 'political') {
+                if(this.game.currentConflict.conflictType === 'political') {
                     this.untilEndOfConflict(ability => ({
                         match: context.target,
                         effect: [
