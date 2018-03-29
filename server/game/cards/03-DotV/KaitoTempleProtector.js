2015b3f97468e73f6dde45fe7421993fb40eea01
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-03-29 02:41:20 +0100

    fix TempleProtector name

--- server/game/cards/03-DotV/KaitoTempleProtector.js
+++ server/game/cards/03-DotV/KaitoTempleProtector.js
@@ -1,6 +1,6 @@
 const DrawCard = require('../../drawcard.js');
 
-class Phoenix4Drop extends DrawCard {
+class KaitoTempleProtector extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
             condition: () => this.isDefending(),
@@ -12,14 +12,14 @@ class Phoenix4Drop extends DrawCard {
             condition: context => context.source.isDefending(),
             target: {
                 cardType: 'character',
-                cardCondition: card => card.isParticipating()
+                cardCondition: (card, context) => card.isParticipating() && card !== context.source
             },
             handler: context => {
                 let newMil = context.target.getMilitarySkill();
                 let newPol = context.target.getPoliticalSkill();
                 this.game.addMessage('{0} uses {1}, targeting {2} and changing {1}\'s base {3} skill to {4} and {5} skill to {6}', context.player, context.source, context.target, 'military', newMil, 'political', newPol);
-                this.untilEndOfConflict(ability => ({
-                    match: this,
+                context.source.untilEndOfConflict(ability => ({
+                    match: context.source,
                     effects: [
                         ability.effects.modifyBaseMilitarySkill(newMil - context.source.baseMilitarySkill),
                         ability.effects.modifyBasePoliticalSkill(newPol - context.source.basePoliticalSkill)
@@ -30,6 +30,6 @@ class Phoenix4Drop extends DrawCard {
     }
 }
 
-Phoenix4Drop.id = 'phoenix-4-drop'; // This is a guess at what the id might be - please check it!!!
+KaitoTempleProtector.id = 'kaito-temple-protector'; // This is a guess at what the id might be - please check it!!!
 
-module.exports = Phoenix4Drop;
+module.exports = KaitoTempleProtector;
