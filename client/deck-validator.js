2d675f6a1c555c8191415c3b4d8d81a1b9a89a45
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-04-02 11:37:32 +0100

    update deck validator

--- client/deck-validator.js
+++ client/deck-validator.js
@@ -24,6 +24,17 @@ function getStronghold(deck) {
     return stronghold;
 }
 
+function getRole(deck) {
+    let role;
+    _.each(deck, card => {
+        if(card.card.type === 'role') {
+            role = card;
+        }
+    });
+
+    return role;
+}
+
 function isCardInReleasedPack(packs, card) { // eslint-disable-line no-unused-vars
     let cardPack = '';
     let pack = false;
@@ -52,6 +63,248 @@ function isCardInReleasedPack(packs, card) { // eslint-disable-line no-unused-va
     return releaseDate <= now;
 }
 
+function rulesForKeeperRole(element) {
+    return {
+        influence: 3,
+        roleRestrictions: ['keeper', element]
+    };
+}
+
+function rulesForSeekerRole(element) {
+    return {
+        additionalProvince: {[element]: 2},
+        roleRestrictions: ['seeker', element]
+    };
+}
+
+function rulesForSupportRole(faction) {
+    return {
+        influence: 8,
+        roleRestrictions: ['support'],
+        rules: [
+            {
+                message: 'Support roles can only be used with their respective factio',
+                condition: deck => deck.alliance.value === faction
+            }
+        ]
+    };
+}
+
+/**
+ * Validation rule structure is as follows. All fields are optional.
+ *
+ * requiredDraw  - the minimum amount of cards required for the draw deck.
+ * requiredPlots - the exact number of cards required for the plot deck.
+ * maxDoubledPlots - the maximum amount of plot cards that can be contained twice in the plot deck.
+ * mayInclude    - function that takes a card and returns true if it is allowed in the overall deck.
+ * cannotInclude - function that takes a card and return true if it is not allowed in the overall deck.
+ * rules         - an array of objects containing a `condition` function that takes a deck and return true if the deck is valid for that rule, and a `message` used for errors when invalid.
+ */
+const roleRules = {
+    'keeper-of-air': rulesForKeeperRole('air'),
+    'keeper-of-earth': rulesForKeeperRole('earth'),
+    'keeper-of-fire': rulesForKeeperRole('fire'),
+    'keeper-of-void': rulesForKeeperRole('void'),
+    'keeper-of-water': rulesForKeeperRole('water'),
+    'seeker-of-air': rulesForSeekerRole('air'),
+    'seeker-of-earth': rulesForSeekerRole('earth'),
+    'seeker-of-fire': rulesForSeekerRole('fire'),
+    'seeker-of-void': rulesForSeekerRole('void'),
+    'seeker-of-water': rulesForSeekerRole('water'),
+    'support-of-the-phoenix': rulesForSupportRole('phoenix')
+};
+
+class DeckValidator {
+    constructor(packs, restrictedListRules) {
+        this.packs = packs;
+    }
+
+    validateDeck(deck) {
+        let errors = [];
+        let unreleasedCards = [];
+        let rules = this.getRules(deck);
+        let stronghold = getStronghold(deck);
+        let role = getRole(deck);
+        let provinceCount = getDeckCount(deck.provinceCards)
+        let dynastyCount = getDeckCount(deck.dynastyCards);
+        let conflictCount = getDeckCount(deck.conflictCards);
+
+        if(provinceCount < rules.requiredProvinces) {
+            errors.push('Too few province cards');
+        } else if(provinceCount > rules.requiredProvinces) {
+            errors.push('Too many province cards');
+        }
+
+        if(dynastyCount < rules.minimumDynasty) {
+            errors.push('Too few dynasty cards');
+        } else if(dynastyCount > rules.maximumDynasty) {
+            errors.push('Too many dynasty cards');
+        }
+
+        if(conflictCount < rules.minimumConflict) {
+            errors.push('Too few conflict cards');
+        } else if(conflictCount > rules.maximumConflict) { 
+            errors.push('Too many conflict cards');
+        }
+
+        _.each(rules.rules, rule => {
+            if(!rule.condition(deck)) {
+                errors.push(rule.message);
+            }
+        });
+
+        let allCards = deck.provinceCards.concat(deck.dynastyCards).concat(deck.conflictCards);
+        let cardCountByName = {};
+
+        _.each(allCards, cardQuantity => {
+            cardCountByName[cardQuantity.card.name] = cardCountByName[cardQuantity.card.name] || { name: cardQuantity.card.name, faction: cardQuantity.card.clan, influence: cardQuantity.card.influence_cost, limit: cardQuantity.card.deck_limit, count: 0 };
+            cardCountByName[cardQuantity.card.name].count += cardQuantity.count;
+
+            if(!rules.mayInclude(cardQuantity.card) || rules.cannotInclude(cardQuantity.card) || !rules.roleRestrictions.includes(cardQuantity.card.role_restriction)) {
+                errors.push(cardQuantity.card.name + ' is not allowed by clan, alliance or role');
+            }
+
+            if(!isCardInReleasedPack(this.packs, cardQuantity.card)) {
+                unreleasedCards.push(cardQuantity.card.name + ' is not yet released');
+            }
+        });
+
+        if(!stronghold) {
+            errors.push('No stronghold');
+        } else if (!isCardInReleasedPack(this.packs, stronghold)) {
+            unreleasedCards.push(stronghold.name + ' is not yet released');
+        }
+
+        if(role && !isCardInReleasedPack(this.packs, role)) {
+            unreleasedCards.push(role.name + ' is not yet released');
+        }
+
+        _.each(rules.maxProvince, element => {
+            let provinces = _.filter(deck.provinceCards, card => card.card.element === element);
+            if(provinces.length > rules.maxProvince[element]) {
+                errors.push('Too many provinces with ' + element + ' element');
+            }
+        });
+
+        _.each(cardCountByName, card => {
+            if(card.count > card.limit) {
+                errors.push(card.name + ' has limit ' + card.limit);
+            }
+        });
+
+        let totalInfluence = _.reduce(cardCountByName, (total, card) => {
+            if(card.influence && card.faction !== deck.faction.value) {
+                return total + card.influence * card.count;
+            }
+            return total;
+        }, 0);
+        if(totalInfluence > rules.influence) {
+            errors.push('Total influence (' + totalInfluence.toString() + ') is higher than max allowed influence(' + rules.influence.toString() + ')');
+        }
+
+        return {
+            basicRules: errors.length === 0,
+            noUnreleasedCards: unreleasedCards.length === 0,
+            provinceCount: provinceCount,
+            dynastyCount: dynastyCount,
+            conflictCount: conflictCount,
+            extendedStatus: errors.concat(unreleasedCards)
+        };
+    }
+
+    getRules(deck) {
+        const standardRules = {
+            minimumDynasty: 40,
+            maximumDynasty: 45,
+            minimumConflict: 40,
+            maximumConflict: 45,
+            requiredProvinces: 5,
+            maxProvince: {
+                air: 1,
+                earth: 1,
+                fire: 1,
+                void: 1,
+                water: 1
+            }
+        };
+        let factionRules = this.getFactionRules(deck.faction.value.toLowerCase());
+        let allianceRules = this.getAllianceRules(deck.alliance.value.toLowerCase());
+        let roleRules = this.getRoleRules(getRole(deck));
+        let strongholdRules = this.getStrongholdRules(getStronghold(deck));
+        return this.combineValidationRules([standardRules, factionRules, allianceRules, roleRules, strongholdRules]);
+    }
+
+    getFactionRules(faction) {
+        return {
+            mayInclude: card => card.clan === faction || card.faction === 'neutral',
+            rules: [
+                {
+                    message: 'Your stronghold must match your clan',
+                    condition: deck => {
+                        let stronghold = getStronghold(deck);
+                        return stronghold && stronghold.clan === faction;
+                    }
+                }
+            ]
+        };
+    }
+
+    getAllianceRules(clan) {
+        return {
+            mayInclude: card => card.side === 'conflict' && card.clan === clan
+        };
+    }
+
+    getStrongholdRules(stronghold) {
+        return {
+            influence: stronghold.influence_pool
+        };
+    }
+
+    getRoleRules(role) {
+        if(!role && !roleRules[role.id]) {
+            return {};
+        }
+        return roleRules[role.id];
+    }
+
+    combineValidationRules(validators) {
+        let mayIncludeFuncs = _.compact(_.pluck(validators, 'mayInclude'));
+        let cannotIncludeFuncs = _.compact(_.pluck(validators, 'cannotInclude'));
+        let combinedRules = _.reduce(validators, (rules, validator) => rules.concat(validator.rules || []), []);
+        let combinedRoles = _.reduce(validators, (roles, validator) => roles.concat(validator.roleRestrictions || []), []);
+        let totalInfluence = _.reduce(validators, (total, validator) => total + validator.influence || 0, 0);
+        let maxProvince = _.reduce(validators, (result, validator) => {
+            if(validator.maxProvince) {
+                _.each(_.keys(result), key => result[key] += validator.maxProvince[key] || 0);
+            }
+            return result;
+        }, { air: 0, earth: 0, fire: 0, void: 0, water: 0 });
+        let combined = {
+            mayInclude: card => _.any(mayIncludeFuncs, func => func(card)),
+            cannotInclude: card => _.any(cannotIncludeFuncs, func => func(card)),
+            rules: combinedRules,
+            roleRestrictions: combinedRoles,
+            influence: totalInfluence,
+            maxProvince: maxProvince
+        };
+        return _.extend({}, ...validators, combined);
+    }
+}
+
+module.exports = function validateDeck(deck, options) {
+    options = Object.assign({ includeExtendedStatus: true }, options);
+
+    let validator = new DeckValidator(options.packs);
+    let result = validator.validateDeck(deck);
+
+    if(!options.includeExtendedStatus) {
+        return _.omit(result, 'extendedStatus');
+    }
+
+    return result;
+};
+/*
 module.exports = function validateDeck(deck, packs) { // eslint-disable-line no-unused-vars
     var provinceCount = getDeckCount(deck.provinceCards);
     var conflictCount = getDeckCount(deck.conflictCards);
