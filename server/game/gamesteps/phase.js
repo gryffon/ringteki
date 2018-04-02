a54de340dec446ed03b501dab7740a9548e40616
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-04-02 03:39:58 +0100

    Change phase transitions so that they check game state

--- server/game/gamesteps/phase.js
+++ server/game/gamesteps/phase.js
@@ -15,23 +15,20 @@ class Phase extends BaseStepWithPipeline {
     }
 
     startPhase() {
-        this.game.currentPhase = this.name;
-        if(this.name === 'dynasty') {
-            this.game.roundNumber++;
-        }
-        this.game.raiseEvent('onPhaseStarted', { phase: this.name });
-        if(this.name !== 'setup') {
-            this.game.addAlert('endofround', 'turn: {0} - {1} phase', this.game.roundNumber, this.name);
-        }
+        this.game.raiseEvent('onPhaseStarted', { phase: this.name }, () => {
+            this.game.currentPhase = this.name;
+            if(this.name === 'dynasty') {
+                this.game.roundNumber++;
+            }
+            if(this.name !== 'setup') {
+                this.game.addAlert('endofround', 'turn: {0} - {1} phase', this.game.roundNumber, this.name);
+            }    
+        });
     }
 
     endPhase() {
         this.game.raiseEvent('onPhaseEnded', { phase: this.name });
         this.game.currentPhase = '';
-        _.each(this.game.getPlayers(), player => {
-            player.phase = '';
-        });
-        this.game.raiseEvent('onAtEndOfPhase');
     }
 }
 
