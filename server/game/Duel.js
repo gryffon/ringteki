c9e98d49258bb10078c5dca9516be1f42ebcb6e1
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-04-10 16:40:04 +0100

    Fix duels to cope with a character dying mid-duel

--- server/game/Duel.js
+++ server/game/Duel.js
@@ -14,16 +14,18 @@ class Duel {
     }
 
     getSkillTotal(card) {
-        if(this.type === 'military') {
-            return card.getMilitarySkill(false, this.bidFinished);
-        } else if(this.type === 'political') {
-            return card.getPoliticalSkill(false, this.bidFinished);
+        if(card.location === 'play area') {
+            if(this.type === 'military') {
+                return card.getMilitarySkill(false, this.bidFinished);
+            } else if(this.type === 'political') {
+                return card.getPoliticalSkill(false, this.bidFinished);
+            }
         }
-        return null;
+        return '-';
     }
 
     isInvolved(card) {
-        return card === this.challenger || card === this.target;
+        return (card === this.challenger || card === this.target) && card.location === 'play area';
     }
 
     getTotalsForDisplay() {
@@ -32,30 +34,43 @@ class Duel {
 
     modifyDuelingSkill() {
         this.bidFinished = true;
+        let cards = [this.challenger, this.target].filter(card => card.location === 'play area');
         if(this.type === 'military') {
-            _.each([this.challenger, this.target], card => {
+            _.each(cards, card => {
                 this.source.untilEndOfDuel(ability => ({
                     match: card,
                     effect: ability.effects.modifyMilitarySkill(parseInt(card.controller.honorBid))
                 }));
             });
         } else if(this.type === 'political') {
-            _.each([this.challenger, this.target], card => {
+            _.each(cards, card => {
                 this.source.untilEndOfDuel(ability => ({
                     match: card,
                     effect: ability.effects.modifyPoliticalSkill(parseInt(card.controller.honorBid))
                 }));
             });
         }
+        this.game.checkGameState();
     }
 
     determineResult() {
         let challengerTotal = this.getSkillTotal(this.challenger);
         let targetTotal = this.getSkillTotal(this.target);
-        if(challengerTotal > targetTotal) {
+        if(this.challenger.location !== 'play area') {
+            if(this.target.location === 'play area') {
+                // Challenger dead, target alive
+                this.winner = this.target;
+            }
+            // Both dead
+        } else if(this.target.location !== 'play area') {
+            // Challenger alive, target dead
+            this.winner = this.challenger;
+        } else if(challengerTotal > targetTotal) {
+            // Both alive, challenger wins
             this.winner = this.challenger;
             this.loser = this.target;
         } else if(challengerTotal < targetTotal) {
+            // Both alive, target wins
             this.winner = this.target;
             this.loser = this.challenger;
         }
