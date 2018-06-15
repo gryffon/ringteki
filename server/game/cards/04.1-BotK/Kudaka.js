662b656bb7f99db567d38c498e1cddd2eb8b102b
Caelan Mayberry <gmodman12@gmail.com>
2018-06-15 09:24:33 -0400

    Added Kudaka

--- server/game/cards/04.1-BotK/Kudaka.js
+++ server/game/cards/04.1-BotK/Kudaka.js
@@ -2,6 +2,15 @@ const DrawCard = require('../../drawcard.js');
 
 class Kudaka extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.reaction({
+            title: 'Gain 1 fate and draw 1 card',
+            limit: ability.limit.perRound(2),
+            effect: 'gain 1 fate and draw 1 card',
+            when: {
+                onClaimRing: (event, context) => event.conflict && event.conflict.ring.element === 'air' && event.player === context.player
+            },
+            gameAction: [ability.actions.gainFate(), ability.actions.draw()]
+        });
     }
 }
 
