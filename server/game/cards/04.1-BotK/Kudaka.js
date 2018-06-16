c90e6697c47c0895248b742cde87676a046c05d7
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-06-16 19:13:36 -0400

    Checks for via hasElement. Added basic test.

--- server/game/cards/04.1-BotK/Kudaka.js
+++ server/game/cards/04.1-BotK/Kudaka.js
@@ -7,7 +7,7 @@ class Kudaka extends DrawCard {
             limit: ability.limit.perRound(2),
             effect: 'gain 1 fate and draw 1 card',
             when: {
-                onClaimRing: (event, context) => event.conflict && event.conflict.ring.element === 'air' && event.player === context.player
+                onClaimRing: (event, context) => event.conflict && event.conflict.ring.hasElement('air') && event.player === context.player
             },
             gameAction: [ability.actions.gainFate(), ability.actions.draw()]
         });
