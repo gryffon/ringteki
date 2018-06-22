873c2cd4e377a5eb4d5aee0d76ec0b58fb8aa301
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-06-22 14:56:00 +0100

    Make mirror work!

--- server/game/Events/EventWindow.js
+++ server/game/Events/EventWindow.js
@@ -48,11 +48,11 @@ class EventWindow extends BaseStepWithPipeline {
         this.events = _.reject(this.events, e => e === event);
     }
 
-    addThenAbility(events, ability) {
+    addThenAbility(events, ability, context) {
         if(!Array.isArray(events)) {
             events = [events];
         }
-        this.thenAbilities.push({ events: events, ability: ability });
+        this.thenAbilities.push({ events: events, ability: ability, context: context });
     }
 
     setCurrentEventWindow() {
@@ -118,7 +118,7 @@ class EventWindow extends BaseStepWithPipeline {
     checkThenAbilities() {
         for(const thenAbility of this.thenAbilities) {
             if(thenAbility.events.every(event => !event.cancelled)) {
-                this.game.resolveAbility(thenAbility.ability.createContext());
+                this.game.resolveAbility(thenAbility.ability.createContext(thenAbility.context.player));
             }
         }
     }
--- server/game/cards/04.1-BotK/TheMirrorsGaze.js
+++ server/game/cards/04.1-BotK/TheMirrorsGaze.js
@@ -5,15 +5,33 @@ class TheMirrorsGaze extends DrawCard {
         this.reaction({
             title: 'Mirror an opponent\'s event',
             when: {
-                onAbilityResolved: (event, context) => event.card.type === 'event' && !event.context.ability.cannotBeMirrored &&
-                                                       event.context.player === context.player.opponent &&
-                                                       !context.player.isAbilityAtMax(event.context.ability.maxIdentifier)
+                onAbilityResolved: (event, context) => {
+                    let ability = event.context.ability;
+                    if(event.card.type !== 'event' || ability.cannotBeMirrored) {
+                        return false;
+                    } else if(event.context.player !== context.player.opponent || context.player.isAbilityAtMax(ability.maxIdentifier)) {
+                        return false;
+                    }
+                    let newContext = event.context.ability.createContext(context.player);
+                    if(ability.targets.length === 0) {
+                        return ability.gameAction.length === 0 || ability.gameAction.some(action => action.hasLegalTarget(newContext));
+                    }
+                    return ability.canResolveTargets(context);
+                }
             },
             gameAction: ability.actions.resolveAbility(context => ({
                 target: context.event.card,
                 ability: context.event.context.ability
             }))
         });
+    }
+
+    canAttach(card, context) {
+        if(card.hasTrait('shugenja') === false || card.controller !== context.player) {
+            return false;
+        }
+
+        return super.canAttach(card, context);
     }
 }
 
