1d0ec078b26ca56bf517805d4b5c3536f98cb5af
Caelan Mayberry <gmodman12@gmail.com>
2018-06-22 10:10:16 -0400

    Fixed condition

--- server/game/cards/04.1-BotK/WindsOfChange.js
+++ server/game/cards/04.1-BotK/WindsOfChange.js
@@ -3,7 +3,7 @@ const DrawCard = require('../../drawcard.js');
 class WindsOfChange extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
-            condition: this.game.rings.air.claimed,
+            condition: () => this.game.rings.air.claimed,
             title: 'Return the air ring to the unclaimed pool',
             effect: 'return the air ring to the unclaimed pool',
             gameAction: ability.actions.returnRing(context => ({
