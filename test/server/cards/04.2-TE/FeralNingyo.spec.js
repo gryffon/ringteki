46fcda994ea0cca64bb99a8865f9fcf722771277
Caelan Mayberry <gmodman12@gmail.com>
2018-06-22 11:10:41 -0400

    Feral Ningyo and tests added

--- test/server/cards/04.2-TE/FeralNingyo.spec.js
+++ test/server/cards/04.2-TE/FeralNingyo.spec.js
@@ -1,17 +1,24 @@
-describe('Feral Ningyo', function() {
-    integration(function() {
-        describe('Feral Ningyo\'s ability', function() {
-            beforeEach(function() {
+describe('Feral Ningyo', function () {
+    integration(function () {
+        describe('Feral Ningyo\'s ability', function () {
+            beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
+                        fate: 3,
                         inPlay: ['adept-of-the-waves'],
                         hand: ['feral-ningyo']
                     }
                 });
+                this.feral = this.player1.findCardByName('feral-ningyo');
             });
 
-            it('should not activate during non-water conflicts', function() {
+            it('should not activate outside of conflicts', function () {
+                this.player1.clickCard(this.feral);
+                expect(this.player1).not.toHavePromptButton('Put into play');
+            });
+
+            it('should not activate during non-water conflicts', function () {
                 this.noMoreActions();
                 this.initiateConflict({
                     type: 'military',
@@ -20,43 +27,36 @@ describe('Feral Ningyo', function() {
                     defenders: []
                 });
                 this.player2.clickPrompt('Pass');
-                this.player1.clickCard('feral-ningyo');
-                expect(this.player1).toHavePrompt('Initiate an action');
-                //this.iuchiWayfinder = this.player1.playCharacterFromHand('iuchi-wayfinder');
-                // expect(this.player1).toHavePrompt('Triggered Abilities');
-                // this.player1.clickCard(this.iuchiWayfinder);
-                // expect(this.player1).toHavePrompt('Iuchi Wayfinder');
-                // expect(this.player1).toBeAbleToSelect(this.shamefulDisplay1);
-                // expect(this.player1).not.toBeAbleToSelect(this.shamefulDisplay2);
+                this.player1.clickCard(this.feral);
+                expect(this.player1).not.toHavePromptButton('Put into play');
             });
 
-            it('should not activate during non-water conflicts', function() {
-                this.noMoreActions();
-                this.initiateConflict({
-                    type: 'military',
-                    ring: 'water',
-                    attackers: ['adept-of-the-waves'],
-                    defenders: []
+            describe('during a water conflict', function () {
+                beforeEach(function () {
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        type: 'military',
+                        ring: 'water',
+                        attackers: ['adept-of-the-waves'],
+                        defenders: []
+                    });
+                    this.player2.clickPrompt('Pass');
+                    this.player1.clickCard(this.feral);
                 });
-                this.player2.clickPrompt('Pass');
-                this.player1.clickCard('feral-ningyo');
-                expect(this.player1).toHavePrompt('Initiate an action');
-                //this.iuchiWayfinder = this.player1.playCharacterFromHand('iuchi-wayfinder');
-                // expect(this.player1).toHavePrompt('Triggered Abilities');
-                // this.player1.clickCard(this.iuchiWayfinder);
-                // expect(this.player1).toHavePrompt('Iuchi Wayfinder');
-                // expect(this.player1).toBeAbleToSelect(this.shamefulDisplay1);
-                // expect(this.player1).not.toBeAbleToSelect(this.shamefulDisplay2);
+
+                it('should put it into play', function () {
+                    this.player1.clickPrompt('Put into play');
+                    expect(this.feral.location).toBe('play area');
+                });
+
+                it('should return to it the conflict deck at the end of the conflict', function () {
+                    this.player1.clickPrompt('Put into play');
+                    this.noMoreActions();
+                    this.player1.clickPrompt('No');
+                    this.player1.clickPrompt('Don\'t resolve');
+                    expect(this.feral.location).toBe('conflict deck');
+                });
             });
-
-
-            // it('should display a message in chat when a province is chosen', function() {
-            //     this.chat = spyOn(this.game, 'addMessage');
-            //     this.iuchiWayfinder = this.player1.playCharacterFromHand('iuchi-wayfinder');
-            //     this.player1.clickCard(this.iuchiWayfinder);
-            //     this.player1.clickCard(this.shamefulDisplay1);
-            //     expect(this.chat).toHaveBeenCalledWith('{0} reveals {1}', this.iuchiWayfinder, [this.shamefulDisplay1]);
-            // });
         });
     });
 });
