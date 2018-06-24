a99fdc78379941cbbab2254f44fe7c61d15931f7
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-06-23 20:37:46 -0400

    Added The Wrath of the Kami with test

---
+++ test/server/cards/04.3-TFW/WrathOfTheKami.spec.js
@@ -0,0 +1,54 @@
+describe('The Wrath of the Kami', function () {
+    integration(function () {
+        describe('The Wrath of the Kami\'s ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['adept-of-the-waves'],
+                        dynastyDeck: ['the-wrath-of-the-kami']
+                    },
+                    player2: {
+                        honor: 10,
+                        provinces: ['pilgrimage', 'kuroi-mori'],
+                        dynastyDeck: ['the-wrath-of-the-kami']
+                    }
+                });
+                this.wrath1 = this.player1.placeCardInProvince('the-wrath-of-the-kami', 'province 1');
+                this.wrath2 = this.player2.placeCardInProvince('the-wrath-of-the-kami', 'province 1');
+            });
+
+            it('should not activate during a conflict at another province', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    province: 'kuroi-mori',
+                    attackers: ['adept-of-the-waves'],
+                    defenders: []
+                });
+                this.player2.clickCard(this.wrath2);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
+
+            describe('during a conflict at the holding\'s province', function () {
+                beforeEach(function () {
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        province: 'pilgrimage',
+                        attackers: ['adept-of-the-waves'],
+                        defenders: []
+                    });
+                    this.player2.clickCard(this.wrath2);
+                })
+
+                it('should increase the province\'s strength by 1', function () {
+                    // Pilgrimage 5 PS, holding +1 PS, and +1 PS from ability.
+                    expect(this.game.currentConflict.conflictProvince.getStrength()).toBe(7);
+                });
+
+                it('should cost 1 honor', function () {
+                    expect(this.player2.honor).toBe(9);
+                });
+            })
+        });
+    });
+});
