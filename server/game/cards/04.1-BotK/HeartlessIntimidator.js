cecfa3d77afa670a61a75aff259c4e3f3620af65
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-06-25 17:53:39 -0400

    Added with basic test

--- server/game/cards/04.1-BotK/HeartlessIntimidator.js
+++ server/game/cards/04.1-BotK/HeartlessIntimidator.js
@@ -1,7 +1,17 @@
 const DrawCard = require('../../drawcard.js');
 
 class HeartlessIntimidator extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Force opponent to discard 1 card',
+            limit: ability.limit.perPhase(Infinity),
+            when: {
+                onModifyHonor: (event, context) => event.player === context.player.opponent && event.amount < 0
+            },
+            gameAction: ability.actions.discardCard(context => ({
+                target: context.player.opponent.conflictDeck.first()
+            }))
+        });
     }
 }
 
---
+++ test/server/cards/04.1-BotK/HeartlessIntimidator.spec.js
@@ -0,0 +1,50 @@
+describe('Heartless Intimidator', function () {
+    integration(function () {
+        describe('Heartless Intimidator\'s ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['heartless-intimidator'],
+                        hand: []
+                    },
+                    player2: {
+                        honor: 10,
+                        fate: 10,
+                        inPlay: ['adept-of-shadows'],
+                        dynastyDeck: ['windswept-yurt'],
+                        hand: [],
+                        conflictDeck: ['seeker-of-knowledge']
+                    }
+                });
+                this.heartless = this.player1.findCardByName('heartless-intimidator');
+                this.yurt = this.player2.placeCardInProvince('windswept-yurt', 'province 1');
+            });
+
+            it('will not trigger for honor gains', function() {
+                this.player1.clickPrompt('Pass');
+                this.player2.clickCard(this.yurt);
+                this.player2.clickPrompt('Each player gains 2 honor');
+                expect(this.player1).not.toHavePrompt('Triggered Abilities');
+
+            })
+
+            describe('when an opponent loses an honor', function() {
+                beforeEach(function () {
+                    this.player1.clickPrompt('Pass');
+                });
+
+                it('make the opponent discard a card from the top of their conflict deck', function () {
+                    this.player2.clickCard('adept-of-shadows');    
+                    this.initialSize = this.player2.conflictDeck.length;
+                    this.topCard = this.player2.conflictDeck[0];
+                    expect(this.player1).toHavePrompt('Triggered Abilities');
+                    expect(this.player1).toBeAbleToSelect(this.heartless);
+                    this.player1.clickCard(this.heartless);
+                    expect(this.player2.conflictDeck.length).toBe(this.initialSize - 1);
+                    expect(this.topCard.location).toBe('conflict discard pile')
+                });
+            });
+        });
+    });
+});
