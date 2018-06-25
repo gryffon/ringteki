0e58dfa2f682eb07325c5b7e0be022cb9d901b46
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-06-25 17:58:18 -0400

    my hero, eslint

--- test/server/cards/04.1-BotK/HeartlessIntimidator.spec.js
+++ test/server/cards/04.1-BotK/HeartlessIntimidator.spec.js
@@ -27,7 +27,7 @@ describe('Heartless Intimidator', function () {
                 this.player2.clickPrompt('Each player gains 2 honor');
                 expect(this.player1).not.toHavePrompt('Triggered Abilities');
 
-            })
+            });
 
             describe('when an opponent loses an honor', function() {
                 beforeEach(function () {
@@ -35,14 +35,14 @@ describe('Heartless Intimidator', function () {
                 });
 
                 it('make the opponent discard a card from the top of their conflict deck', function () {
-                    this.player2.clickCard('adept-of-shadows');    
+                    this.player2.clickCard('adept-of-shadows');
                     this.initialSize = this.player2.conflictDeck.length;
                     this.topCard = this.player2.conflictDeck[0];
                     expect(this.player1).toHavePrompt('Triggered Abilities');
                     expect(this.player1).toBeAbleToSelect(this.heartless);
                     this.player1.clickCard(this.heartless);
                     expect(this.player2.conflictDeck.length).toBe(this.initialSize - 1);
-                    expect(this.topCard.location).toBe('conflict discard pile')
+                    expect(this.topCard.location).toBe('conflict discard pile');
                 });
             });
         });
