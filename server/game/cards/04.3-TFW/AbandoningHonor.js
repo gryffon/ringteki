275d5aa3dff3577de019be975f0bcfdae7e977fd
Kwaice <kwaice@gmail.com>
2018-06-26 02:38:54 +0200

    TFW: AbandoningHonor
    Closes #1981

---
+++ server/game/cards/04.3-TFW/AbandoningHonor.js
@@ -0,0 +1,28 @@
+const ProvinceCard = require('../../provincecard.js');
+
+class AbandoningHonor extends ProvinceCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+             match: this,
+             targetLocation: 'province',
+             condition: () => this.controller.role.hasTrait('fire'),
+             effect: ability.effects.modifyProvinceStrength(2)
+        });
+
+        this.interrupt({
+            title: 'Choose a dishonored character',
+            when: {
+                onBreakProvince: (event, context) => event.card === context.source
+           },
+            target: {
+                cardtype: 'character',
+                cardCondition: card => card.isDishonored,
+                gameAction: ability.actions.discardFromPlay()
+            }
+        });
+    }
+}
+
+AbandoningHonor.id = 'abandoning-honor';
+ 
+module.exports = AbandoningHonor;
