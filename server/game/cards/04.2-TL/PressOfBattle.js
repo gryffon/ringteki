7783587a213e9689327d41c0b9672e6fbef97a20
Kwaice <kwaice@gmail.com>
2018-06-29 10:17:36 +0200

    context fix

--- server/game/cards/04.2-TL/PressOfBattle.js
+++ server/game/cards/04.2-TL/PressOfBattle.js
@@ -4,8 +4,8 @@ class PressOfBattle extends DrawCard {
     setupCardAbilities(ability) {
         this.action({
             title: 'Bow a non-unique character',
-            condition: context => context.game.currentConflict.hasMoreParticipants(context.player) &&
-                                    this.game.isDuringConflict('military'),
+            condition: context => this.game.isDuringConflict('military') &&
+                                 this.game.currentConflict.hasMoreParticipants(context.player),
             target: {
                 activePromptTitle: 'Choose a character',
                 cardType: 'character',
