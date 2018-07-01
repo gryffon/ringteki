2815e879f17bfad959fff5e2667c834862a45b91
Nathan <nathaniel.j.mith@gmail.com>
2018-06-30 20:31:16 -0600

    Update DeceptiveOffer.js

--- server/game/cards/04.4-TEaF/DeceptiveOffer.js
+++ server/game/cards/04.4-TEaF/DeceptiveOffer.js
@@ -1,31 +1,31 @@
 const DrawCard = require('../../drawcard.js');
 
 class DeceptiveOffer extends DrawCard {
-  setupCardAbilities(ability) {
-    this.action({
-      title: 'Increase a character\'s military and political skill or take an honor from your opponent',
-      targets: {
-        character: {
-          cardType: 'character',
-          controller: 'self',
-          cardCondition: card => card.isparticipating()
-        },
-        select: {
-          mode: 'select',
-          dependsOn: 'character',
-          player: 'opponent',
-          choices: {
-            'Allow your opponent\'s character to gain military and political skill': ability.actions.cardLastingEffect(() => ({
-              target: context.targets.character,
-              duration: 'untilEndOfConflict',
-              effect: ability.effects.modifyBothSkills(2)
-            })),
-            'Give your opponent 1 honor': ability.actions.takeHonor()
-          }
-        }
-      }
-    });
-  }
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Increase a character\'s military and political skill or take an honor from your opponent',
+            targets: {
+                character: {
+                    cardType: 'character',
+                    controller: 'self',
+                    cardCondition: card => card.isparticipating()
+                },
+                select: {
+                    mode: 'select',
+                    dependsOn: 'character',
+                    player: 'opponent',
+                    choices: {
+                        'Allow your opponent\'s character to gain military and political skill': ability.actions.cardLastingEffect(() => ({
+                            target: context.targets.character,
+                            duration: 'untilEndOfConflict',
+                            effect: ability.effects.modifyBothSkills(2)
+                        })),
+                        'Give your opponent 1 honor': ability.actions.takeHonor()
+                    }
+                }
+            }
+        });
+    }
 }
 
 DeceptiveOffer.id = 'deceptive-offer';
