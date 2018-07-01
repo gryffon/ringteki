48c09b4caf0bf29c214bdd80614a9040296a30e2
Nathan <nathaniel.j.mith@gmail.com>
2018-06-30 21:16:03 -0600

    Update DeceptiveOffer.js
    Just trying something to see if I can make it work. I'm just a little lost on how to make sure the originally selected character is targeted for the +2/+2 boost.

--- server/game/cards/04.4-TEaF/DeceptiveOffer.js
+++ server/game/cards/04.4-TEaF/DeceptiveOffer.js
@@ -16,7 +16,6 @@ class DeceptiveOffer extends DrawCard {
                     player: 'opponent',
                     choices: {
                         'Allow your opponent\'s character to gain military and political skill': ability.actions.cardLastingEffect(() => ({
-                            target: targets.character,
                             duration: 'untilEndOfConflict',
                             effect: ability.effects.modifyBothSkills(2)
                         })),
