1b2cba3e8634fe352fd656f779d1f74cd90b3a4c
Kwaice <kwaice@gmail.com>
2018-07-01 11:46:15 +0200

    Fix

--- test/server/cards/04.2-TL/CourtNovice.spec.js
+++ test/server/cards/04.2-TL/CourtNovice.spec.js
@@ -17,17 +17,20 @@ describe('Court Novice', function() {
 
             it('should give +2 to political skill if the air ring is claimed', function() {
                 this.game.rings.air.claimRing(this.player1.player);
+                this.game.checkGameState(true);
                 expect(this.novice.getPoliticalSkill()).toBe(3);
             });
 
             it('should give +2 to political skill if the water ring is claimed', function() {
                 this.game.rings.water.claimRing(this.player1.player);
+                this.game.checkGameState(true);
                 expect(this.novice.getPoliticalSkill()).toBe(3);
             });
 
             it('should not give +2 to political skill if the opponent claimed the air or water ring', function() {
                 this.game.rings.air.claimRing(this.player2.player);
                 this.game.rings.water.claimRing(this.player2.player);
+                this.game.checkGameState(true);
                 expect(this.novice.getPoliticalSkill()).toBe(1);
             });
 
@@ -35,6 +38,7 @@ describe('Court Novice', function() {
                 this.game.rings.void.claimRing(this.player1.player);
                 this.game.rings.earth.claimRing(this.player1.player);
                 this.game.rings.fire.claimRing(this.player1.player);
+                this.game.checkGameState(true);
                 expect(this.novice.getPoliticalSkill()).toBe(1);
             });
         });
