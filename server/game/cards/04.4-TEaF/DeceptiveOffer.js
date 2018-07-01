7d1945f9625934fc6dc64b1160642d150cfea504
Nathan <nathaniel.j.mith@gmail.com>
2018-06-30 18:34:36 -0600

    Update DeceptiveOffer.js

--- server/game/cards/04.4-TEaF/DeceptiveOffer.js
+++ server/game/cards/04.4-TEaF/DeceptiveOffer.js
@@ -1,10 +1,26 @@
-const DrawCard = require('../../drawcard.js');
-
-class DeceptiveOffer extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
-    }
-}
-
-DeceptiveOffer.id = 'deceptive-offer'; // This is a guess at what the id might be - please check it!!!
-
-module.exports = DeceptiveOffer;
+this.action({
+      title: 'Increase a character\'s military and political skill or take an honor from your opponent',
+      targets: {
+        character: {
+          cardType: 'character',
+          controller: 'self',
+          cardCondition: card => card.isparticipating()
+        },
+        select: {
+          mode: 'select',
+          dependsOn: 'character',
+          player: 'opponent',
+          choices: {
+            'Allow your opponent\'s character to gain military and political skill': ability.actions.cardLastingEffect(() => ({
+              target: context.targets.character,
+              duration: 'untilEndOfConflict',
+              effect: [
+                ability.effects.modifyMilitarySkill(2),
+                ability.effects.modifyPoliticalSkill(2)
+              ]
+            })),
+            'Give your opponent 1 honor': ability.actions.takeHonor()
+          }
+        }
+      }
+    });
