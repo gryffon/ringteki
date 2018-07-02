4c7c4b79b1ef65a6faa8d8badf490f6951412fac
Kwaice <kwaice@gmail.com>
2018-07-02 19:28:38 +0200

    TFW: Wicked-Tetsubo

--- server/game/cards/04.3-TFW/WickedTetsubo.js
+++ server/game/cards/04.3-TFW/WickedTetsubo.js
@@ -1,10 +1,46 @@
 const DrawCard = require('../../drawcard.js');
 
 class WickedTetsubo extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Set Military or Political skill to 0',
+            condition: context => context.source.parent.isAttacking(),
+            targets: {
+                character: {
+                    activePromptTitle: 'Choose a defending character',
+                    cardType: 'character',
+                    cardCondition: card => card.isDefending()
+                },
+                skill: {
+                    dependsOn: 'character',
+                    mode: 'select',
+                    activePromptTitle: 'Choose a skill to set to 0',
+                    choices: {
+                        'military': ability.actions.cardLastingEffect(context => ({
+                            target: context.targets.character,
+                            effect: ability.effects.setMilitarySkill(0)
+                        })),
+                        'political': ability.actions.cardLastingEffect(context => ({
+                            target: context.targets.character,
+                            effect: ability.effects.setPoliticalSkill(0)
+                        }))
+                    }
+                }
+            },
+            effect: 'set {0}\'s {1} skill to 0',
+            effectArgs: context => context.selects.skill.choice
+        });
+    }
+
+    canAttach(card, context) {
+        if(card.controller !== context.player) {
+            return false;
+        }
+
+        return super.canAttach(card, context);
     }
 }
 
-WickedTetsubo.id = 'wicked-tetsubo'; // This is a guess at what the id might be - please check it!!!
+WickedTetsubo.id = 'wicked-tetsubo';
 
 module.exports = WickedTetsubo;
