e831d0d5d6a83125481ab0abb10f0cff5d11f63f
Kwaice <kwaice@gmail.com>
2018-07-03 08:23:14 +0200

    Update

--- server/game/cards/04.2-TL/ApprenticeEarthcaller.js
+++ server/game/cards/04.2-TL/ApprenticeEarthcaller.js
@@ -10,8 +10,8 @@ class ApprenticeEarthcaller extends DrawCard {
                 cardCondition: card => card.isAttacking() && card.attachments.size() === 0,
                 gameAction: ability.actions.cardLastingEffect(context => ({
                     effect: [
-                        ability.effects.setMilitarySkill(context.target.printedMilitarySkill()),
-                        ability.effects.setPoliticalSkill(context.target.printedPoliticalSkill())
+                        ability.effects.setMilitarySkill(context.target.printedMilitarySkill),
+                        ability.effects.setPoliticalSkill(context.target.printedPoliticalSkill)
                     ]
                 }))
             },
