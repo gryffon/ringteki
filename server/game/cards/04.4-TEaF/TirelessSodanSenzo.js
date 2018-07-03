24aaeb0bc9a24a2ccfad263fcddde644d9410a77
Kwaice <kwaice@gmail.com>
2018-07-03 13:05:46 +0200

    Update

--- server/game/cards/04.4-TEaF/TirelessSodanSenzo.js
+++ server/game/cards/04.4-TEaF/TirelessSodanSenzo.js
@@ -3,7 +3,7 @@ const DrawCard = require('../../drawcard.js');
 class TirelessSodanSenzo extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => this.isParticipating && this.game.currentConflict.loser,
+            condition: () => this.isParticipating && this.game.currentConflict.loser === this.controller,
             match: this,
             effect: ability.effects.doesNotBow()
         });
