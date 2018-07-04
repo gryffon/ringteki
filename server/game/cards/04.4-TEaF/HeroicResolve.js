5771db46a0d6ee9bb856a3e8fbd93a6efbeccb38
markjbyrne <markjbyrne@gmail.com>
2018-07-04 22:03:08 +0100

    Implement Heroic Resolve.

--- server/game/cards/04.4-TEaF/HeroicResolve.js
+++ server/game/cards/04.4-TEaF/HeroicResolve.js
@@ -2,6 +2,11 @@ const DrawCard = require('../../drawcard.js');
 
 class HeroicResolve extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.action({
+            title: 'Ready attached character',
+            condition: context => context.player.getClaimedRings().length >= 2,
+            gameAction: ability.actions.ready(context => ({ target: context.source.parent }))
+        });
     }
 }
 
