e8b66861ba63468fb23c0fb6615734d1afb65a41
Kwaice <kwaice@gmail.com>
2018-07-05 17:50:23 +0200

    TEaF: Master of the Swift Waves

--- server/game/cards/04.4-TEaF/MasterOfTheSwiftWaves.js
+++ server/game/cards/04.4-TEaF/MasterOfTheSwiftWaves.js
@@ -1,10 +1,29 @@
 const DrawCard = require('../../drawcard.js');
 
 class MasterOfTheSwiftWaves extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title:'Switch 2 characters you control',
+            condition: () => this.game.isDuringConflict(),
+            targets: {
+                conflict: {
+                    activePromptTitle: 'Choose a participating character',
+                    cardType: 'character',
+                    cardCondition: (card,context) => card.isParticipating() && card.controller === context.player
+                },
+                home: {
+                    activePromptTitle: 'Choose a character at home',
+                    cardType: 'character',
+                    cardCondition: (card,context) => !card.isParticipating() && card.controller === context.player && !card.hasDash(this.game.currentConflict.conflictType),
+                    gameAction: context => [ability.actions.sendHome(context.targets.conflict), ability.actions.moveToConflict(context.targets.home)]
+                }
+            },
+            effect: 'switch {1} and {2} positions',
+            effectArgs: context => [context.targets.conflict, context.targets.home]
+        });
     }
 }
 
-MasterOfTheSwiftWaves.id = 'master-of-the-swift-waves'; // This is a guess at what the id might be - please check it!!!
+MasterOfTheSwiftWaves.id = 'master-of-the-swift-waves';
 
 module.exports = MasterOfTheSwiftWaves;
