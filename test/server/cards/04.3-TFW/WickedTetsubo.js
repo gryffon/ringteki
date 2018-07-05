2497094ba1ceb1c38b76e8fc26de8dcf4597a704
Kwaice <kwaice@gmail.com>
2018-07-05 15:21:36 +0200

    Added test

---
+++ test/server/cards/04.3-TFW/WickedTetsubo.js
@@ -0,0 +1,98 @@
+describe('Wicked Tetsubo', function() {
+    integration(function() {
+        describe('Wicked Tetsubo\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['third-tower-guard', 'crisis-breaker'],
+                        hand: ['wicked-tetsubo']
+                    },
+                    player2: {
+                        inPlay: ['matsu-berserker','venerable-historian']
+                    }
+                });
+
+                this.ttg = this.player1.findCardByName('third-tower-guard');
+                this.breaker = this.player1.findCardByName('crisis-breaker');
+                this.tetsubo = this.player1.findCardByName('wicked-tetsubo');
+
+                this.matsu = this.player2.findCardByName('matsu-berserker');
+                this.historian = this.player2.findCardByName('venerable-historian');
+            });
+
+            it('should only attach to Berserker characters', function() {
+                this.player1.clickCard(this.tetsubo);
+                expect.this.player1.not.toBeAbleToSelect(this.historian);
+                expect.this.player1.not.toBeAbleToSelect(this.ttg);
+                expect.this.player1.toBeAbleToSelect(this.breaker);
+                expect.this.player1.toBeAbleToSelect(this.matsu);
+            });
+
+            it('should not work on defense', function() {
+                this.player1.clickPrompt('Pass Conflict');
+                this.player1.clickPrompt('Yes');
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.historian],
+                    defenders: [this.breaker]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                this.player1.clickCard(this.breaker);
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                expect.this.player1.toHavePrompt('Pass');
+            });
+
+            it('should work on offense', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.breaker],
+                    defenders: [this.historian]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                this.player1.clickCard(this.breaker);
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                expect.this.player1.toBeAbleToSelect(this.historian);
+                expect.this.player1.not.toBeAbleToSelect(this.matsu);
+            });
+
+            it('should not work on targets with a dash', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.breaker],
+                    defenders: [this.historian]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                this.player1.clickCard(this.breaker);
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                expect.this.player1.toBeAbleToSelect(this.historian);
+                this.player1.clickCard(this.historian);
+                expect.this.player1.toHavePrompt('Political');
+                expect.this.player1.not.toHavePrompt('Military');
+            });
+
+            it('should correctly set skill to 0', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.breaker],
+                    defenders: [this.historian]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                this.player1.clickCard(this.breaker);
+                this.player2.pass();
+                this.player1.clickCard(this.tetsubo);
+                this.player1.clickCard(this.historian);
+                this.player1.clickPrompt('Political');
+                expect(this.historian.getPoliticalSkill()).toBe(0);
+            });
+        });
+    });
+});
