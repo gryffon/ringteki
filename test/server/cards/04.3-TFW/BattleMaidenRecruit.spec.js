337f7c2b642ab777c780d1448557c7e8f87a07ad
Noah <nbtheduke@gmail.com>
2018-07-11 16:31:19 -0400

    Battle Maiden Recruit

--- test/server/cards/04.3-TFW/BattleMaidenRecruit.spec.js
+++ test/server/cards/04.3-TFW/BattleMaidenRecruit.spec.js
@@ -5,25 +5,52 @@ describe('Battle Maiden Recruit', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['battle-maiden-recruit']
                     },
                     player2: {
                         inPlay: []
                     }
                 });
+                this.maiden = this.player1.findCardByName('battle-maiden-recruit');
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
+            function worksWithRing(ringType) {
+                it('should give +2 to military skill if the ' + ringType + ' ring is claimed',
+                    function() {
+                        let military = this.maiden.getMilitarySkill();
+                        this.game.rings[ringType].claimRing(this.player1.player);
+                        this.game.checkGameState(true);
+                        expect(this.maiden.getMilitarySkill()).toBe(military + 2);
+                    }
+                );
+            }
 
-            });
+            for(let ringType of ['water', 'void']) {
+                worksWithRing(ringType);
+            }
 
-            it('should not trigger under ABC circumstances', function() {
+            function doesntWorkWithRing(ringType) {
+                it('should not give +2 to military skill if the ' + ringType + ' ring is claimed',
+                    function() {
+                        let military = this.maiden.getMilitarySkill();
+                        this.game.rings[ringType].claimRing(this.player1.player);
+                        this.game.checkGameState(true);
+                        expect(this.maiden.getMilitarySkill()).toBe(military);
+                    }
+                );
+            }
 
-            });
+            for(let ringType of ['air', 'earth', 'fire']) {
+                doesntWorkWithRing(ringType);
+            }
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should not give +2 to military skill if the opponent claimed the air or water ring', function() {
+                let military = this.maiden.getMilitarySkill();
+                this.game.rings.air.claimRing(this.player2.player);
+                this.game.rings.water.claimRing(this.player2.player);
+                this.game.checkGameState(true);
+                expect(this.maiden.getMilitarySkill()).toBe(military);
             });
         });
     });
