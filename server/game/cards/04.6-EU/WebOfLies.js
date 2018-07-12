38c4901507c5cf83ae335b6d10fdc3ac6e4cdd05
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-12 07:50:53 +0100

    bugfix

--- server/game/cards/04.6-EU/WebOfLies.js
+++ server/game/cards/04.6-EU/WebOfLies.js
@@ -1,6 +1,6 @@
-const DrawCard = require('../../drawcard.js');
+const ProvinceCard = require('../../provincecard.js');
 
-class WebOfLies extends DrawCard {
+class WebOfLies extends ProvinceCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
     }
 }
