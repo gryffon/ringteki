c8e890f1ea2a61da0dd2cac898c5a7a79fd87d02
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-12 07:49:19 +0100

    Placeholders for EU

---
+++ server/game/cards/04.6-EU/WebOfLies.js
@@ -0,0 +1,10 @@
+const DrawCard = require('../../drawcard.js');
+
+class WebOfLies extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    }
+}
+
+WebOfLies.id = 'web-of-lies'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = WebOfLies;
