39095dc4659e8a7cba7059a4e6b250b117d6d373
Kwaice <kwaice@gmail.com>
2018-07-14 11:37:21 +0200

    TEaF: Nezumi Infiltrator
    Closes #1997

--- server/game/cards/04.4-TEaF/NezumiInfiltrator.js
+++ server/game/cards/04.4-TEaF/NezumiInfiltrator.js
@@ -1,10 +1,40 @@
 const DrawCard = require('../../drawcard.js');
 
 class NezumiInfiltrator extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: this,
+            effect: ability.effects.immuneTo({
+                restricts: '',
+                source: this
+            })
+        }),
+        this.reaction({
+            title: 'Change attacked province\'s strength',
+            condition: () => this.game.isDuringConflict(),
+            when: {
+                onCharacterEntersPlay: (event, context) => event.card === context.source
+            },
+            max: ability.limit.perConflict(1),
+            target: {
+                mode: 'select',
+                choices: {
+                    'Raise attacked province\'s strength by 1': ability.actions.cardLastingEffect(() => ({
+                        target: this.game.currentConflict.conflictProvince,
+                        targetLocation: 'province',
+                        effect: ability.effects.modifyProvinceStrength(1)
+                    })),
+                    'Lower attacked province\'s strength by 1': ability.actions.cardLastingEffect(() => ({
+                        target: this.game.currentConflict.conflictProvince,
+                        targetLocation: 'province',
+                        effect: ability.effects.modifyProvinceStrength(-1)
+                    }))
+                }
+            }
+        });
     }
 }
 
-NezumiInfiltrator.id = 'nezumi-infiltrator'; // This is a guess at what the id might be - please check it!!!
+NezumiInfiltrator.id = 'nezumi-infiltrator';
 
 module.exports = NezumiInfiltrator;
