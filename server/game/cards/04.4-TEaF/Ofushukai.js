daf136761cb20f36db03cd58b3a48cab7b41e179
markjbyrne <markjbyrne@gmail.com>
2018-07-16 21:52:17 +0100

    Implementing Ofushukai
    Test to follow..

--- server/game/cards/04.4-TEaF/Ofushukai.js
+++ server/game/cards/04.4-TEaF/Ofushukai.js
@@ -2,6 +2,31 @@ const DrawCard = require('../../drawcard.js');
 
 class Ofushukai extends DrawCard {
     setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+        this.grantedAbilityLimits = {};
+        this.whileAttached({
+            match: card => card.hasTrait('champion'),
+            effect: ability.effects.gainAbility('action', {
+                title: 'Send a character home',
+                condition: context => context.source.parent.isParticipating(),
+                printedAbility: false,
+                target: {
+                    cardType: 'character',
+                    controller: 'any',
+                    cardCondition: card => card.isParticipating(),
+                    gameAction: [
+                        ability.actions.sendHome(),
+                        ability.actions.cardLastingEffect({
+                            duration: 'untilEndOfPhase',
+                            effect: ability.effects.cannotParticipateAsAttacker()
+                        })
+                    ]
+                }
+            })
+        });
+    }
+
+    canAttach(card, context) {
+        return context.source.isUnique() && context.source.getPrintedFaction() === 'phoenix' ? super.canAttach(card, context) : false;
     }
 }
 
