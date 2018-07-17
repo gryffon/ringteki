d184809e46d7ee3dc394e1202e940a06cadf936f
Caelan Mayberry <gmodman12@gmail.com>
2018-07-17 11:03:58 -0400

    Added dreamer and tests

--- server/game/cards/04.5-AaN/EtherealDreamer.js
+++ server/game/cards/04.5-AaN/EtherealDreamer.js
@@ -1,10 +1,27 @@
 const DrawCard = require('../../drawcard.js');
 
 class EtherealDreamer extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Gain +2/+2 while contesting the target ring',
+            when: {
+                onPhaseStarted: event => event.phase === 'conflict'
+            },
+            target: {
+                mode: 'ring',
+                ringCondition: () => true
+            },
+            effect: 'Gain +2{1}/+2{2} while {0} is contested',
+            effectArgs: ['military', 'political'],
+            gameAction: ability.actions.playerLastingEffect(context => ({
+                duration: 'untilEndOfPhase',
+                condition: () => this.game.currentConflict && this.game.currentConflict.ring === context.ring,
+                effect: ability.effects.modifyBothSkills(2)
+            }))
+        });
     }
 }
 
-EtherealDreamer.id = 'ethereal-dreamer'; // This is a guess at what the id might be - please check it!!!
+EtherealDreamer.id = 'ethereal-dreamer';
 
 module.exports = EtherealDreamer;
