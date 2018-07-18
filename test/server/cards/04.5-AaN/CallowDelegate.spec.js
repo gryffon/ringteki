a2f74f695be7f2991e590336e680141f93788c1d
markjbyrne <markjbyrne@gmail.com>
2018-07-19 00:07:45 +0100

    Implementing Callow Delegate
    Includes test.

--- test/server/cards/04.5-AaN/CallowDelegate.spec.js
+++ test/server/cards/04.5-AaN/CallowDelegate.spec.js
@@ -5,25 +5,40 @@ describe('Callow Delegate', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: ['callow-delegate']
+                        inPlay: ['bayushi-shoju'],
+                        hand: ['assassination']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['callow-delegate', 'doji-challenger']
                     }
                 });
+                this.bayushiShoju = this.player1.findCardByName('bayushi-shoju');
+                this.assassination = this.player1.findCardByName('assassination');
+
+                this.callowDelegate = this.player2.findCardByName('callow-delegate');
+                this.dojiChallenger = this.player2.findCardByName('doji-challenger');
+            });
+
+            it('should prompt to honor a character when leaving play', function() {
                 this.noMoreActions();
-            });
-
-            it('should trigger under XYZ circumstances', function() {
-
-            });
-
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.bayushiShoju],
+                    defenders: [this.callowDelegate]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.assassination);
+                this.player1.clickCard(this.callowDelegate);
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.callowDelegate);
+                this.player2.clickCard(this.callowDelegate);
+                expect(this.player2).toHavePrompt('Callow Delegate');
+                expect(this.player2).toBeAbleToSelect(this.callowDelegate);
+                expect(this.player2).toBeAbleToSelect(this.dojiChallenger);
+                expect(this.player2).not.toBeAbleToSelect(this.bayushiShoju);
+                this.player2.clickCard(this.dojiChallenger);
+                expect(this.callowDelegate.location).toBe('dynasty discard pile');
+                expect(this.dojiChallenger.isHonored).toBe(true);
             });
         });
     });
