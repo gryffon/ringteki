139e0848ad99266a5b53fda76af9db7f2ae3a8de
Kwaice <kwaice@gmail.com>
2018-07-23 17:34:21 +0200

    Simpler test

--- test/server/cards/04.5-AaN/BustlingAcademy.spec.js
+++ test/server/cards/04.5-AaN/BustlingAcademy.spec.js
@@ -8,8 +8,8 @@ describe('Bustling Academy', function() {
                         inPlay: ['naive-student','bustling-academy','kanjo-district']
                     },
                     player2: {
-                        inPlay: ['shiotome-encampment','moto-youth','moto-juro','shinjo-scout'],
-                        hand: ['noble-sacrifice']
+                        inPlay: ['shiotome-encampment','moto-youth','shinjo-scout'],
+                        hand: []
                     }
                 });
 
@@ -19,7 +19,6 @@ describe('Bustling Academy', function() {
 
                 this.shio = this.player2.placeCardInProvince('shiotome-encampment','province 1');
                 this.youth = this.player2.placeCardInProvince('moto-youth','province 2');
-                this.juro = this.player2.findCardByName('moto-juro');
                 this.scout = this.player2.findCardByName('shinjo-scout');
             });
 
@@ -53,17 +52,10 @@ describe('Bustling Academy', function() {
             });
 
             it('should not work if a scholar is not in play', function() {
-                this.naive.dishonor();
-                this.juro.honor();
-                this.player1.pass();
-                this.player2.clickCard('noble-sacrifice');
-                expect(this.player2).toBeAbleToSelect(this.naive);
-                this.player2.clickCard(this.naive);
-                expect(this.player2).toBeAbleToSelect(this.juro);
-                this.player2.clickCard(this.juro);
+                this.player1.moveCard('naive-student', 'dynasty discard pile');
                 expect(this.naive.location).toBe('dynasty discard pile');
                 this.player1.clickCard(this.ba);
-                expect(this.player1).not.toHavePrompt('Choose a card');
+                expect(this.player1).toHavePrompt('Initiate an action');
             });
         });
     });
