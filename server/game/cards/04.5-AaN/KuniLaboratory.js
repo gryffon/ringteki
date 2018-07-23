2dc5722ab4b4e931ea0cd0d971feb6852711479b
markjbyrne <markjbyrne@gmail.com>
2018-07-23 22:14:04 +0100

    Initial Commit of Kuni Laboratory

--- server/game/cards/04.5-AaN/KuniLaboratory.js
+++ server/game/cards/04.5-AaN/KuniLaboratory.js
@@ -1,10 +1,23 @@
 const DrawCard = require('../../drawcard.js');
 
 class KuniLaboratory extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            match: card => card.getType() === 'character' && card.location === 'play area',
+            targetController: 'self',
+            effect: ability.effects.modifyBothSkills(1)
+        });
+
+        this.forcedReaction({
+            title: 'After the conflict phase begins lose 1 honor',
+            when: {
+                onPhaseStarted: event => event.phase === 'conflict'
+            },
+            gameAction: ability.actions.loseHonor()
+        });
     }
 }
 
-KuniLaboratory.id = 'kuni-laboratory'; // This is a guess at what the id might be - please check it!!!
+KuniLaboratory.id = 'kuni-laboratory';
 
 module.exports = KuniLaboratory;
--- test/server/cards/04.5-AaN/KuniLaboratory.spec.js
+++ test/server/cards/04.5-AaN/KuniLaboratory.spec.js
@@ -3,28 +3,79 @@ describe('Kuni Laboratory', function() {
         describe('Kuni Laboratory\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
-                    phase: 'conflict',
+                    phase: 'draw',
                     player1: {
-                        inPlay: []
+                        honor: 10,
+                        inPlay: ['bayushi-shoju'],
+                        hand: ['assassination']                       
                     },
                     player2: {
-                        inPlay: []
+                        fate: 4,
+                        honor: 10,
+                        inPlay: ['borderlands-defender', 'third-tower-guard'],
+                        hand: ['hiruma-ambusher'],
+                        dynastyDeck: ['kuni-laboratory']
                     }
                 });
+  
+                this.bayushiShoju = this.player1.findCardByName('bayushi-shoju');
+  
+                this.borderlandsDefender = this.player2.findCardByName('borderlands-defender');
+                this.thirdTowerGuard = this.player2.findCardByName('third-tower-guard');
+                this.hirumaAmbusher = this.player2.findCardByName('hiruma-ambusher');
+                this.kuniLaboratory = this.player2.placeCardInProvince('kuni-laboratory', 'province 1');
+                
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+            });
+  
+            it('should trigger at the start of the conflict phase', function() {              
                 this.noMoreActions();
+                expect(this.game.currentPhase).toBe('conflict');
+                expect(this.player2.honor).toBe(9);
             });
-
-            it('should trigger under XYZ circumstances', function() {
-
-            });
-
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
-            });
+  
+            // it('should give +1/+1 to all of the controllers characters in play', function() {
+            //     this.noMoreActions();
+            //     expect(this.game.currentPhase).toBe('conflict');
+            //     this.initiateConflict({
+            //         type: 'military',
+            //         attackers: [this.bayushiShoju],
+            //         defenders: [this.borderlandsDefender, this.thirdTowerGuard]
+            //     });
+            //     expect(this.borderlandsDefender.getMilitarySkill()).toBe(this.borderlandsDefender.getBaseMilitarySkill() + 1);
+            //     expect(this.borderlandsDefender.getPoliticalSkill()).toBe(this.borderlandsDefender.getBasePoliticalSkill() + 1);            
+            //     expect(this.thirdTowerGuard.getMilitarySkill()).toBe(this.thirdTowerGuard.getBaseMilitarySkill() + 1);
+            //     expect(this.thirdTowerGuard.getPoliticalSkill()).toBe(this.thirdTowerGuard.getBasePoliticalSkill() + 1);
+            // });
+  
+            // it('should give +1/+1 to the controllers character when it is player from hand', function() {
+            //     this.noMoreActions();
+            //     expect(this.game.currentPhase).toBe('conflict');
+            //     this.initiateConflict({
+            //         type: 'military',
+            //         attackers: [this.bayushiShoju],
+            //         defenders: []
+            //     });
+            //     this.player1.playCharacterFromHand('hiruma-ambusher');
+            //     expect(this.hirumaAmbusher.getMilitarySkill()).toBe(this.hirumaAmbusher.getBaseMilitarySkill() + 1);
+            //     expect(this.hirumaAmbusher.getPoliticalSkill()).toBe(this.hirumaAmbusher.getBasePoliticalSkill() + 1);
+            // });
+  
+            // it('should not give +1/+1 to the opponents characters in play', function() {
+            //     this.noMoreActions();
+            //     expect(this.game.currentPhase).toBe('conflict');
+            //     this.initiateConflict({
+            //         type: 'military',
+            //         attackers: [this.bayushiShoju],
+            //         defenders: [this.borderlandsDefender, this.thirdTowerGuard]
+            //     });
+            //     expect(this.bayushiShoju.getMilitarySkill()).not.toBe(this.bayushiShoju.getBaseMilitarySkill() + 1);
+            //     expect(this.bayushiShoju.getPoliticalSkill()).not.toBe(this.bayushiShoju.getBasePoliticalSkill() + 1);
+            //     expect(this.bayushiShoju.getMilitarySkill()).toBe(this.bayushiShoju.getBaseMilitarySkill());
+            //     expect(this.bayushiShoju.getPoliticalSkill()).toBe(this.bayushiShoju.getBasePoliticalSkill());
+            // });
         });
     });
 });
+  
