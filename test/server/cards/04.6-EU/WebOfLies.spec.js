f89d4253543d1d68019152f3c201960285abd37d
markjbyrne <markjbyrne@gmail.com>
2018-07-24 22:09:11 +0100

    Implementing Web Of Lies
    Including test.

--- test/server/cards/04.6-EU/WebOfLies.spec.js
+++ test/server/cards/04.6-EU/WebOfLies.spec.js
@@ -5,24 +5,30 @@ describe('Web of Lies', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['borderlands-defender']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['bayushi-kachiko'],
+                        hand: ['policy-debate'],
+                        provinces: ['web-of-lies']
                     }
                 });
+                this.borderlandsDefender = this.player1.findCardByName('borderlands-defender');
+
+                this.webOfLies = this.player2.findCardByName('web-of-lies');
+                this.bayushiKachiko = this.player2.findCardByName('bayushi-kachiko');
+                this.player2.showBid = 3;
+
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('province strength should be 6', function() {
+                this.webOfLies.facedown = false;
+                this.game.checkGameState(true);
+                expect(this.webOfLies.getStrength()).toBe(6);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
+            it('provice should update if the players bid changes', function() {
 
             });
         });
