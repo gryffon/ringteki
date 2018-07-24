48500ba2da4b236b1f9bd4b1f67e3c1b6e87ace4
markjbyrne <markjbyrne@gmail.com>
2018-07-24 20:46:25 +0100

    Removing unnecessary code

--- test/server/cards/04.5-AaN/TaintedHero.spec.js
+++ test/server/cards/04.5-AaN/TaintedHero.spec.js
@@ -6,21 +6,17 @@ describe('Tainted Hero', function() {
                     this.setupTest({
                         phase: 'conflict',
                         player1: {
-                            inPlay: ['tainted-hero', 'hida-guardian'],
-                            provinces: ['shameful-display']
+                            inPlay: ['tainted-hero', 'hida-guardian']
                         },
                         player2: {
-                            inPlay: ['bayushi-manipulator'],
-                            provinces: ['manicured-garden']
+                            inPlay: ['bayushi-manipulator']
                         }
                     });
 
                     this.taintedHero = this.player1.findCardByName('tainted-hero');
                     this.hidaGuardian = this.player1.findCardByName('hida-guardian');
-                    this.shamefulDisplay = this.player1.findCardByName('shameful-display', 'province 1');
 
                     this.bayushiManipulator = this.player2.findCardByName('bayushi-manipulator');
-                    this.manicuredGarden = this.player2.findCardByName('manicured-garden', 'province 1');
                 });
 
                 it('should not be allowed to be declared as an attacker', function() {
@@ -28,24 +24,20 @@ describe('Tainted Hero', function() {
                     // skip pre-conflict action phase
                     this.noMoreActions();
 
-                    // select ring and province to attack
-                    this.player1.clickRing('fire');
-                    expect(this.player1).toHavePrompt('Military Fire Conflict');
-                    this.player1.clickCard(this.manicuredGarden);
+                    // initiate conflict
+                    this.initiateConflict({
+                        type: 'military',
+                        attackers: [this.taintedHero, this.hidaGuardian],
+                        defenders: [this.bayushiManipulator]
+                    });
 
                     // should allow the player to attack with the hida guardian
-                    this.player1.clickCard(this.hidaGuardian);
                     expect(this.game.currentConflict.attackers).toContain(this.hidaGuardian);
                     expect(this.hidaGuardian.inConflict).toBe(true);
 
                     // should not allow the player to attack with the tainted hero
-                    this.player1.clickCard(this.taintedHero);
                     expect(this.game.currentConflict.attackers).not.toContain(this.taintedHero);
                     expect(this.taintedHero.inConflict).not.toBe(true);
-
-                    // initiate the conflict
-                    this.player1.clickPrompt('Initiate Conflict');
-                    expect(this.player2).toHavePrompt('Choose Defenders');
                 });
 
                 it('should be allowed to be declared as an attacker after sacrificing a friendly character', function() {
@@ -56,23 +48,19 @@ describe('Tainted Hero', function() {
                     expect(this.player1).toHavePrompt('Select card to sacrifice');
                     this.player1.clickCard(this.hidaGuardian);
                     expect(this.hidaGuardian.location).toBe('dynasty discard pile');
-
                     expect(this.player2).toHavePrompt('Initiate an action');
                     this.noMoreActions();
 
-                    // select ring and province to attack
-                    this.player1.clickRing('fire');
-                    expect(this.player1).toHavePrompt('Military Fire Conflict');
-                    this.player1.clickCard(this.manicuredGarden);
+                    // initiate conflict
+                    this.initiateConflict({
+                        type: 'military',
+                        attackers: [this.taintedHero],
+                        defenders: [this.bayushiManipulator]
+                    });
 
                     // should not allow the player to attack with the tainted hero
-                    this.player1.clickCard(this.taintedHero);
                     expect(this.game.currentConflict.attackers).toContain(this.taintedHero);
                     expect(this.taintedHero.inConflict).toBe(true);
-
-                    // initiate the conflict
-                    this.player1.clickPrompt('Initiate Conflict');
-                    expect(this.player2).toHavePrompt('Choose Defenders');
                 });
             });
 
@@ -81,21 +69,17 @@ describe('Tainted Hero', function() {
                     this.setupTest({
                         phase: 'conflict',
                         player1: {
-                            inPlay: ['bayushi-manipulator'],
-                            provinces: ['manicured-garden']
+                            inPlay: ['bayushi-manipulator']
                         },
                         player2: {
-                            inPlay: ['tainted-hero', 'hida-guardian'],
-                            provinces: ['shameful-display']
+                            inPlay: ['tainted-hero', 'hida-guardian']
                         }
                     });
 
                     this.bayushiManipulator = this.player1.findCardByName('bayushi-manipulator');
-                    this.manicuredGarden = this.player1.findCardByName('manicured-garden', 'province 1');
 
                     this.taintedHero = this.player2.findCardByName('tainted-hero');
                     this.hidaGuardian = this.player2.findCardByName('hida-guardian');
-                    this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 1');
                 });
 
                 it('should not be allowed to be declared as an defender', function() {
@@ -103,32 +87,27 @@ describe('Tainted Hero', function() {
                     // skip pre-conflict action phase
                     this.noMoreActions();
 
-                    // select ring and province to attack
-                    this.player1.clickRing('fire');
-                    expect(this.player1).toHavePrompt('Military Fire Conflict');
-                    this.player1.clickCard(this.shamefulDisplay);
+                    // initiate conflict
+                    this.initiateConflict({
+                        type: 'military',
+                        attackers: [this.bayushiManipulator],
+                        defenders: [this.taintedHero, this.hidaGuardian]
+                    });
 
-                    // should allow the player to attack with the bayushi manipulator
-                    this.player1.clickCard(this.bayushiManipulator);
                     expect(this.game.currentConflict.attackers).toContain(this.bayushiManipulator);
                     expect(this.bayushiManipulator.inConflict).toBe(true);
 
-                    // initiate the conflict
-                    this.player1.clickPrompt('Initiate Conflict');
-                    expect(this.player2).toHavePrompt('Choose Defenders');
-
                     // should allow the player to defend with the hida guardian
-                    this.player2.clickCard(this.hidaGuardian);
                     expect(this.game.currentConflict.defenders).toContain(this.hidaGuardian);
                     expect(this.hidaGuardian.inConflict).toBe(true);
 
                     // should not allow the player to defend with the tainted hero
-                    this.player2.clickCard(this.taintedHero);
                     expect(this.game.currentConflict.defenders).not.toContain(this.taintedHero);
                     expect(this.taintedHero.inConflict).not.toBe(true);
                 });
 
                 it('should be allowed to be declared as a defender after sacrificing a friendly character', function() {
+                    
                     // pre-conflict actions
                     expect(this.player1).toHavePrompt('Initiate an action');
                     this.player1.pass();
@@ -142,19 +121,15 @@ describe('Tainted Hero', function() {
                     expect(this.player1).toHavePrompt('Initiate an action');
                     this.noMoreActions();
 
-                    // select ring and province to attack
-                    this.player1.clickRing('fire');
-                    expect(this.player1).toHavePrompt('Military Fire Conflict');
-                    this.player1.clickCard(this.shamefulDisplay);
+                    // initiate conflict
+                    this.initiateConflict({
+                        type: 'military',
+                        attackers: [this.bayushiManipulator],
+                        defenders: [this.taintedHero, this.hidaGuardian]
+                    });
 
-                    // should allow the player to attack with the bayushi manipulator
-                    this.player1.clickCard(this.bayushiManipulator);
                     expect(this.game.currentConflict.attackers).toContain(this.bayushiManipulator);
                     expect(this.bayushiManipulator.inConflict).toBe(true);
-
-                    // initiate the conflict
-                    this.player1.clickPrompt('Initiate Conflict');
-                    expect(this.player2).toHavePrompt('Choose Defenders');
 
                     // should allow the player to defend with the tainted hero
                     this.player2.clickCard(this.taintedHero);
