fe155e24b6d51835b955d137706fd721aa04ffc3
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-07-27 21:28:09 +0100

    placeholders for EU

---
+++ server/game/cards/04.6-EU/HisuMoriToride.js
@@ -0,0 +1,10 @@
+const DrawCard = require('../../drawcard.js');
+
+class HisuMoriToride extends DrawCard {
+    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    }
+}
+
+HisuMoriToride.id = 'hisu-mori-toride'; // This is a guess at what the id might be - please check it!!!
+
+module.exports = HisuMoriToride;
