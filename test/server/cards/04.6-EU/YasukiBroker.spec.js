bfe1e98060461b4490af8b416689d4bc69a24350
markjbyrne <markjbyrne@gmail.com>
2018-07-30 21:53:35 +0100

    Implementing Yasuki Broker
    Including test.

--- test/server/cards/04.6-EU/YasukiBroker.spec.js
+++ test/server/cards/04.6-EU/YasukiBroker.spec.js
@@ -5,25 +5,49 @@ describe('Yasuki Broker', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        fate: 3,
+                        inPlay: ['yasuki-broker', 'hida-guardian'],
+                        hand: []
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['border-rider']
                     }
                 });
+
+                this.yasukiBroker = this.player1.findCardByName('yasuki-broker');
+                this.hidaGuardian = this.player1.findCardByName('hida-guardian');
+
+                this.borderRider = this.player2.findCardByName('border-rider');
+
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should give all characters courtesy and sincerity while participating', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.yasukiBroker],
+                    defenders: [this.borderRider]
+                });
+                expect(this.yasukiBroker).toBe(true);
+                expect(this.yasukiBroker).toBe(true);
+                expect(this.hidaGuardian).toBe(true);
+                expect(this.hidaGuardian).toBe(true);
+                expect(this.borderRider).toBe(false);
+                expect(this.borderRider).toBe(false);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+            it('should not give all characters courtesy and sincerity when not participating', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.hidaGuardian],
+                    defenders: [this.borderRider]
+                });
+                expect(this.yasukiBroker).toBe(false);
+                expect(this.yasukiBroker).toBe(false);
+                expect(this.hidaGuardian).toBe(false);
+                expect(this.hidaGuardian).toBe(false);
+                expect(this.borderRider).toBe(false);
+                expect(this.borderRider).toBe(false);
             });
         });
     });
