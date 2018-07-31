8ae9a666a015ebadb391c50f49db3936dcd9dfe0
Caelan Mayberry <gmodman12@gmail.com>
2018-07-31 11:08:26 -0400

    Added roving w/ tests

--- server/game/cards/04.6-EU/RovingMichibiku.js
+++ server/game/cards/04.6-EU/RovingMichibiku.js
@@ -1,10 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class RovingMichibiku extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Take a ring from opponent\'s claimed pool',
+            when: {
+                afterConflict: (event, context) => context.source.isParticipating() && event.conflict.winner === context.player
+            },
+            gameAction: ability.actions.takeRing(context => ({
+                takeFate: true,
+                promptForSelect: {
+                    activePromptTitle: 'Choose a ring to take',
+                    ringCondition: ring => ring.claimedBy === context.player.opponent.name,
+                    message: '{0} takes {1}',
+                    messageArgs: card => [context.player, card]
+                }
+            }))
+        });
     }
 }
 
-RovingMichibiku.id = 'roving-michibiku'; // This is a guess at what the id might be - please check it!!!
+RovingMichibiku.id = 'roving-michibiku';
 
 module.exports = RovingMichibiku;
