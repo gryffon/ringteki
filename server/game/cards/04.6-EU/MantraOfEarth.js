519b45f53d6f71ce0de05e8f26eac18595202338
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2018-07-30 20:17:45 -0400

    Initial pass

--- server/game/cards/04.6-EU/MantraOfEarth.js
+++ server/game/cards/04.6-EU/MantraOfEarth.js
@@ -1,7 +1,28 @@
 const DrawCard = require('../../drawcard.js');
 
 class MantraOfEarth extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'make a monk untargetable by opponents\' card effects and draw a card',
+            when: {
+                onConflictDeclared: (event, context) => event.ring.hasElement('earth') && event.conflict.attackingPlayer === context.player.opponent
+            },
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.hasTrait('monk') || card.attachments.any(card => card.hasTrait('monk')),
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: [
+                        ability.effects.cardCannot({
+                            cannot: 'target',
+                            restricts: 'opponentsCardEffects',
+                            player: context.player
+                        })
+                    ]
+                }))
+            },
+            effect: 'make {0} untargetable by opponents\' card effects and draw a card',
+            gameAction: ability.actions.draw()
+        });
     }
 }
 
--- test/server/cards/04.6-EU/MantraOfEarth.spec.js
+++ test/server/cards/04.6-EU/MantraOfEarth.spec.js
@@ -1,29 +1,71 @@
-describe('Mantra of Earth', function() {
-    integration(function() {
-        describe('Mantra of Earth\'s ability', function() {
-            beforeEach(function() {
+describe('Mantra of Earth', function () {
+    integration(function () {
+        describe('Mantra of Earth\'s ability', function () {
+            beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        provinces: ['kuroi-mori'],
+                        inPlay: ['child-of-the-plains', 'togashi-initiate'],
+                        hand: ['spreading-the-darkness', 'mantra-of-earth']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['solemn-scholar'],
+                        hand: ['assassination', 'cloud-the-mind', 'spreading-the-darkness', 'mantra-of-earth']
                     }
                 });
+                this.mantra = this.player1.findCard('mantra-of-earth', 'hand');
+                this.togashi = this.player1.findCard('togashi-initiate');
+                this.noMoreActions();
+                this.player1.pass();
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('it should trigger when the earth ring is contested', function () {
+                this.initiateConflict({
+                    ring: 'earth',
+                    attackers: ['solemn-scholar']
+                });
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.mantra);
+                this.player1.clickCard(this.mantra);
+                expect(this.player1).toBeAbleToSelect(this.togashi);
+                this.player1.clickCard(this.togashi);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
-            it('should not trigger under ABC circumstances', function() {
+            describe('when the ability is triggered', function () {
+                beforeEach(function () {
+                    this.initiateConflict({
+                        ring: 'earth',
+                        province: 'kuroi-mori',
+                        attackers: ['solemn-scholar']
+                    });
+                    this.player1.clickCard(this.mantra);
+                    this.player1.clickCard(this.togashi);
+                });
 
-            });
+                it('should prevent targeting by opponent\'s events', function () {
+                    this.player2.clickCard('assassination');
+                    expect(this.player2).toHavePrompt('Assassination');
+                    expect(this.player2).toBeAbleToSelect('solemn-scholar');
+                    expect(this.player2).not.toBeAbleToSelect(this.togashi);
+                });
 
-            it('should have DEF effect on GHI', function() {
+                it('should not prevent attaching attachments to that character', function () {
+                    this.player2.clickCard('cloud-the-mind');
+                    expect(this.player2).toHavePrompt('Cloud the Mind');
+                    expect(this.player2).toBeAbleToSelect('solemn-scholar');
+                    expect(this.player2).toBeAbleToSelect(this.togashi);
+                });
 
+                it('should not prevent targeting that character with ring effects', function () {
+                    this.player1.clickCard('kuroi-mori');
+                    this.player1.clickRing('water');
+                    this.noMoreActions();
+                    expect(this.player2).toHavePrompt('Water Ring');
+                    expect(this.player2).toBeAbleToSelect(this.togashi);
+                });
             });
         });
     });
