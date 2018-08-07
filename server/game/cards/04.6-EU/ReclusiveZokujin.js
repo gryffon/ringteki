4c7b46c6810552af16fd070f210da6ab8c52b816
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-08-07 17:43:09 +0100

    fix

--- server/game/cards/04.6-EU/ReclusiveZokujin.js
+++ server/game/cards/04.6-EU/ReclusiveZokujin.js
@@ -11,6 +11,7 @@ class ReclusiveZokujin extends DrawCard {
                     restricts: 'opponentsCardEffects',
                     source: this
                 })
+            ]
         });
     }
 }
