4f1c866aaad909840eeaa7bdc4a5b41592dd721a
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-09-26 14:10:34 +0100

    Update HenshinDisciple.js

--- server/game/cards/02.5-FHNS/HenshinDisciple.js
+++ server/game/cards/02.5-FHNS/HenshinDisciple.js
@@ -3,21 +3,21 @@ const DrawCard = require('../../drawcard.js');
 class HenshinDisciple extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: () => 
+            condition: () =>
                 this.game.rings.air.isConsideredClaimed(this.controller) ||
                 this.game.rings.air.contested,
             match: this,
             effect: ability.effects.modifyPoliticalSkill(2)
         });
         this.persistentEffect({
-            condition: () => 
+            condition: () =>
                 this.game.rings.earth.isConsideredClaimed(this.controller) ||
                 this.game.rings.earth.contested,
             match: this,
             effect: ability.effects.modifyMilitarySkill(2)
         });
         this.persistentEffect({
-            condition: () => 
+            condition: () =>
                 this.game.rings.fire.isConsideredClaimed(this.controller) ||
                 this.game.rings.fire.contested,
             match: this,
