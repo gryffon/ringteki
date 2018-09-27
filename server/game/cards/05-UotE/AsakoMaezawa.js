dfa09b306e73b7b83262f0b39c9ffe619560f207
Caelan Mayberry <gmodman12@gmail.com>
2018-09-27 17:24:42 -0400

    Added Maezawa

--- server/game/cards/05-UotE/AsakoMaezawa.js
+++ server/game/cards/05-UotE/AsakoMaezawa.js
@@ -1,7 +1,25 @@
 const DrawCard = require('../../drawcard.js');
 
 class AsakoMaezawa extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Double a character\'s base political skill',
+            condition: context => context.source.isParticipating() && context.player.opponent && (
+                // My total glory
+                context.player.cardsInPlay.reduce((myTotal, card) => myTotal + (card.isParticipating() && !card.bowed ? card.getGlory() : 0), 0) >
+                // is greater than Opponents total glory
+                context.player.opponent.cardsInPlay.reduce((oppTotal, card) => oppTotal + (card.isParticipating() && !card.bowed ? card.getGlory() : 0), 0)
+            ),
+            target: {
+                cardType: 'character',
+                cardCondition: card => card.isParticipating(),
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: ability.effects.modifyBasePoliticalSkill(context.target.getBasePoliticalSkill())
+                }))
+            },
+            effect: 'double {0}\'s base {1} skill',
+            effectArgs: () => ['political']
+        });
     }
 }
 
