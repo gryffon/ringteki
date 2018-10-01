3471ad5c4f8f93d8fed8e9e4a11b95fbf78e6820
Kwaice <kwaice@gmail.com>
2018-10-01 18:32:07 +0200

    Added tests

--- test/server/cards/05-UotE/DojiFumiki.spec.js
+++ test/server/cards/05-UotE/DojiFumiki.spec.js
@@ -30,12 +30,43 @@ describe('Doji Fumiki', function() {
                 expect(this.player1).not.toHavePrompt('Doji Fumiki');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should only target dishonored participating characters', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: ['doji-fumiki'],
+                    defenders: ['akodo-toturi','matsu-berserker']
+                });
+                this.berserker.dishonor();
+                this.player2.pass();
+                this.player1.clickCard(this.fumiki);
+                expect(this.player1).toHavePrompt('Doji Fumiki');
+                expect(this.player1).toBeAbleToSelect(this.berserker);
+                expect(this.player1).not.toBeAbleToSelect(this.toturi);
             });
 
-            it('should have DEF effect on GHI', function() {
+            it('should correctly bow target character', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: ['doji-fumiki'],
+                    defenders: ['akodo-toturi','matsu-berserker']
+                });
+                this.berserker.dishonor();
+                this.player2.pass();
+                this.player1.clickCard(this.fumiki);
+                this.player1.clickCard(this.berserker);
+                expect(this.berserker.bowed).toBe(true);
+            });
 
+            it('should not work if she is not participating', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: ['doji-whisperer'],
+                    defenders: ['akodo-toturi']
+                });
+                this.toturi.dishonor();
+                this.player2.pass();
+                this.player1.clickCard(this.fumiki);
+                expect(this.player1).not.toHavePrompt('Doji Fumiki');
             });
         });
     });
