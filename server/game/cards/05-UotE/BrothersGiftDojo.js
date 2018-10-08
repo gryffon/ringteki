565afc58e309554e4a07a3a382e0bad28c0b2b72
Kwaice <kwaice@gmail.com>
2018-10-08 20:52:34 +0200

    UotE: Brother's Gift Dojo

--- server/game/cards/05-UotE/BrothersGiftDojo.js
+++ server/game/cards/05-UotE/BrothersGiftDojo.js
@@ -1,10 +1,23 @@
 const ProvinceCard = require('../../provincecard.js');
 
 class BrothersGiftDojo extends ProvinceCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Move a character home',
+            limit: ability.limit.perRound(2),
+            condition: () => this.game.isDuringConflict(),
+            cost: ability.costs.payHonor(1),
+            target: {
+                cardType: 'character',
+                controller: 'self',
+                cardCondition: card => card.isParticipating(),
+                gameAction: ability.actions.sendHome()
+            },
+            effect: 'move {0} home'
+        });
     }
 }
 
-BrothersGiftDojo.id = 'brother-s-gift-dojo'; // This is a guess at what the id might be - please check it!!!
+BrothersGiftDojo.id = 'brother-s-gift-dojo';
 
 module.exports = BrothersGiftDojo;
