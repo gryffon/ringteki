dd4129872e505c6f9ff5bf87ea5703de84b38e10
Kwaice <kwaice@gmail.com>
2018-10-09 21:32:40 +0200

    match fix

--- server/game/cards/05-UotE/UtakuTetsuko.js
+++ server/game/cards/05-UotE/UtakuTetsuko.js
@@ -8,7 +8,7 @@ class UtakuTetsuko extends DrawCard {
             targetController: 'opponent',
             effect: ability.effects.increaseCost({
                 amount: 1,
-                match: card => card.type === 'event' || 'character' || 'attachment'
+                match: card => ['event', 'character', 'attachment'].includes(card.type)
             })
         });
     }
