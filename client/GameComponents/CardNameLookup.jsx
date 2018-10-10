a9f355d17f4f861ba98f0e66b04ee2536328bdde
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-10-10 13:55:01 +0100

    Update CardNameLookup.jsx

--- client/GameComponents/CardNameLookup.jsx
+++ client/GameComponents/CardNameLookup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Typeahead from '../Form/Typeahead';
+import Typeahead from '../FormComponents/Typeahead.jsx';
 
 class CardNameLookup extends React.Component {
     constructor(props) {
