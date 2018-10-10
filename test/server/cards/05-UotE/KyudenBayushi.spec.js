d76a2412205ec493f2b8cc44b1c4905620dda510
Kwaice <kwaice@gmail.com>
2018-10-10 17:37:23 +0200

    UotE: Kyuden Bayushi

--- test/server/cards/05-UotE/KyudenBayushi.spec.js
+++ test/server/cards/05-UotE/KyudenBayushi.spec.js
@@ -5,25 +5,56 @@ describe('Kyuden Bayushi', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        stronghold: 'kyuden-bayushi',
+                        inPlay: ['bayushi-aramoro']
                     },
                     player2: {
                         inPlay: []
                     }
                 });
-                this.noMoreActions();
+                this.kyuden = this.player1.findCardByName('kyuden-bayushi');
+                this.aramoro = this.player1.findCardByName('bayushi-aramoro');
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should give +1 to both stats if 6 or less honor', function() {
+                this.aramoro.dishonor();
+                this.aramoro.bow();
+                this.player1.honor = 5;
+                this.player1.clickCard(this.kyuden);
+                expect(this.player1).toBeAbleToSelect(this.aramoro);
+                this.player1.clickCard(this.aramoro);
+                expect(this.aramoro.bowed).toBe(false);
+                expect(this.aramoro.militarySkill).toBe(4);
+                expect(this.aramoro.politicalSkill).toBe(1);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('shouldn not give +1 to both stats if more than 6 honor', function() {
+                this.aramoro.dishonor();
+                this.aramoro.bow();
+                this.player1.honor = 7;
+                this.player1.clickCard(this.kyuden);
+                expect(this.player1).toBeAbleToSelect(this.aramoro);
+                this.player1.clickCard(this.aramoro);
+                expect(this.aramoro.bowed).toBe(false);
+                expect(this.aramoro.militarySkill).toBe(3);
+                expect(this.aramoro.politicalSkill).toBe(0);
             });
 
-            it('should have DEF effect on GHI', function() {
+            it('should still give +1 to both stats if 6 or less honor and the character is ready', function() {
+                this.aramoro.dishonor();
+                this.player1.honor = 5;
+                this.player1.clickCard(this.kyuden);
+                expect(this.player1).toBeAbleToSelect(this.aramoro);
+                this.player1.clickCard(this.aramoro);
+                expect(this.aramoro.militarySkill).toBe(4);
+                expect(this.aramoro.politicalSkill).toBe(1);
+            });
 
+            it('should not work if the character is not dishonored', function() {
+                this.aramoro.bow();
+                this.player1.honor = 5;
+                this.player1.clickCard(this.kyuden);
+                expect(this.player1).not.toBeAbleToSelect(this.aramoro);
             });
         });
     });
