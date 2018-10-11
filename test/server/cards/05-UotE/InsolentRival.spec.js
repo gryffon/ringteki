e14950c46889acad3ae73314bed58d2262c81d2e
Kwaice <kwaice@gmail.com>
2018-10-11 19:49:56 +0200

    UotE: Insolent Rival

--- test/server/cards/05-UotE/InsolentRival.spec.js
+++ test/server/cards/05-UotE/InsolentRival.spec.js
@@ -5,25 +5,80 @@ describe('Insolent Rival', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['insolent-rival','bayushi-manipulator']
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['doji-whisperer']
                     }
                 });
+                this.rival = this.player1.findCardByName('insolent-rival');
+                this.doji = this.player2.findCardByName('doji-whisperer');
+            });
+
+            it('should get +2 to both skills if honor bid is higher', function() {
+                this.player1.player.showBid = 5;
+                this.player2.player.showBid = 1;
+                expect(this.rival.militarySkill).toBe(4);
+                expect(this.rival.politicalSkill).toBe(3);
+            });
+
+            it('should not get +2 to both skills if honor bid is lower', function() {
+                this.player1.player.showBid = 1;
+                this.player2.player.showBid = 5;
+                expect(this.rival.militarySkill).toBe(2);
+                expect(this.rival.politicalSkill).toBe(1);
+            });
+
+            it('should not trigger if he is not participating', function() {
                 this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: ['bayushi-manipulator'],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.rival);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should not trigger if there is no defender', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.rival],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.rival);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
+            it('should trigger if he is participating and opponent char too', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.rival],
+                    defenders: ['doji-whisperer']
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.rival);
+                expect(this.player1).toHavePrompt('Insolent Rival');
             });
 
-            it('should have DEF effect on GHI', function() {
-
+            it('should dishonor the loser of the duel', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.rival],
+                    defenders: ['doji-whisperer']
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.rival);
+                expect(this.player1).toBeAbleToSelect('doji-whisperer');
+                this.player1.clickCard('doji-whisperer');
+                this.player1.clickPrompt('5');
+                this.player2.clickPrompt('1');
+                expect(this.doji.isDishonored).toBe(true);
             });
         });
     });
