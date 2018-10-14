884afb00b34d73aacbae6c7ca4826cc80ec93b0f
Kwaice <kwaice@gmail.com>
2018-10-14 15:15:40 +0200

    UotE: Shosuro Takao (#2509)
    * UotE: Shosuro Takao
    
    * Update ShosuroTakao.js

--- server/game/cards/05-UotE/ShosuroTakao.js
+++ server/game/cards/05-UotE/ShosuroTakao.js
@@ -1,10 +1,15 @@
 const DrawCard = require('../../drawcard.js');
 
 class ShosuroTakao extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Move this character into or out of the conflict',
+            condition: () => this.game.isDuringConflict() && this.game.currentConflict.getNumberOfParticipants(card => card.isDishonored) > 0,
+            gameAction: [ability.actions.sendHome(), ability.actions.moveToConflict()]
+        });
     }
 }
 
-ShosuroTakao.id = 'shosuro-takao'; // This is a guess at what the id might be - please check it!!!
+ShosuroTakao.id = 'shosuro-takao';
 
 module.exports = ShosuroTakao;
