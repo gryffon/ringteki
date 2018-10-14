fe4ebaf8392850dda5ff6c592538f181b8ef3fc1
Kwaice <kwaice@gmail.com>
2018-10-14 15:11:38 +0200

    UotE: Kyuden Bayushi (#2521)

--- server/game/cards/05-UotE/KyudenBayushi.js
+++ server/game/cards/05-UotE/KyudenBayushi.js
@@ -1,10 +1,27 @@
 const StrongholdCard = require('../../strongholdcard.js');
 
 class KyudenBayushi extends StrongholdCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Ready a dishonored character',
+            cost: ability.costs.bowSelf(),
+            target: {
+                cardtype: 'character',
+                controller: 'self',
+                cardCondition: card => card.isDishonored,
+                gameAction: [
+                    ability.actions.ready(),
+                    ability.actions.cardLastingEffect(context => ({
+                        target: context.player.honor <= 6 ? context.target : [],
+                        duration: 'untilEndOfPhase',
+                        effect: ability.effects.modifyBothSkills(1)
+                    }))
+                ]
+            }
+        });
     }
 }
 
-KyudenBayushi.id = 'kyuden-bayushi'; // This is a guess at what the id might be - please check it!!!
+KyudenBayushi.id = 'kyuden-bayushi';
 
 module.exports = KyudenBayushi;
