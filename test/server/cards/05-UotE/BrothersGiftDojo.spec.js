8eb2938b75c2abf4529cdf549b48db8d44ed9818
Kwaice <kwaice@gmail.com>
2018-10-14 15:15:51 +0200

    UotE: Brother's Gift Dojo (#2508)
    * UotE: Brother's Gift Dojo
    
    * Removed effect

--- test/server/cards/05-UotE/BrothersGiftDojo.spec.js
+++ test/server/cards/05-UotE/BrothersGiftDojo.spec.js
@@ -1,29 +1,62 @@
-describe('Brother\'s Gift Dojo', function() {
+describe('Brothers Gift Dojo', function() {
     integration(function() {
-        describe('Brother\'s Gift Dojo\'s ability', function() {
+        describe('Brothers Gift Dojo\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['bayushi-liar'],
+                        provinces: ['brother-s-gift-dojo'],
+                        dynastyDiscard: ['favorable-ground'],
+                        honor: 9
                     },
                     player2: {
-                        inPlay: []
+                        inPlay: ['akodo-toturi']
                     }
                 });
+                this.bgd = this.player1.findCardByName('brother-s-gift-dojo');
+                this.bgd.facedown = false;
+                this.fg = this.player1.placeCardInProvince('favorable-ground','province 3');
+                this.liar = this.player1.findCardByName('bayushi-liar');
+
                 this.noMoreActions();
             });
 
-            it('should trigger under XYZ circumstances', function() {
-
+            it('should correctly make the player lose 1 honor', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.liar],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.bgd);
+                expect(this.player1).toBeAbleToSelect(this.liar);
+                this.player1.clickCard(this.liar);
+                expect(this.player1.honor).toBe(8);
+                expect(this.liar.inConflict).toBe(false);
             });
 
-            it('should not trigger under ABC circumstances', function() {
-
-            });
-
-            it('should have DEF effect on GHI', function() {
-
+            it('should be able to trigger twice a round', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.liar],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.bgd);
+                this.player1.clickCard(this.liar);
+                expect(this.player1.honor).toBe(8);
+                expect(this.liar.inConflict).toBe(false);
+                this.player2.pass();
+                this.player1.clickCard(this.fg);
+                this.player1.clickCard(this.liar);
+                expect(this.liar.inConflict).toBe(true);
+                this.player2.pass();
+                this.player1.clickCard(this.bgd);
+                expect(this.player1).toBeAbleToSelect(this.liar);
+                this.player1.clickCard(this.liar);
+                expect(this.liar.inConflict).toBe(false);
+                expect(this.player1.honor).toBe(7);
             });
         });
     });
