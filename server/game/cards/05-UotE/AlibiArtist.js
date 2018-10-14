017f85cad0589862fe57eeb5ee4a2d4079e8724d
Kwaice <kwaice@gmail.com>
2018-10-14 15:12:44 +0200

    UotE: Alibi Artist (#2515)

--- server/game/cards/05-UotE/AlibiArtist.js
+++ server/game/cards/05-UotE/AlibiArtist.js
@@ -1,10 +1,30 @@
 const DrawCard = require('../../drawcard.js');
 
 class AlibiArtist extends DrawCard {
-    setupCardAbilities(ability) { // eslint-disable-line no-unused-vars
+    setupCardAbilities() {
+        this.action({
+            title: 'Look at top 2 cards of conflict deck',
+            condition: context => context.player.honor <= 6,
+            effect: 'look at the top two cards of their conflict deck',
+            handler: context => {
+                if(context.player.conflictDeck.size() === 0) {
+                    return;
+                }
+                this.game.promptWithHandlerMenu(context.player, {
+                    activePromptTitle: 'Choose a card to put in your hand',
+                    context: context,
+                    cards: context.player.conflictDeck.first(2),
+                    cardHandler: card => {
+                        this.game.addMessage('{0} takes one card to their hand and puts the other on the bottom of their deck', context.player);
+                        context.player.moveCard(card, 'hand');
+                        this.game.queueSimpleStep(() => context.player.moveCard(context.player.conflictDeck.first(), 'conflict deck', { bottom: true }));
+                    }
+                });
+            }
+        });
     }
 }
 
-AlibiArtist.id = 'alibi-artist'; // This is a guess at what the id might be - please check it!!!
+AlibiArtist.id = 'alibi-artist';
 
 module.exports = AlibiArtist;
