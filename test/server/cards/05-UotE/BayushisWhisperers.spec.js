ba2102f70e58da549859070a922883d3bc38ff1f
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-10-18 21:05:44 +0100

    Bayushi's Whisperers (tests failing)

--- test/server/cards/05-UotE/BayushisWhisperers.spec.js
+++ test/server/cards/05-UotE/BayushisWhisperers.spec.js
@@ -1,30 +1,95 @@
 describe('Bayushi\'s Whisperers', function() {
-    integration(function() {
-        describe('Bayushi\'s Whisperers\'s ability', function() {
-            beforeEach(function() {
+    integration(function () {
+        describe('Bayushi\'s Whisperers', function () {
+            beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: []
+                        inPlay: ['bayushi-s-whisperers', 'adept-of-shadows'],
+                        hand: ['fine-katana']
                     },
                     player2: {
-                        inPlay: []
                     }
                 });
-                this.noMoreActions();
+
+                this.bayushisWhisperers = this.player1.findCardByName('bayushi-s-whisperers');
+                this.adeptOfShadows = this.player1.findCardByName('adept-of-shadows');
+                this.fineKatana = this.player1.findCardByName('fine-katana');
             });
 
-            it('should trigger under XYZ circumstances', function() {
+            it('should not allow attachments', function () {
+                this.player1.clickCard(this.fineKatana);
+                expect(this.player1).toBeAbleToSelect(this.adeptOfShadows);
+                expect(this.player1).not.toBeAbleToSelect(this.bayushisWhisperers);
+            });
+        });
+
+        describe('Bayushi\'s Whisperers\'s ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['bayushi-s-whisperers', 'adept-of-shadows']
+                    },
+                    player2: {
+                        hand: ['assassination', 'fine-katana']
+                    }
+                });
+
+                this.bayushisWhisperers = this.player1.findCardByName('bayushi-s-whisperers');
+                this.adeptOfShadows = this.player1.findCardByName('adept-of-shadows');
+                this.assassination = this.player2.findCardByName('assassination');
+                this.fineKatana = this.player2.findCardByName('fine-katana');
+            });
+
+            it('should not trigger outside a conflict', function () {
+                this.player1.clickCard(this.bayushisWhisperers);
+                expect(this.player1).toHavePrompt('Initiate an action');
+            });
+
+            describe('when activated', function () {
+                beforeEach(function () {
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        attackers: [this.adeptOfShadows.id],
+                        defenders: []
+                    });
+                    this.player2.pass();
+                    this.spy = spyOn(this.game, 'addMessage');
+                    this.player1.clickCard(this.bayushisWhisperers);
+                });
+
+                it('should reveal the hand', function () {
+                    expect(this.spy).toHaveBeenCalledWith('{0} sees {1}', this.bayushisWhisperers, [this.assassination, this.fineKatana]);
+                });
+
+                it('should prompt to name a card', function () {
+                    expect(this.player1).toHavePrompt('Name a card');
+                    expect(this.player1).toHavePromptButton(this.assassination.name);
+                    expect(this.player1).toHavePromptButton(this.fineKatana.name);
+                });
 
             });
 
-            it('should not trigger under ABC circumstances', function() {
+            describe('if it resolves', function () {
+                beforeEach(function () {
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        attackers: [this.bayushisWhisperers.id],
+                        defenders: []
+                    });
+                    this.player2.pass();
+                    this.player1.clickCard(this.bayushisWhisperers);
+                    this.player1.clickPrompt(this.fineKatana.name);
+                    this.player2.pass();
+                });
 
+                it('opponent should not be able to play copies of named card', function () {
+                    this.player1.clickCard(this.fineKatana);
+                    expect(this.player1).toHavePrompt('Initiate an action');
+                });
             });
 
-            it('should have DEF effect on GHI', function() {
-
-            });
         });
     });
 });
