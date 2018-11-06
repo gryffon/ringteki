14daf93298401943a885bb3f4e600085515f4172
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-11-06 17:35:53 +0000

    Add additional tests for StolenSecrets for 100% coverage

--- test/server/cards/05-UotE/StolenSecrets.spec.js
+++ test/server/cards/05-UotE/StolenSecrets.spec.js
@@ -12,6 +12,7 @@ describe('Stolen Secrets', function() {
                         hand: ['assassination', 'centipede-tattoo', 'mantra-of-fire', 'censure']
                     }
                 });
+
                 this.assassination = this.player2.findCardByName('assassination', 'hand');
                 this.centipedeTattoo = this.player2.findCardByName('centipede-tattoo', 'hand');
                 this.mantraOfFire = this.player2.findCardByName('mantra-of-fire', 'hand');
@@ -63,8 +64,12 @@ describe('Stolen Secrets', function() {
                 });
 
                 it('should not be playable if there are 0 cards in your opponent\'s deck', function () {
-                    this.player2.conflictDeck = [];
+                    for(var i = this.player2.conflictDeck.length - 1; i >= 0; i--) {
+                        this.player2.moveCard(this.player2.conflictDeck[i], 'hand');
+                    }
+                    expect(this.player2.conflictDeck.length).toBe(0);
                     this.player1.clickCard(this.stolenSecrets);
+                    expect(this.player1).toHavePrompt('Conflict Action Window');
                 });
 
                 describe('if costs are paid', function () {
@@ -86,7 +91,7 @@ describe('Stolen Secrets', function() {
                     });
                 });
 
-                describe('if there are 2 or more cards in deck', function () {
+                describe('if there are 4 or more cards in deck', function () {
                     beforeEach(function () {
                         this.player1.clickCard(this.stolenSecrets);
                         this.player1.clickCard(this.favoredNiece);
@@ -103,6 +108,42 @@ describe('Stolen Secrets', function() {
                         expect(this.player1).toHavePromptButton(this.censure.name);
                         expect(this.player1).toHavePromptButton(this.centipedeTattoo.name);
                         this.player1.clickPrompt(this.censure.name);
+                    });
+                });
+
+                describe('if there are exacttly 2 cards in deck', function () {
+                    beforeEach(function () {
+                        for(var i = this.player2.conflictDeck.length - 1; i >= 2; i--) {
+                            this.player2.moveCard(this.player2.conflictDeck[i], 'hand');
+                        }
+                        expect(this.player2.conflictDeck.length).toBe(2);
+                        this.player1.clickCard(this.stolenSecrets);
+                        this.player1.clickCard(this.favoredNiece);
+                        this.player1.clickPrompt(this.censure.name);
+                    });
+
+                    it('should not prompt to place the remaining card', function () {
+                        expect(this.player1).not.toHavePrompt('Which card do you want to be on top?');
+                    });
+                });
+
+                describe('if there are exacttly 3 cards in deck', function () {
+                    beforeEach(function () {
+                        for(var i = this.player2.conflictDeck.length - 1; i >= 3; i--) {
+                            this.player2.moveCard(this.player2.conflictDeck[i], 'hand');
+                        }
+                        expect(this.player2.conflictDeck.length).toBe(3);
+                        this.player1.clickCard(this.stolenSecrets);
+                        this.player1.clickCard(this.favoredNiece);
+                        this.player1.clickPrompt(this.censure.name);
+                    });
+
+                    it('should prompt to place the top card only', function () {
+                        expect(this.player1).toHavePrompt('Which card do you want to be on top?');
+                        expect(this.player1).toHavePromptButton(this.centipedeTattoo.name);
+                        expect(this.player1).toHavePromptButton(this.mantraOfFire.name);
+                        this.player1.clickPrompt(this.mantraOfFire.name);
+                        expect(this.player1).not.toHavePrompt('Which card do you want to be the second card?');
                     });
                 });
 
@@ -127,6 +168,9 @@ describe('Stolen Secrets', function() {
                         this.player2.pass();
                         this.player1.clickCard(this.assassination);
                         expect(this.player1).toHavePrompt('Choose a character');
+                        this.player1.clickCard(this.soshiIllusionist);
+                        expect(this.assassination.location).toBe('conflict discard pile');
+                        expect(this.assassination.controller).toBe(this.player2.player);
                     });
 
                     it('should reorder the remaining cards', function () {
