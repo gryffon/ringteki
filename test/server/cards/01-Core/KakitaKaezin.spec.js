bf05a302fceb3cf732d1bdfaa3098549f2efa199
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-11-08 21:40:49 +0000

    Add tests for Kakita Kaezin
    Remove unneeded condition in duel resolutionHandler

---
+++ test/server/cards/01-Core/KakitaKaezin.spec.js
@@ -0,0 +1,142 @@
+describe('Kakita Kaezin', function () {
+    integration(function () {
+        describe('when a character leaves play during the duel', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        honor: 11,
+                        inPlay: ['kakita-kaezin','doji-whisperer']
+                    },
+                    player2: {
+                        honor: 11,
+                        inPlay: ['obstinate-recruit','akodo-gunso']
+                    }
+                });
+                this.kakitaKaezin = this.player1.findCardByName('kakita-kaezin');
+                this.dojiWhisperer = this.player1.findCardByName('doji-whisperer');
+                this.obstinateRecruit = this.player2.findCardByName('obstinate-recruit');
+                this.akodoGunso = this.player2.findCardByName('akodo-gunso');
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin, this.dojiWhisperer],
+                    defenders: [this.obstinateRecruit, this.akodoGunso]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                this.player2.clickCard(this.obstinateRecruit);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('2');
+            });
+
+            it('the duel should still successfully resolve', function () {
+                expect(this.kakitaKaezin.inConflict).toBe(true);
+                expect(this.dojiWhisperer.inConflict).toBe(false);
+                expect(this.obstinateRecruit.location).toBe('dynasty discard pile');
+                expect(this.akodoGunso.inConflict).toBe(false);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
+        });
+
+        describe('Kakita Kaezin\'s ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['kakita-kaezin', 'doji-whisperer']
+                    },
+                    player2: {
+                        inPlay: ['matsu-berserker', 'akodo-gunso']
+                    }
+                });
+                this.kakitaKaezin = this.player1.findCardByName('kakita-kaezin');
+                this.dojiWhisperer = this.player1.findCardByName('doji-whisperer');
+                this.matsuBerserker = this.player2.findCardByName('matsu-berserker');
+                this.akodoGunso = this.player2.findCardByName('akodo-gunso');
+                
+            });
+
+            it('should not trigger if he is not participating', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.dojiWhisperer],
+                    defenders: [this.matsuBerserker, this.akodoGunso]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should not trigger if there is no defender', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin, this.dojiWhisperer],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should trigger if he is participating as well as an opponent\'s character', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin],
+                    defenders: [this.matsuBerserker]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                expect(this.player2).toHavePrompt('Kakita Kaezin');
+            });
+
+            it('should allow the opponent to choose a target of the duel', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin],
+                    defenders: [this.matsuBerserker, this.akodoGunso]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                expect(this.player2).toHavePrompt('Kakita Kaezin');
+                expect(this.player2).toBeAbleToSelect(this.matsuBerserker);
+                expect(this.player2).toBeAbleToSelect(this.akodoGunso);
+            });
+
+            it('should send all other characters home if Kakita Kaezin wins', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin, this.dojiWhisperer],
+                    defenders: [this.matsuBerserker, this.akodoGunso]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                this.player2.clickCard(this.matsuBerserker);
+                this.player1.clickPrompt('2');
+                this.player2.clickPrompt('1');
+                expect(this.kakitaKaezin.inConflict).toBe(true);
+                expect(this.dojiWhisperer.inConflict).toBe(false);
+                expect(this.matsuBerserker.inConflict).toBe(true);
+                expect(this.akodoGunso.inConflict).toBe(false);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should send Kakita Kaezin home if Kakita Kaezin loses', function () {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.kakitaKaezin, this.dojiWhisperer],
+                    defenders: [this.matsuBerserker, this.akodoGunso]
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                this.player2.clickCard(this.matsuBerserker);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('2');
+                expect(this.kakitaKaezin.inConflict).toBe(false);
+                expect(this.dojiWhisperer.inConflict).toBe(true);
+                expect(this.matsuBerserker.inConflict).toBe(true);
+                expect(this.akodoGunso.inConflict).toBe(true);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
+        });
+    });
+});
