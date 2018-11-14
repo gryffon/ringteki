b3429290373efd13d92c8113551c6cda5df298fc
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-11-14 19:54:24 +0000

    Add implementation and tests for In Defense of Rokugan

---
+++ server/game/cards/06-CotE/InDefenseOfRokugan.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes } = require('../../Constants');
+
+class InDefenseOfRokugan extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'title',
+            condition: () => this.game.isDuringConflict(),
+            cost: ability.costs.sacrifice(card => card.type === CardTypes.Character && card.isDefending()),
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: card => card.isAttacking(),
+                gameAction: ability.actions.cardLastingEffect({
+                    effect: ability.effects.setMilitarySkill(0)
+                })
+            },
+            effect: 'effect'
+        });
+    }
+}
+
+InDefenseOfRokugan.id = 'in-defense-of-rokugan';
+
+module.exports = InDefenseOfRokugan;
