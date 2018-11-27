2b7c5dfec6355d3245d90a23bfeaa214c2b5695a
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-11-27 18:13:18 +0000

    Tests - Tainted Lands (+ some fixes) (#2634)
    * Adjust tests for Asahina Takako to test for faceup cards
    
    Required to achieve 100% branch coverage
    
    * Fix match for Kuni Yori constant ability
    
    * Add tests for Kuni Yori
    
    * Add tests for Press of Battle
    
    * Add tests for Sabotage
    
    * Add tests for Teachings of the Elements
    
    * Fix message in Upholding Authority when mutlitple cards are discarded
    
    * Add additional tests for Upholding Authority
    
    * Add more comprehensive tests for Upholding Authority
    
    * Use slice instead of splice on Upholding Authority
    
    * Upholding Authority checks if cards in hand before trigger
    
    If opponent has zero cards in hand, Upholding Authority should not trigger

--- test/server/cards/04.2-TL/UpholdingAuthority.spec.js
+++ test/server/cards/04.2-TL/UpholdingAuthority.spec.js
@@ -6,7 +6,7 @@ describe('Upholding Authority', function() {
                     phase: 'conflict',
                     player1: {
                         inPlay: ['matsu-berserker'],
-                        hand: ['banzai', 'banzai', 'charge', 'court-games']
+                        hand: ['banzai', 'banzai', 'banzai', 'charge', 'court-games']
                     },
                     player2: {
                         provinces: ['upholding-authority']
@@ -18,6 +18,7 @@ describe('Upholding Authority', function() {
                     defenders: [],
                     jumpTo: 'afterConflict'
                 });
+                this.chat = spyOn(this.game, 'addMessage');
             });
 
             it('should trigger when it is broken', function() {
@@ -50,22 +51,101 @@ describe('Upholding Authority', function() {
 
             it('should ask the player how many cards to discard if the player picks a card with multiple copies', function() {
                 this.upholdingAuthority = this.player2.clickCard('upholding-authority');
-                this.banzai1 = this.player1.findCardByName('banzai');
-                this.player2.clickPrompt('Banzai! (2)');
+                this.player2.clickPrompt('Banzai! (3)');
                 expect(this.player2).toHavePrompt('Choose how many cards to discard');
                 expect(this.player2.currentButtons).toContain('0');
                 expect(this.player2.currentButtons).toContain('1');
                 expect(this.player2.currentButtons).toContain('2');
+                expect(this.player2.currentButtons).toContain('3');
             });
 
-            it('should discard the correct number of cards if the player picks a card with multiple copies', function() {
+            it('should discard the correct number of cards if the player picks a card with multiple copies (0 chosen)', function() {
                 this.upholdingAuthority = this.player2.clickCard('upholding-authority');
-                this.banzai = this.player1.findCardByName('banzai');
-                this.player2.clickPrompt('Banzai! (2)');
-                this.player2.clickPrompt('1');
-                expect(this.banzai.location).toBe('conflict discard pile');
+                this.banzaiCards = this.player1.filterCardsByName('banzai');
+                this.banzai1 = this.banzaiCards[0];
+                this.banzai2 = this.banzaiCards[1];
+                this.banzai3 = this.banzaiCards[2];
+                this.player2.clickPrompt('Banzai! (3)');
+                this.player2.clickPrompt('0');
+                expect(this.banzai1.location).toBe('hand');
+                expect(this.banzai2.location).toBe('hand');
+                expect(this.banzai3.location).toBe('hand');
                 expect(this.player1).toHavePrompt('Break Upholding Authority');
+                expect(this.chat).toHaveBeenCalledWith('{0} chooses not to discard anything', this.player2.player);
+            });
+
+            it('should discard the correct number of cards if the player picks a card with multiple copies (1 chosen)', function() {
+                this.upholdingAuthority = this.player2.clickCard('upholding-authority');
+                this.banzaiCards = this.player1.filterCardsByName('banzai');
+                this.banzai1 = this.banzaiCards[0];
+                this.banzai2 = this.banzaiCards[1];
+                this.banzai3 = this.banzaiCards[2];
+                this.player2.clickPrompt('Banzai! (3)');
+                this.player2.clickPrompt('1');
+                expect(this.banzai1.location).toBe('conflict discard pile');
+                expect(this.banzai2.location).toBe('hand');
+                expect(this.banzai3.location).toBe('hand');
+                expect(this.player1).toHavePrompt('Break Upholding Authority');
+                expect(this.chat).toHaveBeenCalledWith('{0} chooses to discard {1} cop{2} of {3}', this.player2.player, 1, 'y', this.banzai1);
+            });
+
+            it('should discard the correct number of cards if the player picks a card with multiple copies (2 chosen)', function() {
+                this.upholdingAuthority = this.player2.clickCard('upholding-authority');
+                this.banzaiCards = this.player1.filterCardsByName('banzai');
+                this.banzai1 = this.banzaiCards[0];
+                this.banzai2 = this.banzaiCards[1];
+                this.banzai3 = this.banzaiCards[2];
+                this.player2.clickPrompt('Banzai! (3)');
+                this.player2.clickPrompt('2');
+                expect(this.banzai1.location).toBe('conflict discard pile');
+                expect(this.banzai2.location).toBe('conflict discard pile');
+                expect(this.banzai3.location).toBe('hand');
+                expect(this.player1).toHavePrompt('Break Upholding Authority');
+                expect(this.chat).toHaveBeenCalledWith('{0} chooses to discard {1} cop{2} of {3}', this.player2.player, 2, 'ies', this.banzai1);
+            });
+
+            it('should discard the correct number of cards if the player picks a card with multiple copies (3 chosen)', function() {
+                this.upholdingAuthority = this.player2.clickCard('upholding-authority');
+                this.banzaiCards = this.player1.filterCardsByName('banzai');
+                this.banzai1 = this.banzaiCards[0];
+                this.banzai2 = this.banzaiCards[1];
+                this.banzai3 = this.banzaiCards[2];
+                this.player2.clickPrompt('Banzai! (3)');
+                this.player2.clickPrompt('3');
+                expect(this.banzai1.location).toBe('conflict discard pile');
+                expect(this.banzai2.location).toBe('conflict discard pile');
+                expect(this.banzai3.location).toBe('conflict discard pile');
+                expect(this.player1).toHavePrompt('Break Upholding Authority');
+                expect(this.chat).toHaveBeenCalledWith('{0} chooses to discard {1} cop{2} of {3}', this.player2.player, 3, 'ies', this.banzai1);
             });
         });
+
+        describe('Upholding Authority\'s ability when the opponent has zero cards in hand', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['matsu-berserker'],
+                        hand: []
+                    },
+                    player2: {
+                        provinces: ['upholding-authority']
+                    }
+                });
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: ['matsu-berserker'],
+                    defenders: [],
+                    jumpTo: 'afterConflict'
+                });
+            });
+
+            it('should not be able to be triggered', function() {
+                expect(this.player1.player.hand.size()).toBe(0);
+                expect(this.player2).not.toHavePrompt('Triggered Abilities');
+                this.upholdingAuthority = this.player2.clickCard('upholding-authority');
+            });
+        });
+
     });
 });
