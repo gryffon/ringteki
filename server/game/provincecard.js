25bfc0b2441eb09da62b8b772bc38282f502254b
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-11-27 15:10:39 +0000

    Floor province strength at zero

--- server/game/provincecard.js
+++ server/game/provincecard.js
@@ -22,7 +22,8 @@ class ProvinceCard extends BaseCard {
         }
 
         let strength = this.baseStrength + this.sumEffects(EffectNames.ModifyProvinceStrength) + this.getDynastyOrStrongholdCardModifier();
-        return this.getEffects(EffectNames.ModifyProvinceStrengthMultiplier).reduce((total, value) => total * value, strength);
+        strength = this.getEffects(EffectNames.ModifyProvinceStrengthMultiplier).reduce((total, value) => total * value, strength);
+        return Math.max(0, strength);
     }
 
     get baseStrength() {
