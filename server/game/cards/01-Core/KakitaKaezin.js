a2503183fb750f48e0cd981c4bac5a46da0a0928
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-11-30 15:05:46 +0000

    Update KakitaKaezin.js

--- server/game/cards/01-Core/KakitaKaezin.js
+++ server/game/cards/01-Core/KakitaKaezin.js
@@ -24,7 +24,7 @@ class KakitaKaezin extends DrawCard {
         if(winner === context.source) {
             this.game.addMessage('{0} wins the duel, and sends all characters except {0} and {1} home', winner, loser);
             this.game.applyGameAction(context, { sendHome: this.game.currentConflict.getParticipants(card => ![winner, loser].includes(card)) });
-        } else {
+        } else if(loser === context.source) {
             this.game.addMessage('{0} loses the duel, and is sent home', loser);
             this.game.applyGameAction(context, { sendHome: loser });
         }
