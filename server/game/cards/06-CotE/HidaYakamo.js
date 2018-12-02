70f9fb25f1ca6d1c06137a55e58996a13cba9201
jeremylarner <jeremyaroundtheworld@gmail.com>
2018-12-02 05:38:40 +0000

    Hida Yakamo
    needs tests

---
+++ server/game/cards/06-CotE/HidaYakamo.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class HidaYakamo extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: context => context.player.opponent && context.player.honor < context.player.opponent.honor,
+            effect: ability.effects.cardCannot('loseDuels')
+        });
+
+        this.persistentEffect({
+            condition: context => 
+                context.player.opponent && context.player.honor < context.player.opponent.honor &&
+                this.game.isDuringConflict('military'),
+            effect: ability.effects.doesNotBow()
+        });
+    }
+}
+
+HidaYakamo.id = 'hida-yakamo';
+
+module.exports = HidaYakamo;
