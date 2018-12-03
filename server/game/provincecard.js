24f682a0edd19cbbebab6e2f5a2c1b0ab1b6cb79
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2018-12-03 20:53:18 +0000

    fix bug with province refills (#2638)

--- server/game/provincecard.js
+++ server/game/provincecard.js
@@ -79,7 +79,6 @@ class ProvinceCard extends BaseCard {
                             () => {
                                 this.game.addMessage('{0} chooses to discard {1}', this.controller.opponent, dynastyCard.facedown ? 'the facedown card' : dynastyCard);
                                 this.game.applyGameAction(this.game.getFrameworkContext(), { discardCard: dynastyCard });
-                                this.game.queueSimpleStep(() => this.controller.replaceDynastyCard(this.location));
                             },
                             () => this.game.addMessage('{0} chooses not to discard {1}', this.controller.opponent, dynastyCard.facedown ? 'the facedown card' : dynastyCard)
                         ]
