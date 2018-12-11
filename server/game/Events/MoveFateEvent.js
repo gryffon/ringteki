11c1892b465f570070f9aa729fe13af8134e18ed
jeremylarner <jeremyaroundtheworld@gmail.com>
2018-12-11 18:35:02 +0000

    Bugfix for Feast or Famine/Embrace the Void interaction
    Also cleans up Karmic Twist

--- server/game/Events/MoveFateEvent.js
+++ server/game/Events/MoveFateEvent.js
@@ -1,4 +1,5 @@
 const Event = require('./Event.js');
+const { CardTypes } = require('../Constants');
 
 class MoveFateEvent extends Event {
     constructor(params, fate, origin, recipient, gameAction) {
@@ -8,6 +9,23 @@ class MoveFateEvent extends Event {
         this.recipient = recipient;
         this.fate = fate;
         this.gameAction = gameAction;
+    }
+
+    checkCondition() {
+        if(this.cancelled || this.resolved) {
+            return;
+        }
+
+        if(this.origin) {
+            if(this.origin.fate === 0 || this.origin === CardTypes.Character && !this.origin.allowGameAction('removeFate', this.context)) {
+                this.cancel();
+                return;
+            }
+        }
+
+        if(this.recipient && this.recipient === CardTypes.Character && !this.recipient.allowGameAction('placeFate', this.context)) {
+            this.cancel();
+        }
     }
 
     moveFate() {
