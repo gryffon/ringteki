b51256669f4eee30433a3468da4fff33f1aee310
James Oddy <james.oddy@adeysteelgroup.co.uk>
2018-12-13 18:55:20 +0000

    Add implementation and tests for Iuchi Daiyu

---
+++ server/game/cards/07-WotW/IuchiDaiyu.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+
+class IuchiDaiyu extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: '+1 military for each faceup province',
+            condition: () => this.game.isDuringConflict(),
+            target: {
+                gameAction: ability.actions.cardLastingEffect(context => ({
+                    effect: ability.effects.modifyMilitarySkill(context.player.opponent.getNumberOfFaceupProvinces())
+                }))
+            },
+            effect: 'give {0} +1{1} for each faceup non-stronghold province their opponent controls',
+            effectArgs: () => ['military']
+        });
+    }
+}
+
+IuchiDaiyu.id = 'iuchi-daiyu';
+
+module.exports = IuchiDaiyu;
