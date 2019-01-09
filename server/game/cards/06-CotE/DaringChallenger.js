c890098e1b8edf2e61141d620a9da81931dda645
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-01-09 16:25:42 +0000

    CotE - Daring Challenger (#2759)
    * Add implementation and tests for Daring Challenger
    
    * Update DaringChallenger.js

---
+++ server/game/cards/06-CotE/DaringChallenger.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class DaringChallenger extends DrawCard {
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: context => context.player.opponent && context.player.honor < context.player.opponent.honor,
+            effect: AbilityDsl.effects.modifyMilitarySkill(1)
+        });
+
+        this.action({
+            title: 'Initiate a Military duel',
+            initiateDuel: context => ({
+                type: 'military',
+                resolutionHandler: winner => this.resolutionHandler(context, winner)
+            })
+        });
+    }
+    resolutionHandler(context, winner) {
+        this.game.addMessage('1 fate is placed on {0} due to winning the duel', winner);
+        this.game.actions.placeFate().resolve(winner, context);
+    }
+}
+
+DaringChallenger.id = 'daring-challenger';
+
+module.exports = DaringChallenger;
