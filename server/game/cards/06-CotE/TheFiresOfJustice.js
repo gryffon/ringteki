0970cdb4dfad668c14da43ce9961ef4c10a27194
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-01-09 08:01:09 +0000

    Rework The Fires of Justice

--- server/game/cards/06-CotE/TheFiresOfJustice.js
+++ server/game/cards/06-CotE/TheFiresOfJustice.js
@@ -2,7 +2,7 @@ const DrawCard = require('../../drawcard.js');
 const { CardTypes, Players } = require('../../Constants');
 
 class TheFiresOfJustice extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.reaction({
             title: 'Remove fate or move fate to a character',
             when: {
@@ -10,24 +10,68 @@ class TheFiresOfJustice extends DrawCard {
             },
             target: {
                 activePromptTitle: 'Choose a character',
-                cardType: CardTypes.Character,
+                cardCondition: card => card.isParticipating() && (card.controller.fate !== 0 || card.fate > 0),
                 player: Players.Opponent,
-                controller: Players.Opponent,
-                cardCondition: card => card.isParticipating(),
-                gameAction: ability.actions.chooseAction(context => ({
-                    messages: {
-                        'Remove all fate': '{0} chooses to remove all fate from {1}'
-                    },
-                    choices: {
-                        'Remove all fate': ability.actions.removeFate({ amount: context.target.fate }),
-                        'Move fate to character': ability.actions.placeFate({
-                            origin: context.player.opponent,
-                            amount: context.player.opponent.fate,
-                            promptForAmount: true
-                        })
-                    }
-                }))
-            }
+                controller: Players.Opponent
+            },
+            handler: context => this.promptToChooseRemoveOrMove(context.target, context)
+        });
+    }
+
+    canPlay(context, playType) {
+        let anyOpponentsCharactersParticipatingWithFate = context.player.opponent.cardsInPlay.any(card => card.getType() === CardTypes.Character && card.isParticipating() && card.fate > 0);
+        let anyFateInOpponentsFatePool = context.player.opponent.fate > 0;
+
+        if(!anyOpponentsCharactersParticipatingWithFate && !anyFateInOpponentsFatePool) {
+            return false;
+        }
+
+        return super.canPlay(context, playType);
+    }
+
+    promptToChooseRemoveOrMove(card, context) {
+        let choices = [];
+        if(context.player.opponent.fate !== 0) {
+            choices.push('Move fate to character');
+        }
+        if(context.target.fate !== 0) {
+            choices.push('Remove all fate');
+        }
+        let handlers = choices.map(choice => {
+            return () => this.applyChoice(choice, card, context);
+        });
+        this.game.promptWithHandlerMenu(context.player, {
+            activePromptTitle: 'Select an effect:',
+            context: context,
+            choices: choices,
+            handlers: handlers
+        });
+    }
+
+    applyChoice(choice, card, context) {
+        if(choice === 'Remove all fate') {
+            let amount = card.fate;
+            this.game.actions.removeFate({ amount: amount }).resolve(card, context);
+            this.game.addMessage('{0} chooses to remove all fate ({1}) from {2}', context.player, amount, card);
+        }
+        if(choice === 'Move fate to character') {
+            this.promptForFateAmount(card, context);
+        }
+    }
+
+    promptForFateAmount(card, context) {
+        let choices = Array.from(Array(context.player.opponent.fate), (x, i) => (i + 1).toString());
+        let handlers = choices.map(choice => {
+            return () => {
+                this.game.actions.placeFate({ origin: context.player.opponent, amount: parseInt(choice) }).resolve(card, context);
+                this.game.addMessage('{0} chooses to take {1} fate from {2} and place it on {3}', context.player, choice, context.player.opponent, card);
+            };
+        });
+        this.game.promptWithHandlerMenu(context.player, {
+            activePromptTitle: 'Select fate amount:',
+            context: context,
+            choices: choices,
+            handlers: handlers
         });
     }
 }
--- test/server/cards/06-CotE/TheFiresOfJustice.spec.js
+++ test/server/cards/06-CotE/TheFiresOfJustice.spec.js
@@ -10,7 +10,7 @@ describe('The Fires of Justice', function() {
                     },
                     player2: {
                         inPlay: ['doji-whisperer', 'doji-challenger', 'guest-of-honor'],
-                        fate: 4
+                        fate: 10
                     }
                 });
 
@@ -22,28 +22,72 @@ describe('The Fires of Justice', function() {
                 this.guestOfHonor = this.player2.findCardByName('guest-of-honor');
 
                 this.theFiresOfJustice = this.player1.findCardByName('the-fires-of-justice');
+            });
 
+            it('should trigger after you win a military conflict', function() {
                 this.noMoreActions();
                 this.initiateConflict({
                     attackers: [this.matsuBerserker],
                     defenders: [this.dojiWhisperer, this.dojiChallenger]
                 });
-            });
-
-            it('should trigger after you win a military conflict', function() {
                 this.noMoreActions();
                 expect(this.player1).toHavePrompt('Triggered Abilities');
                 expect(this.player1).toBeAbleToSelect(this.theFiresOfJustice);
             });
 
-            describe('if triggered', function() {
+            it('should not trigger if there are no participating characters with fate and opponent has 0 fate', function() {
+                this.player2.fate = 0;
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.matsuBerserker],
+                    defenders: [this.dojiWhisperer]
+                });
+                this.noMoreActions();
+                expect(this.player1).not.toHavePrompt('Triggered Abilities');
+                expect(this.player1).toHavePrompt('Break Shameful Display');
+            });
+
+            describe('if triggered when opponent has no fate', function() {
                 beforeEach(function() {
+                    this.player2.fate = 0;
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        attackers: [this.matsuBerserker],
+                        defenders: [this.dojiWhisperer, this.dojiChallenger]
+                    });
+                    this.noMoreActions();
+                    this.player1.clickCard(this.theFiresOfJustice);
+                });
+
+                it('should prompt your opponent to choose a participating character they control with fate', function() {
+                    expect(this.player2).toHavePrompt('The Fires of Justice');
+                    expect(this.player2).toBeAbleToSelect(this.dojiChallenger);
+                    expect(this.player2).not.toBeAbleToSelect(this.dojiWhisperer);
+                    expect(this.player2).not.toBeAbleToSelect(this.guestOfHonor);
+                    expect(this.player2).not.toBeAbleToSelect(this.matsuBerserker);
+                });
+
+                it('should just prompt to remove all fate', function() {
+                    this.player2.clickCard(this.dojiChallenger);
+                    expect(this.player1).toHavePrompt('Select an effect:');
+                    expect(this.player1).toHavePromptButton('Remove all fate');
+                    expect(this.player1).not.toHavePromptButton('Move fate to character');
+                });
+            });
+
+            describe('if triggered when opponent has fate', function() {
+                beforeEach(function() {
+                    this.noMoreActions();
+                    this.initiateConflict({
+                        attackers: [this.matsuBerserker],
+                        defenders: [this.dojiWhisperer, this.dojiChallenger]
+                    });
                     this.noMoreActions();
                     this.player1.clickCard(this.theFiresOfJustice);
                 });
 
                 it('should prompt your opponent to choose a participating character they control', function() {
-                    expect(this.player2).toHavePrompt('THe Fires of Justice');
+                    expect(this.player2).toHavePrompt('The Fires of Justice');
                     expect(this.player2).toBeAbleToSelect(this.dojiChallenger);
                     expect(this.player2).toBeAbleToSelect(this.dojiWhisperer);
                     expect(this.player2).not.toBeAbleToSelect(this.guestOfHonor);
@@ -52,8 +96,15 @@ describe('The Fires of Justice', function() {
 
                 it('should prompt to either remove all fate or move fate to the character', function() {
                     this.player2.clickCard(this.dojiChallenger);
-                    expect(this.player1).toHavePrompt('Select an action:');
+                    expect(this.player1).toHavePrompt('Select an effect:');
                     expect(this.player1).toHavePromptButton('Remove all fate');
+                    expect(this.player1).toHavePromptButton('Move fate to character');
+                });
+
+                it('should prompt to just move fate to the character if a character with no fate is chosen', function() {
+                    this.player2.clickCard(this.dojiWhisperer);
+                    expect(this.player1).toHavePrompt('Select an effect:');
+                    expect(this.player1).not.toHavePromptButton('Remove all fate');
                     expect(this.player1).toHavePromptButton('Move fate to character');
                 });
 
@@ -63,27 +114,30 @@ describe('The Fires of Justice', function() {
                     expect(this.dojiChallenger.fate).toBe(0);
                 });
 
-                it(', if \'Move fate to character\' chosen, should prompt to choose the amount of fate', function() {
+                it('if \'Move fate to character\' chosen, should prompt to choose the amount of fate', function() {
                     this.player2.clickCard(this.dojiChallenger);
                     this.player1.clickPrompt('Move fate to character');
-                    expect(this.player1).toHavePrompt('Choose fate amount');
+                    expect(this.player1).toHavePrompt('Select fate amount:');
                     expect(this.player1).not.toHavePromptButton('0');
                     expect(this.player1).toHavePromptButton('1');
                     expect(this.player1).toHavePromptButton('2');
                     expect(this.player1).toHavePromptButton('3');
-                    expect(this.player1).toHavePromptButton('More');
-                    this.player1.clickPrompt('More');
                     expect(this.player1).toHavePromptButton('4');
-                    expect(this.player1).not.toHavePromptButton('5');
+                    expect(this.player1).toHavePromptButton('5');
+                    expect(this.player1).toHavePromptButton('6');
+                    expect(this.player1).toHavePromptButton('7');
+                    expect(this.player1).toHavePromptButton('8');
+                    expect(this.player1).toHavePromptButton('9');
+                    expect(this.player1).toHavePromptButton('10');
+                    expect(this.player1).not.toHavePromptButton('11');
                 });
 
-                it(', if \'Move fate to character\' chosen, should move the chosen amount of fate', function() {
+                it('if \'Move fate to character\' chosen, should move the chosen amount of fate', function() {
                     this.player2.clickCard(this.dojiChallenger);
                     this.player1.clickPrompt('Move fate to character');
-                    this.player1.clickPrompt('More');
-                    this.player1.clickPrompt('4');
-                    expect(this.dojiChallenger.fate).toBe(6);
-                    expect(this.player2.player.fate).toBe(0);
+                    this.player1.clickPrompt('6');
+                    expect(this.dojiChallenger.fate).toBe(8);
+                    expect(this.player2.player.fate).toBe(4);
                 });
             });
 
