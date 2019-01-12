fc830c6ee1b64bd7bb8f96117a855b4283102c72
Kwaice <kwaice@gmail.com>
2019-01-12 15:51:28 +0100

    CotE: Gift of Amaterasu
    Closes #2717

---
+++ server/game/cards/06-CotE/GiftOfAmaterasu.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+const { Players, CardTypes } = require('../../Constants');
+
+class GiftofAmaterasu extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Honor a character',
+            when: {
+                afterConflict: (event, context) => event.conflict.winner === context.player && event.conflict.skillDifference >= 5
+            },
+            target: {
+                cardType: CardTypes.Character,
+                activePromptTitle: 'Choose a character to honor',
+                controller: Players.Self,
+                gameAction: AbilityDsl.actions.honor()
+            }
+        });
+    }
+}
+
+GiftofAmaterasu.id = 'gift-of-amaterasu';
+
+module.exports = GiftofAmaterasu;
