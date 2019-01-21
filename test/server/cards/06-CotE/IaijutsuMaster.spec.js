6841e68b0b3060d752d5a30b54f0acccc5dc5cb4
Kwaice <kwaice@gmail.com>
2019-01-21 20:49:31 +0100

    CotE: Iaijutsu Master

---
+++ test/server/cards/06-CotE/IaijutsuMaster.spec.js
@@ -0,0 +1,70 @@
+describe('Iaijutsu Master', function() {
+    integration(function() {
+        describe('During a duel,', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        honor: 11,
+                        inPlay: ['mirumoto-raitsugu']
+                    },
+                    player2: {
+                        honor: 11,
+                        inPlay: ['doji-challenger','doji-whisperer'],
+                        hand: ['iaijutsu-master']
+                    }
+                });
+                this.raitsugu = this.player1.findCardByName('mirumoto-raitsugu');
+
+                this.challenger = this.player2.findCardByName('doji-challenger');
+                this.iaijutsu = this.player2.findCardByName('iaijutsu-master');
+                this.player1.pass();
+                this.player2.clickCard(this.iaijutsu);
+                expect(this.player2).toBeAbleToSelect(this.challenger);
+                expect(this.player2).not.toBeAbleToSelect('doji-whisperer');
+                this.player2.clickCard(this.challenger);
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.raitsugu],
+                    defenders: [this.challenger]
+                });
+            });
+
+            it('should trigger after dials are revealed', function() {
+                this.player2.pass();
+                this.player1.clickCard(this.raitsugu);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickPrompt('2');
+                this.player2.clickPrompt('2');
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+            });
+
+            it('should be able to change dial value to 0', function() {
+                this.player2.pass();
+                this.player1.clickCard(this.raitsugu);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+                this.player2.clickCard(this.iaijutsu);
+                expect(this.player2).toHavePrompt('Select one');
+                expect(this.player2.currentButtons).toContain('Increase honor bid','Decrease honor bid');
+                this.player2.clickPrompt('Decrease honor bid');
+                expect(this.player2.honor).toBe(12);
+                expect(this.player1.honor).toBe(10);
+            });
+
+            it('should correctly modify the value', function() {
+                this.player2.pass();
+                this.player1.clickCard(this.raitsugu);
+                this.player1.clickCard(this.challenger);
+                this.player1.clickPrompt('2');
+                this.player2.clickPrompt('1');
+                this.player2.clickCard(this.iaijutsu);
+                this.player2.clickPrompt('Increase honor bid');
+                expect(this.raitsugu.location).toBe('dynasty discard pile');
+                expect(this.player2.honor).toBe(11);
+                expect(this.player1.honor).toBe(11);
+            });
+        });
+    });
+});
