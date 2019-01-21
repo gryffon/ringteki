7ab46d13aa73162939d9dabebd310a31c7005a5c
Kwaice <kwaice@gmail.com>
2019-01-21 21:01:53 +0100

    Test update

--- test/server/cards/06-CotE/IaijutsuMaster.spec.js
+++ test/server/cards/06-CotE/IaijutsuMaster.spec.js
@@ -1,5 +1,33 @@
 describe('Iaijutsu Master', function() {
     integration(function() {
+        describe('During the draw phase,', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'draw',
+                    player1: {
+                        honor: 11,
+                        inPlay: ['mirumoto-raitsugu']
+                    },
+                    player2: {
+                        honor: 11,
+                        inPlay: ['doji-challenger','doji-whisperer'],
+                        hand: ['iaijutsu-master']
+                    }
+                });
+                this.raitsugu = this.player1.findCardByName('mirumoto-raitsugu');
+
+                this.challenger = this.player2.findCardByName('doji-challenger');
+                this.iaijutsu = this.player2.playAttachment('iaijutsu-master', this.challenger);
+
+            });
+            it('should not trigger', function() {
+                this.player1.clickPrompt('3');
+                this.player2.clickPrompt('1');
+                expect(this.player2).not.toHavePrompt('Triggered Abilities');
+                expect(this.player2).not.toBeAbleToSelect(this.iaijutsu);
+            });
+        });
+
         describe('During a duel,', function() {
             beforeEach(function() {
                 this.setupTest({
@@ -37,6 +65,7 @@ describe('Iaijutsu Master', function() {
                 this.player1.clickPrompt('2');
                 this.player2.clickPrompt('2');
                 expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.iaijutsu);
             });
 
             it('should be able to change dial value to 0', function() {
