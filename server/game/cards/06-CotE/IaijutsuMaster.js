d1a0dcc053540a9235d14d49764a2d8a55ae7fa3
Kwaice <kwaice@gmail.com>
2019-01-23 21:01:52 +0100

    CotE: Iaijutsu Master (#2796)
    * CotE: Iaijutsu Master
    
    * Test update
    
    * Update IaijutsuMaster.js

---
+++ server/game/cards/06-CotE/IaijutsuMaster.js
@@ -0,0 +1,25 @@
+const DrawCard = require('../../drawcard.js');
+
+class IaijutsuMaster extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Change your bid by 1 during a duel',
+            when: {
+                onHonorDialsRevealed: (event, context) => this.game.currentDuel && this.game.currentDuel.isInvolved(context.source.parent)
+            },
+            gameAction: ability.actions.modifyBid({ direction: 'prompt' })
+        });
+    }
+
+    canAttach(card, context) {
+        if(!card.hasTrait('duelist')) {
+            return false;
+        }
+
+        return super.canAttach(card, context);
+    }
+}
+
+IaijutsuMaster.id = 'iaijutsu-master';
+
+module.exports = IaijutsuMaster;
