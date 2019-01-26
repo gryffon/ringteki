9c82704bedf34590c456aba3c148b79434f886a9
Matthew Griffith <matthew-griffith@protonmail.com>
2019-01-25 20:00:53 -0500

    forgot to the match on the persistnetEffects object for the card

--- server/game/cards/06-CotE/UnveiledDestiny.js
+++ server/game/cards/06-CotE/UnveiledDestiny.js
@@ -5,6 +5,7 @@ class UnveiledDestiny extends DrawCard {
     setupCardAbilities() {
         this.persistentEffect({
             condition: context => context.source.parent.isAttacking() && context.player.role.getElement(),
+            match: ring => ring.contested,
             effect: context => context.player.role.getElement().forEach(element => AbilityDsl.effects.addElement(element))
         });
     }
