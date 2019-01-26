7e782764974e69a69bb9701aaade19b3989ab103
Matthew Griffith <matthew-griffith@protonmail.com>
2019-01-25 20:14:34 -0500

    I thought that with an and statment it would evaluate an empty array as false and a non-empty array as true but thats not how and works

--- server/game/cards/06-CotE/UnveiledDestiny.js
+++ server/game/cards/06-CotE/UnveiledDestiny.js
@@ -4,7 +4,7 @@ const AbilityDsl = require('../../abilitydsl.js');
 class UnveiledDestiny extends DrawCard {
     setupCardAbilities() {
         this.persistentEffect({
-            condition: context => context.source.parent.isAttacking() && context.player.role.getElement(),
+            condition: context => context.source.parent.isAttacking() && context.player.role.getElement() !== [],
             match: ring => ring.contested,
             effect: context => context.player.role.getElement().forEach(element => AbilityDsl.effects.addElement(element))
         });
