ecfbed3d2bf2ce2f870eeb3f31c42c5338c613d1
Matthew-Griffith <matthew-griffith@protonmail.com>
2019-02-03 13:06:44 -0500

    CotE - Seppun hidden guard (#2828)
    * first implementation of seppun hidden guard, along with a test for when the ability should and shouldnt work
    
    * used context.game.applyGameAction to force the opponent to discard so now thats working
    
    * changed the random discard incase you cancel your own effect
    
    * added some new test, one to show she doesnt trigger on non-unique character and the other to cancel your own abilities, the 2nd is failing
    
    * changed fdescribe back to describe
    
    * changed player settings so they could cancel their own abilities
    
    * Update SeppunHiddenGuard.js

---
+++ server/game/cards/06-CotE/SeppunHiddenGuard.js
@@ -0,0 +1,27 @@
+const DrawCard = require('../../drawcard.js');
+const { Locations, CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class SeppunHiddenGuard extends DrawCard {
+    setupCardAbilities() {
+        this.wouldInterrupt({
+            title: 'Cancel ability',
+            when: {
+                onInitiateAbilityEffects: (event, context) => event.card.type === CardTypes.Character && event.cardTargets.some(card => (
+                    card.isUnique() && card.controller === context.player && card.location === Locations.PlayArea)
+                )
+            },
+            cost: AbilityDsl.costs.sacrificeSelf(),
+            effect: 'cancel the effects of {1}',
+            effectArgs: context => context.event.card,
+            handler: context => {
+                context.cancel();
+                context.game.applyGameAction(context, { discardAtRandom: context.event.context.player });
+            }
+        });
+    }
+}
+
+SeppunHiddenGuard.id = 'seppun-hidden-guard';
+
+module.exports = SeppunHiddenGuard;
