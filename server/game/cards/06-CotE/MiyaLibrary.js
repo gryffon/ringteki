853cecd092ca78a020fac3ae33da61cc5ae95e93
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-03 17:59:38 -0500

    took the prompt function from togashi menderict for this card

--- server/game/cards/06-CotE/MiyaLibrary.js
+++ server/game/cards/06-CotE/MiyaLibrary.js
@@ -9,7 +9,26 @@ class MiyaLibrary extends DrawCard {
             condition: context => context.player.dynastyDeck.size() > 0,
             effect: 'Search the top five card for your dynasty deck for an imperial character', 
             handler: context => {
-                
+                // let choices = 
+            }
+        });
+    }
+
+    miyaLibraryPrompt(context, promptCards, orderedCards, promptTitle) {
+        this.game.promptWithHandlerMenu(context.player, {
+            activePromptTitle: promptTitle,
+            context: context,
+            cards: promptCards,
+            cardHandler: card => {
+                orderedCards.push(card);
+                promptCards = promptCards.filter(c => c !== card);
+                if(promptCards.length > 1) {
+                    this.togashiMendicantPrompt(context, promptCards, orderedCards, 'Which card do you want to be the next card?');
+                    return;
+                } else if(promptCards.length === 1) {
+                    orderedCards.push(promptCards[0]);
+                }
+                context.player.dynastyDeck.splice(0, 5, ...orderedCards);
             }
         });
     }
