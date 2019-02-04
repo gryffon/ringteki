904e2c0a53f4c6a3ea28c44cfc70492800aa864d
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-03 23:38:00 -0500

    the card is starting to come together but it seem that miya library is not an option to put back

--- server/game/cards/06-CotE/MiyaLibrary.js
+++ server/game/cards/06-CotE/MiyaLibrary.js
@@ -1,15 +1,27 @@
 const DrawCard = require('../../drawcard.js');
-const { CardTypes } = require('../../Constants');
-const AbilitDsl = require('../../abilitydsl');
+// const { CardTypes } = require('../../Constants');
+// const AbilitDsl = require('../../abilitydsl');
 
 class MiyaLibrary extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Replace Miya Library for a faceup imperial character',
             condition: context => context.player.dynastyDeck.size() > 0,
-            effect: 'Search the top five card for your dynasty deck for an imperial character', 
+            effect: 'Search the top five card for your dynasty deck for an imperial character',
             handler: context => {
-                // let choices = 
+                this.game.promptWithHandlerMenu(context.player, {
+                    activePromptTitle: 'select an imperial character to replace miya library',
+                    context: context,
+                    cards: context.player.dynastyDeck.first(5),
+                    cardHandler: (card) => {
+                        let choices = context.player.dynastyDeck.first(5);
+                        context.player.moveCard(card, context.source.location);
+                        card.facedown = false;
+                        choices.splice(choices.indexOf(card), 1);
+                        choices.push(context.source);
+                        this.miyaLibraryPrompt(context, choices, [], 'Select the card you would like to place on top of your dynasty deck');
+                    }
+                });
             }
         });
     }
@@ -23,7 +35,7 @@ class MiyaLibrary extends DrawCard {
                 orderedCards.push(card);
                 promptCards = promptCards.filter(c => c !== card);
                 if(promptCards.length > 1) {
-                    this.togashiMendicantPrompt(context, promptCards, orderedCards, 'Which card do you want to be the next card?');
+                    this.miyaLibraryPrompt(context, promptCards, orderedCards, 'Which card would you like to put back now');
                     return;
                 } else if(promptCards.length === 1) {
                     orderedCards.push(promptCards[0]);
--- test/server/cards/06-CotE/MiyaLibrary.spec.js
+++ test/server/cards/06-CotE/MiyaLibrary.spec.js
@@ -5,33 +5,38 @@ fdescribe('Miya Library', function () {
                 this.setupTest({
                     phase: 'dynasty',
                     player1: {
-                        inPlay: [],
-                        dynastyDeck: ['kanjo-district', 'shiba-tsukune', 'miya-satoshi', 'adept-of-the-waves', 'kudaka']
+                        inPlay: ['miya-library', 'kanjo-district', 'miya-mystic', 'miya-satoshi', 'adept-of-the-waves', 'kudaka']
                     },
                     player2: {
                     }
                 });
-                this.miyaLibrary = this.player1.placeCardInProvince('miya-library', 'province 1');
+                this.miyaLibrary = this.player1.findCardByName('miya-library');
+                this.player1.placeCardInProvince('miya-library', 'province 1');
+                this.player1.moveCard('kanjo-district', 'dynasty deck');
+                this.player1.moveCard('miya-mystic', 'dynasty deck');
+                this.player1.moveCard('miya-satoshi', 'dynasty deck');
+                this.player1.moveCard('adept-of-the-waves', 'dynasty deck');
+                this.player1.moveCard('kudaka', 'dynasty deck');
 
             });
 
             it('should allow you to replace the library for satoshi', function () {
-                this.player1.clicksCard(this.miyaLibrary);
-                expect(this.player1).toHavePrompt('select an imperial character to replace miya library.');
-                this.player1.clicksCard('miya-satoshi');
+                this.player1.clickCard(this.miyaLibrary);
+                expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
+                this.player1.clickPrompt('Miya Satoshi');
+                expect(this.player1).toHavePrompt('Select the card you would like to place on top of your dynasty deck');
+                this.player1.clickPrompt('Kanjo District');
                 expect(this.player1).toHavePrompt('Which card would you like to put back now');
-                this.player1.clicksCard('kanjo-district');
+                this.player1.clickPrompt('Miya Mystic');
                 expect(this.player1).toHavePrompt('Which card would you like to put back now');
-                this.player1.clicksCard('shiba-tsukune');
+                this.player1.clickPrompt('Adept of the Waves');
                 expect(this.player1).toHavePrompt('Which card would you like to put back now');
-                this.player1.clicksCard('adept-of-the-waves');
+                this.player1.clickPrompt('Kudaka');
                 expect(this.player1).toHavePrompt('Which card would you like to put back now');
-                this.player1.clicksCard('kudaka');
-                expect(this.player1).toHavePrompt('Which card would you like to put back now');
-                this.player1.clicksCard(this.miyaLibrary);
+                this.player1.clickPrompt(this.miyaLibrary);
                 expect(this.player2).toHavePrompt('Dynasty Action Window');
                 this.player2.pass();
-                this.player1.clicksCard('miya-satoshi');
+                this.player1.clickCard('miya-satoshi');
             });
         });
     });
