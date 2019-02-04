c3e80e07509efb68d608812d9fd5ae8f2fd5d96e
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-04 09:52:45 -0500

    changes the number of card to 4 and add a check for imperial and chararcter

--- server/game/cards/06-CotE/MiyaLibrary.js
+++ server/game/cards/06-CotE/MiyaLibrary.js
@@ -1,5 +1,5 @@
 const DrawCard = require('../../drawcard.js');
-// const { CardTypes } = require('../../Constants');
+const { CardTypes } = require('../../Constants');
 // const AbilitDsl = require('../../abilitydsl');
 
 class MiyaLibrary extends DrawCard {
@@ -7,18 +7,20 @@ class MiyaLibrary extends DrawCard {
         this.action({
             title: 'Replace Miya Library for a faceup imperial character',
             condition: context => context.player.dynastyDeck.size() > 0,
-            effect: 'Search the top five card for your dynasty deck for an imperial character',
+            effect: 'Search the top four card for your dynasty deck for an imperial character',
             handler: context => {
                 this.game.promptWithHandlerMenu(context.player, {
                     activePromptTitle: 'select an imperial character to replace miya library',
                     context: context,
-                    cards: context.player.dynastyDeck.first(5),
+                    cards: context.player.dynastyDeck.first(4),
                     cardHandler: (card) => {
-                        let choices = context.player.dynastyDeck.first(5);
-                        context.player.moveCard(card, context.source.location);
-                        card.facedown = false;
-                        choices.splice(choices.indexOf(card), 1);
-                        choices.push(context.source);
+                        let choices = context.player.dynastyDeck.first(4);
+                        if(card.hasTrait('imperial') && card.getType() === CardTypes.Character) {
+                            context.player.moveCard(card, context.source.location);
+                            card.facedown = false;
+                            choices.splice(choices.indexOf(card), 1);
+                            choices.push(context.source);
+                        }
                         this.miyaLibraryPrompt(context, choices, [], 'Select the card you would like to place on top of your dynasty deck');
                     }
                 });
