e9cd423d8e87038b7bf43be7d6471a247adfcd26
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-05 10:45:55 -0500

    new test show that miya library is not switching with the selected card

--- test/server/cards/06-CotE/MiyaLibrary.spec.js
+++ test/server/cards/06-CotE/MiyaLibrary.spec.js
@@ -30,6 +30,7 @@ describe('Miya Library', function () {
                 expect(this.player1).toHavePrompt('Which card would you like to put back now');
                 this.player1.clickPrompt('Kudaka');
                 expect(this.player2).toHavePrompt('Play cards from provinces');
+                expect('miya-satoshi').toBe('province 1');
                 this.player2.pass();
                 this.player1.clickCard('miya-satoshi');
             });
@@ -45,6 +46,29 @@ describe('Miya Library', function () {
                 expect(this.player1).not.toBeAbleToSelect('miya-satoshi');
                 expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
                 this.player1.clickPrompt('Do not replace Miya Library');
+                this.player1.clickPrompt('Kudaka');
+                this.player1.clickPrompt('Naive Student');
+                this.player1.clickPrompt('Adept of the Waves');
+            });
+
+            it('should put the card back in the right order', function () {
+                this.player1.clickCard(this.miyaLibrary);
+                expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
+                this.player1.clickPrompt('Miya Satoshi');
+                expect(this.player1).toHavePrompt('Select the card you would like to place on top of your dynasty deck');
+                this.player1.clickPrompt('Kanjo District');
+                expect(this.player1).toHavePrompt('Which card would you like to put back now');
+                this.player1.clickPrompt('Adept of the Waves');
+                expect(this.player1).toHavePrompt('Which card would you like to put back now');
+                this.player1.clickPrompt('Kudaka');
+                this.player1.moveCard(this.player1.player.dynastyDeck.first(), 'dynasty discard pile');
+                expect(this.miyaLibrary).toBe('dynasty discard pile');
+                this.player1.moveCard(this.player1.player.dynastyDeck.first(), 'dynasty discard pile');
+                expect('kudaka').toBe('dynasty discard pile');
+                this.player1.moveCard(this.player1.player.dynastyDeck.first(), 'dynasty discard pile');
+                expect('adept-of-the-waves').toBe('dynasty discard pile');
+                this.player1.moveCard(this.player1.player.dynastyDeck.first(), 'dynasty discard pile');
+                expect('kanjo-district').toBe('dynasty discard pile');
             });
         });
     });
