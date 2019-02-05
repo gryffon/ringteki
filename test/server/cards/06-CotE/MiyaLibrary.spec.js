b8349a2dfe096315c5953d83a738f6da409c12d6
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-04 22:07:33 -0500

    trying to fix merge conflicts

--- test/server/cards/06-CotE/MiyaLibrary.spec.js
+++ test/server/cards/06-CotE/MiyaLibrary.spec.js
@@ -5,24 +5,15 @@ describe('Miya Library', function () {
                 this.setupTest({
                     phase: 'dynasty',
                     player1: {
-<<<<<<< HEAD
                         inPlay: ['miya-library', 'kanjo-district', 'miya-satoshi', 'adept-of-the-waves', 'kudaka', 'naive-student']
-=======
-                        inPlay: ['miya-library', 'kanjo-district', 'miya-satoshi', 'adept-of-the-waves', 'kudaka']
->>>>>>> 48564c0b31ca56fe3393cff025fe0d08d4e87416
                     },
                     player2: {
                     }
                 });
                 this.miyaLibrary = this.player1.findCardByName('miya-library');
                 this.player1.placeCardInProvince('miya-library', 'province 1');
-<<<<<<< HEAD
                 this.player1.moveCard('miya-satoshi', 'dynasty deck');
                 this.player1.moveCard('kanjo-district', 'dynasty deck');
-=======
-                this.player1.moveCard('kanjo-district', 'dynasty deck');
-                this.player1.moveCard('miya-satoshi', 'dynasty deck');
->>>>>>> 48564c0b31ca56fe3393cff025fe0d08d4e87416
                 this.player1.moveCard('adept-of-the-waves', 'dynasty deck');
                 this.player1.moveCard('kudaka', 'dynasty deck');
 
@@ -45,7 +36,6 @@ describe('Miya Library', function () {
 
             it('should not let you select an imperial holding', function () {
                 this.player1.clickCard(this.miyaLibrary);
-<<<<<<< HEAD
                 expect(this.player1).not.toBeAbleToSelect('kanjo-district');
             });
 
@@ -55,12 +45,6 @@ describe('Miya Library', function () {
                 expect(this.player1).not.toBeAbleToSelect('miya-satoshi');
                 expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
                 this.player1.clickPrompt('Do not replace Miya Library');
-                expect(this.player1).toHavePrompt('Which card would you like to put back now');
-=======
-                expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
-                this.player1.clickPrompt('Kanjo District');\
-                expect(this.player1).toHavePrompt('select an imperial character to replace miya library');
->>>>>>> 48564c0b31ca56fe3393cff025fe0d08d4e87416
             });
         });
     });
