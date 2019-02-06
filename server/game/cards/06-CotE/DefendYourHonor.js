9e7ae7626f58df3b54049f703169123d59df8d01
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-02-06 14:45:06 +0000

    Add implementation and tests for Defend Your Honor

---
+++ server/game/cards/06-CotE/DefendYourHonor.js
@@ -0,0 +1,49 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, DuelTypes, Players } = require('../../Constants');
+
+class DefendYourHonor extends DrawCard {
+    setupCardAbilities() {
+        this.wouldInterrupt({
+            title: 'Initiate a military duel',
+            when: {
+                onInitiateAbilityEffects: (event, context) =>
+                    context.game.isDuringConflict() &&
+                    event.card.type === CardTypes.Event && context.player.opponent
+            },
+            targets: {
+                challenger: {
+                    cardType: CardTypes.Character,
+                    controller: Players.Self,
+                    cardCondition: (card) => card.isParticipating()
+                },
+                duelTarget: {
+                    dependsOn: 'challenger',
+                    cardType: CardTypes.Character,
+                    controller: Players.Opponent,
+                    cardCondition: card => card.isParticipating()
+                }
+            },
+            effect: 'initiate a military duel between {1} and {2}',
+            effectArgs: (context) => [context.targets.challenger, context.targets.duelTarget],
+            handler: context => {
+                this.game.actions.duel({
+                    type: DuelTypes.Military,
+                    challenger: context.targets.challenger,
+                    target: context.targets.duelTarget,
+                    resolutionHandler: (winner) => this.resolutionHandler(context, winner)
+                }).resolve(null, context);
+            }
+        });
+    }
+
+    resolutionHandler(context, winner) {
+        if(winner.controller === context.source.controller) {
+            this.game.addMessage('{0} wins the duel and cancels {1}\'s effect', winner, context.event.card);
+            context.cancel();
+        }
+    }
+}
+
+DefendYourHonor.id = 'defend-your-honor';
+
+module.exports = DefendYourHonor;
---
+++ test/server/cards/06-CotE/DefendYourHonor.spec.js
@@ -0,0 +1,100 @@
+describe('Defend Your Honor', function() {
+    integration(function() {
+        describe('Defend Your Honor\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['border-rider'],
+                        hand: ['defend-your-honor']
+                    },
+                    player2: {
+                        inPlay: ['doji-challenger'],
+                        hand: ['way-of-the-crane']
+                    }
+                });
+                this.borderRider = this.player1.findCardByName('border-rider');
+                this.defendYourHonor = this.player1.findCardByName('defend-your-honor');
+
+                this.dojiChallenger = this.player2.findCardByName('doji-challenger');
+                this.wayOfTheCrane = this.player2.findCardByName('way-of-the-crane');
+            });
+
+            it('should not be able to be triggered outside of a conflict', function() {
+                this.player1.pass();
+                expect(this.player2).toHavePrompt('Action Window');
+                this.player2.clickCard(this.wayOfTheCrane);
+                this.player2.clickCard(this.dojiChallenger);
+                expect(this.player1).not.toHavePrompt('Triggered Abilities');
+                expect(this.dojiChallenger.isHonored).toBe(true);
+            });
+
+            it('should trigger when your opponent plays an event', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider],
+                    defenders: [this.dojiChallenger],
+                    type: 'political'
+                });
+                this.player2.clickCard(this.wayOfTheCrane);
+                this.player2.clickCard(this.dojiChallenger);
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.defendYourHonor);
+            });
+
+            it('should initiate a duel', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider],
+                    defenders: [this.dojiChallenger],
+                    type: 'political'
+                });
+                this.player2.clickCard(this.wayOfTheCrane);
+                this.player2.clickCard(this.dojiChallenger);
+                this.player1.clickCard(this.defendYourHonor);
+                expect(this.player1).toHavePrompt('Choose a character');
+                expect(this.player1).toBeAbleToSelect(this.borderRider);
+                this.player1.clickCard(this.borderRider);
+                expect(this.player1).toHavePrompt('Choose a character');
+                expect(this.player1).toBeAbleToSelect(this.dojiChallenger);
+                this.player1.clickCard(this.dojiChallenger);
+                expect(this.player1).toHavePrompt('Choose your bid for the duel\nBorder Rider: 2 vs 3: Doji Challenger');
+            });
+
+            it('should cancel the event if you win the duel', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider],
+                    defenders: [this.dojiChallenger],
+                    type: 'political'
+                });
+                this.player2.clickCard(this.wayOfTheCrane);
+                this.player2.clickCard(this.dojiChallenger);
+                this.player1.clickCard(this.defendYourHonor);
+                this.player1.clickCard(this.borderRider);
+                this.player1.clickCard(this.dojiChallenger);
+                this.player1.clickPrompt('5');
+                this.player2.clickPrompt('1');
+                expect(this.getChatLogs(3)).toContain('Border Rider wins the duel and cancels Way of the Crane\'s effect');
+                expect(this.dojiChallenger.isHonored).toBe(false);
+            });
+
+            it('should not cancel the event if you do not win the duel', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider],
+                    defenders: [this.dojiChallenger],
+                    type: 'political'
+                });
+                this.player2.clickCard(this.wayOfTheCrane);
+                this.player2.clickCard(this.dojiChallenger);
+                this.player1.clickCard(this.defendYourHonor);
+                this.player1.clickCard(this.borderRider);
+                this.player1.clickCard(this.dojiChallenger);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('5');
+                expect(this.dojiChallenger.isHonored).toBe(true);
+            });
+        });
+    });
+});
