a8f1bf05c0a77c01791d1197a6c25e1db2c99775
Kwaice <kwaice@gmail.com>
2019-02-09 23:09:38 +0100

    Outskirts Sentry

---
+++ server/game/cards/06-CotE/OutskirtsSentry.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes } = require('../../Constants');
+
+class OutskirtsSentry extends DrawCard {
+    setupCardAbilities(ability) {
+        this.reaction({
+            title: 'Honor a participating character',
+            when: {
+                onMoveToConflict: (event, context) => context.source.isParticipating()
+            },
+            collectiveTrigger: true,
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: card => card.isParticipating(),
+                gameAction: ability.actions.honor()
+            }
+        });
+    }
+}
+
+OutskirtsSentry.id = 'outskirts-sentry';
+
+module.exports = OutskirtsSentry;
---
+++ test/server/cards/06-CotE/OutskirtsSentry.spec.js
@@ -0,0 +1,85 @@
+describe('Outskirts Sentry', function() {
+    integration(function() {
+        describe('Outskirts Sentry ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['outskirts-sentry','shinjo-outrider','shinjo-scout'],
+                        hand: ['favored-mount'],
+                        dynastyDeck: ['favorable-ground']
+                    },
+                    player2: {
+                        inPlay: ['shinjo-outrider','matsu-berserker']
+                    }
+                });
+                this.fg = this.player1.placeCardInProvince('favorable-ground', 'province 1');
+                this.sentry = this.player1.findCardByName('outskirts-sentry');
+                this.outrider = this.player1.findCardByName('shinjo-outrider');
+                this.mount = this.player1.playAttachment('favored-mount', this.sentry);
+
+                this.berserker = this.player2.findCardByName('matsu-berserker');
+
+                this.noMoreActions();
+            });
+
+            it('should trigger when another character joins the conflict', function() {
+                this.initiateConflict({
+                    attackers: [this.sentry],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard('shinjo-outrider');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.sentry);
+            });
+
+            it('should trigger when an opposing character joins the conflict', function() {
+                this.initiateConflict({
+                    attackers: [this.sentry],
+                    defenders: []
+                });
+                this.player2.clickCard('shinjo-outrider');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.sentry);
+            });
+
+            it('should trigger when it joins a conflict itself', function() {
+                this.initiateConflict({
+                    attackers: [this.outrider],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.mount);
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.sentry);
+            });
+
+            it('should only target participating characters', function() {
+                this.initiateConflict({
+                    attackers: [this.outrider],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.mount);
+                this.player1.clickCard(this.sentry);
+                expect(this.player1).toBeAbleToSelect(this.sentry);
+                expect(this.player1).toBeAbleToSelect(this.outrider);
+                expect(this.player1).not.toBeAbleToSelect('shinjo-scout');
+                expect(this.player1).not.toBeAbleToSelect(this.berserker);
+            });
+
+            it('should only honor the target', function() {
+                this.initiateConflict({
+                    attackers: [this.outrider],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.mount);
+                this.player1.clickCard(this.sentry);
+                this.player1.clickCard(this.sentry);
+                expect(this.sentry.isHonored).toBe(true);
+            });
+        });
+    });
+});
