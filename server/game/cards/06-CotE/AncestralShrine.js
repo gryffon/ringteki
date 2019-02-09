60c1e2d68d47566ad09e8e1704aabc1bbc891f49
Kwaice <kwaice@gmail.com>
2019-02-09 17:46:57 +0100

    Ancestral Shrine

---
+++ server/game/cards/06-CotE/AncestralShrine.js
@@ -0,0 +1,18 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class AncestralShrine extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Return rings to gain honor',
+            cost: AbilityDsl.costs.returnRings(),
+            gameAction: AbilityDsl.actions.gainHonor(context => ({
+                amount: context.costs.returnRing
+            }))
+        });
+    }
+}
+
+AncestralShrine.id = 'ancestral-shrine';
+
+module.exports = AncestralShrine;
