bbf32b76c873397451e41c93e98c2320cdc68fd7
Surface-676JIMU\cmayberry <cdmayberry1@gmail.com>
2019-02-09 12:02:56 -0500

    Started on unmask

---
+++ server/game/cards/06-CotE/Unmask.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+const { Players, CardTypes } = require('../../Constants');
+
+class Unmask extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Discard a character\'s status token and set its skills to its printed value until the conflicts end. Controller gains 2 honor.',
+            condition: context => context.player.opponent && context.player.showBid > context.player.opponent.showBid,
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Any,
+                cardCondition: card => card.isParticipating(),
+                gameAction: [
+                    ability.actions.discardStatusToken(),
+                    ability.actions.cardLastingEffect(context => ({
+                        effect: [
+                            ability.effects.setBaseMilitarySkill(context.target.getBaseMilitarySkill()),
+                            ability.effects.setBasePoliticalSkill(context.target.getBasePoliticalSkill())
+                        ]
+                    })),
+                    ability.actions.gainHonor(context => ({ amount: 2, target: context.target.controller }))
+
+                ]
+            }
+        });
+    }
+}
+
+Unmask.id = 'unmask';
+
+module.exports = Unmask;
