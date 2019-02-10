908efff2108b94a8fa50b168c852c7ea960b4b36
Kwaice <kwaice@gmail.com>
2019-02-10 11:25:18 +0100

    Fix and tests

--- server/game/cards/06-CotE/SolitaryStrength.js
+++ server/game/cards/06-CotE/SolitaryStrength.js
@@ -3,10 +3,21 @@ const DrawCard = require('../../drawcard.js');
 class SolitaryStrength extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            condition: context =>
-                context.source.isAttacking() &&
-                this.game.currentConflict.getNumberOfParticipantsFor('attacker') === 1,
-            effect: ability.effects.restrictNumberOfDefenders(1)
+            effect: ability.effects.terminalCondition({
+                condition: context => context.source.parent.isParticipating() && this.game.currentConflict.getNumberOfParticipantsFor(context.player) !== 1,
+                message: '{0} is discarded from play as {1} is not participating alone in the conflict',
+                messageArgs: context => [context.source,context.source.parent],
+                gameAction: ability.actions.discardFromPlay()
+            })
+        });
+
+        this.reaction({
+            title: 'Gain 1 honor',
+            when: {
+                afterConflict: (event, context) => context.source.parent.isParticipating() &&
+                                                   event.conflict.winner === context.source.parent.controller
+            },
+            gameAction: ability.actions.gainHonor()
         });
     }
 }
