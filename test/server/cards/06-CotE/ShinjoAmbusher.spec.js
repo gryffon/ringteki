1ff53f0f8096b33e92f86acb489b48e265a14542
Kwaice <kwaice@gmail.com>
2019-02-10 17:34:21 +0100

    Update ShinjoAmbusher.spec.js

--- test/server/cards/06-CotE/ShinjoAmbusher.spec.js
+++ test/server/cards/06-CotE/ShinjoAmbusher.spec.js
@@ -24,37 +24,35 @@ describe('Shinjo Ambusher', function() {
                     defenders: [],
                     province: this.uA
                 });
-            });
-
-            it('should trigger after entering play during a conflict', function() {
                 this.player2.pass(); //sneakily smirking because uA will break
                 this.player1.clickCard(this.ambusher);
                 this.player1.clickPrompt('0');
+            });
+
+            it('should trigger after entering play during a conflict', function() {
                 this.player1.clickPrompt('Conflict');
                 expect(this.player1).toHavePrompt('Triggered Abilities');
                 expect(this.player1).toBeAbleToSelect(this.ambusher);
             });
 
             it('should work if played in a conflict', function() {
-                this.player2.pass();
-                this.player1.clickCard(this.ambusher);
-                this.player1.clickPrompt('0');
                 this.player1.clickPrompt('Conflict');
                 this.player1.clickCard(this.ambusher);
                 expect(this.game.currentConflict.attackerSkill).toBe(5);
                 this.player2.pass();
                 this.player1.pass();
                 expect(this.player2).not.toHavePrompt('Triggered Abilities');
-                expect(this.player2).not.toBeAbleToSelect(this.uA); //frowning angrily
+                expect(this.player2).not.toBeAbleToSelect(this.uA);
+                expect(this.uA.isBroken).toBe(true); //frowning angrily
             });
 
-            it('should not work if played home', function() {
-                this.player2.pass();
-                this.player1.clickCard(this.ambusher);
-                this.player1.clickPrompt('0');
+            it('should not trigger if played home', function() {
                 this.player1.clickPrompt('Home');
+                expect(this.ambusher.inConflict).toBe(false);
+                expect(this.game.currentConflict.attackerSkill).toBe(3);
                 expect(this.player1).not.toHavePrompt('Triggered Abilities');
                 expect(this.player1).not.toBeAbleToSelect(this.ambusher);
+                expect(this.player1).toHavePrompt('Waiting for opponent to take an action or pass');
             });
         });
     });
