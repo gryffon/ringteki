c5e82a5db0383cd39a16f40c2795a84a39a7b41c
Kwaice <kwaice@gmail.com>
2019-02-10 10:55:43 +0100

    Updated test

--- test/server/cards/06-CotE/GuardianOfVirtue.spec.js
+++ test/server/cards/06-CotE/GuardianOfVirtue.spec.js
@@ -34,7 +34,7 @@ describe('Guardian Of Virtue', function() {
             expect(this.guardian.bowed).toBe(true);
         });
 
-        it('should not bow as a result of conflict resolution if player doesn\'t have composure', function() {
+        it('should not bow as a result of conflict resolution if player has composure', function() {
             this.player2.pass();
             this.player1.player.showBid = 3;
             this.player2.player.showBid = 1;
@@ -43,6 +43,23 @@ describe('Guardian Of Virtue', function() {
             this.player1.clickPrompt('Don\'t resolve');
             expect(this.player1).toHavePrompt('Action Window');
             expect(this.guardian.bowed).toBe(false);
+        });
+
+        it('should bow as a result of conflict resolution if player looses composure during the conflict', function() {
+            this.player2.pass();
+            this.player1.player.showBid = 3;
+            this.player2.player.showBid = 1;
+            expect(this.player2.player.hasComposure()).toBe(true);
+            this.player1.clickCard(this.mirumotoRaitsugu);
+            this.player1.clickCard(this.guardian);
+            this.player1.clickPrompt('1');
+            this.player2.clickPrompt('3');
+            expect(this.player2.player.hasComposure()).toBe(false);
+            expect(this.mirumotoRaitsugu.location).toBe('dynasty discard pile');
+            this.player2.pass();
+            this.player1.pass();
+            expect(this.player1).toHavePrompt('Action Window');
+            expect(this.guardian.bowed).toBe(true);
         });
 
         it('should bow if attacking with composure', function() {
