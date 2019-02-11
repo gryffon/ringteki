7e448f005e2a737e8f762f4c316212434b437e0f
Kwaice <kwaice@gmail.com>
2019-02-11 20:11:24 +0100

    Test update

--- test/server/cards/06-CotE/ProvingGround.spec.js
+++ test/server/cards/06-CotE/ProvingGround.spec.js
@@ -1,86 +1,83 @@
-describe('Insult to Injury', function() {
+describe('Proving Ground', function() {
     integration(function() {
-        describe('Insult to Injury\'s ability', function() {
+        describe('Proving Ground\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: ['doji-hotaru', 'doji-challenger'],
-                        hand: ['insult-to-injury','policy-debate']
+                        inPlay: ['doji-challenger'],
+                        hand: ['banzai','game-of-sadane'],
+                        dynastyDiscard: ['proving-ground']
                     },
                     player2: {
                         inPlay: ['mirumoto-raitsugu']
                     }
                 });
                 this.noMoreActions();
-                this.dojiHotaru = this.player1.findCardByName('doji-hotaru');
+                this.provingGround = this.player1.placeCardInProvince('proving-ground', 'province 1');
                 this.dojiChallenger = this.player1.findCardByName('doji-challenger');
-                this.insultToInjury = this.player1.findCardByName('insult-to-injury');
-                this.policyDebate = this.player1.findCardByName('policy-debate');
+                this.sadane = this.player1.findCardByName('game-of-sadane');
                 this.mirumotoRaitsugu = this.player2.findCardByName('mirumoto-raitsugu');
 
                 this.initiateConflict({
-                    attackers: [this.dojiHotaru, this.dojiChallenger],
+                    attackers: [this.dojiChallenger],
                     defenders: [this.mirumotoRaitsugu]
                 });
             });
 
-            it('should be selectable when a card you control with the \'duelist\' trait wins a duel', function () {
+            it('should trigger after winning a duel', function () {
                 this.player2.pass();
-                this.player1.clickCard(this.policyDebate);
+                this.player1.clickCard(this.sadane);
                 this.player1.clickCard(this.dojiChallenger);
                 this.player1.clickCard(this.mirumotoRaitsugu);
                 this.player1.clickPrompt('1');
                 this.player2.clickPrompt('1');
                 expect(this.player1).toHavePrompt('Triggered Abilities');
-                expect(this.player1).toBeAbleToSelect(this.insultToInjury);
+                expect(this.player1).toBeAbleToSelect(this.provingGround);
             });
 
-            it('should not be selectable when a card you control that does not have the \'duelist\' trait wins a duel', function() {
-                this.player2.pass();
-                this.player1.clickCard(this.policyDebate);
-                this.player1.clickCard(this.dojiHotaru);
-                this.player1.clickCard(this.mirumotoRaitsugu);
-                this.player1.clickPrompt('1');
-                this.player2.clickPrompt('1');
-                expect(this.player1).not.toHavePrompt('Triggered Abilities');
-                expect(this.player1).not.toBeAbleToSelect(this.insultToInjury);
-            });
-
-            it('should not be selectable when you lose a duel', function() {
-                this.player2.pass();
-                this.player1.clickCard(this.policyDebate);
-                this.player1.clickCard(this.dojiChallenger);
-                this.player1.clickCard(this.mirumotoRaitsugu);
-                this.player1.clickPrompt('1');
-                this.player2.clickPrompt('5');
-                expect(this.player1).not.toHavePrompt('Triggered Abilities');
-                expect(this.player1).not.toBeAbleToSelect(this.insultToInjury);
-            });
-
-            it('should be selectable if you win a duel initiated by your opponent', function() {
+            it('should not trigger after losing a duel', function() {
                 this.player2.clickCard(this.mirumotoRaitsugu);
                 this.player2.clickCard(this.dojiChallenger);
-                this.player2.clickPrompt('1');
-                this.player1.clickPrompt('2');
-                expect(this.player1).toHavePrompt('Triggered Abilities');
-                expect(this.player1).toBeAbleToSelect(this.insultToInjury);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('2');
+                expect(this.player1).not.toHavePrompt('Triggered Abilities');
             });
 
-            describe('if it resolves', function () {
-                beforeEach(function () {
-                    this.player2.pass();
-                    this.player1.clickCard(this.policyDebate);
-                    this.player1.clickCard(this.dojiChallenger);
-                    this.player1.clickCard(this.mirumotoRaitsugu);
-                    this.player1.clickPrompt('1');
-                    this.player2.clickPrompt('1');
-                    this.player1.clickCard(this.insultToInjury);
-                });
+            it('should draw a card', function() {
+                let hand = this.player1.hand.length;
+                this.player2.pass();
+                this.player1.clickCard(this.sadane);
+                this.player1.clickCard(this.dojiChallenger);
+                this.player1.clickCard(this.mirumotoRaitsugu);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.provingGround);
+                this.player1.clickCard(this.provingGround);
+                expect(this.player1.hand.length).toBe(hand);
+            });
 
-                it('should dishonor the loser of the duel', function () {
-                    expect(this.mirumotoRaitsugu.isDishonored).toBeTruthy();
-                });
+            it('should work twice a round', function() {
+                let hand = this.player1.hand.length;
+                this.player2.pass();
+                this.player1.clickCard(this.sadane);
+                this.player1.clickCard(this.dojiChallenger);
+                this.player1.clickCard(this.mirumotoRaitsugu);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.provingGround);
+                this.player1.clickCard(this.provingGround);
+                expect(this.player1.hand.length).toBe(hand);
+                this.player2.clickCard(this.mirumotoRaitsugu);
+                this.player2.clickCard(this.dojiChallenger);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('1');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.provingGround);
+                this.player1.clickCard(this.provingGround);
+                expect(this.player1.hand.length).toBe(hand + 1);
             });
         });
     });
