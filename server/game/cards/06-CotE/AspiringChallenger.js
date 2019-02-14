6e6b81e15cb1fb3ad53d357847cf8c46a8d37fe1
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-02-14 10:13:23 +0000

    Refactor duels to use gameAction (#2887)

--- server/game/cards/06-CotE/AspiringChallenger.js
+++ server/game/cards/06-CotE/AspiringChallenger.js
@@ -1,5 +1,6 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
+const { DuelTypes } = require('../../Constants');
 
 class AspiringChallenger extends DrawCard {
     setupCardAbilities() {
@@ -8,16 +9,15 @@ class AspiringChallenger extends DrawCard {
         });
         this.action({
             title: 'Initiate a Military duel',
-            initiateDuel: context => ({
-                type: 'military',
-                resolutionHandler: (winner) => this.resolutionHandler(context, winner)
-            })
+            initiateDuel: {
+                type: DuelTypes.Military,
+                message: '{0} wins the duel and is honored',
+                messageArgs: context => context.game.currentDuel.winner,
+                gameAction: AbilityDsl.actions.honor(context => ({
+                    target: context.game.currentDuel.winner
+                }))
+            }
         });
-    }
-
-    resolutionHandler(context, winner) {
-        this.game.addMessage('{0} wins the duel and is honored', winner);
-        this.game.applyGameAction(context, { honor: winner });
     }
 }
 
