edfd60857c88e925230b97eb1507af2d102259ac
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-02-18 17:18:17 +0000

    Add test for Iaijutsu Master if previous duel is canceled

--- test/server/cards/06-CotE/IaijutsuMaster.spec.js
+++ test/server/cards/06-CotE/IaijutsuMaster.spec.js
@@ -38,6 +38,56 @@ describe('Iaijutsu Master', function() {
             });
         });
 
+        describe('Iaijutsu Master\'s ability, if previous duel canceled with Stay Your Hand,', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        honor: 11,
+                        inPlay: ['mirumoto-raitsugu'],
+                        hand: ['challenge-on-the-fields']
+                    },
+                    player2: {
+                        honor: 11,
+                        inPlay: ['doji-challenger', 'doji-whisperer'],
+                        hand: ['iaijutsu-master', 'stay-your-hand']
+                    }
+                });
+                this.mirumotoRaitsugu = this.player1.findCardByName('mirumoto-raitsugu');
+                this.challengeOnTheFields = this.player1.findCardByName('challenge-on-the-fields');
+
+                this.dojiChallenger = this.player2.findCardByName('doji-challenger');
+                this.dojiWhisperer = this.player2.findCardByName('doji-whisperer');
+                this.iaijutsuMaster = this.player2.findCardByName('iaijutsu-master');
+                this.stayYourHand = this.player2.findCardByName('stay-your-hand');
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.mirumotoRaitsugu],
+                    defenders: [this.dojiChallenger, this.dojiWhisperer]
+                });
+            });
+
+            it('should not count as being part of the next duel', function() {
+                this.player2.playAttachment(this.iaijutsuMaster, this.dojiChallenger);
+                this.player1.clickCard(this.mirumotoRaitsugu);
+                this.player1.clickCard(this.dojiChallenger);
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.stayYourHand);
+                this.player2.clickCard(this.stayYourHand);
+                expect(this.game.currentDuel).toBe(null);
+                this.player2.pass();
+                this.player1.clickCard(this.challengeOnTheFields);
+                expect(this.player1).toHavePrompt('Challenge on the Fields');
+                this.player1.clickCard(this.mirumotoRaitsugu);
+                this.player1.clickCard(this.dojiWhisperer);
+                this.player1.clickPrompt('2');
+                this.player2.clickPrompt('2');
+                expect(this.player2).not.toHavePrompt('Triggered Abilities');
+                expect(this.player2).not.toBeAbleToSelect(this.iaijutsuMaster);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+            });
+        });
+
         describe('During a duel,', function() {
             beforeEach(function() {
                 this.setupTest({
