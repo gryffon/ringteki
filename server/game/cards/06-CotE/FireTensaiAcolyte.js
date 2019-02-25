f89ac5c013192d1268445c9783535566125a102c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-02-25 05:02:28 +0000

    Update FireTensaiAcolyte.js

--- server/game/cards/06-CotE/FireTensaiAcolyte.js
+++ server/game/cards/06-CotE/FireTensaiAcolyte.js
@@ -1,12 +1,12 @@
 const DrawCard = require('../../drawcard.js');
-const AbilitDsl = require('../../abilitydsl');
+const AbilityDsl = require('../../abilitydsl');
 const { Elements } = require('../../Constants');
 
 class FireTensaiAcolyte extends DrawCard {
     setupCardAbilities() {
         this.persistentEffect({
             condition: () => !this.game.isDuringConflict(Elements.Fire),
-            effect: AbilitDsl.effects.cardCannot('declareAsAttacker')
+            effect: AbilityDsl.effects.cardCannot('declareAsAttacker')
         });
     }
 }
