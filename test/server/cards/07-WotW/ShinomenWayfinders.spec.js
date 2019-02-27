dbd7e26461a3cc16e7fa84aaceec44727d038136
Matthew Griffith <matthew-griffith@protonmail.com>
2019-02-26 21:36:55 -0500

    added a test for infiltrator

--- test/server/cards/07-WotW/ShinomenWayfinders.spec.js
+++ test/server/cards/07-WotW/ShinomenWayfinders.spec.js
@@ -10,25 +10,41 @@ describe('Shinomen Wayfinders', function() {
                         hand: ['shinomen-wayfinders']
                     },
                     player2: {
-                        honor: 11,
+                        fate: 11,
                         inPlay: ['iuchi-wayfinder'],
                         hand: ['infiltrator']
                     }
                 });
                 this.borderRider = this.player1.findCardByName('border-rider');
+                this.shinomenWayfinders = this.player1.findCardByName('shinomen-wayfinders');
+                this.player1.player.showBid = 4;
+                this.player2.player.showBid = 5;
                 this.noMoreActions();
                 this.initiateConflict({
                     attackers: [this.borderRider, 'kudaka', 'battle-maiden-recruit'],
-                    defenders: []
+                    defenders: ['iuchi-wayfinder']
                 });
-                this.player2.pass();
             });
 
             it('should reduce the cost only for unicorn charaters participating', function () {
-                this.player1.clickCard('shinomen-wayfinders');
+                this.player2.pass();
+                this.player1.clickCard(this.shinomenWayfinders);
                 this.player1.clickPrompt('1');
+                this.player1.clickPrompt('Conflict');
                 expect(this.player1.player.fate).toBe(1);
                 expect(this.game.currentConflict.attackers).toContain(this.shinomenWayfinders);
+            });
+
+            it('should work for your opponent with infiltrator as well', function () {
+                this.player2.playAttachment('infiltrator', 'iuchi-wayfinder');
+                this.player1.moveCard(this.shinomenWayfinders, 'conflict deck');
+                this.player2.clickCard('infiltrator');
+                expect(this.player2).toHavePrompt('Choose an action for Shinomen Wayfinders');
+                this.player2.clickPrompt('Play this card');
+                this.player2.clickPrompt('1');
+                this.player2.clickPrompt('Conflict');
+                expect(this.player2.player.fate).toBe(6);
+                expect(this.game.currentConflict.defenders).toContain(this.shinomenWayfinders);
             });
 
         });
