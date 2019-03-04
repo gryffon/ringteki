cfe91d79216eced84aaf032bf218914534a3ecb2
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-03-04 20:10:50 +0000

    Add implementation and tests for Visiting Advisor

---
+++ server/game/cards/07-WotW/VisitingAdvisor.js
@@ -0,0 +1,28 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, Players } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class VisitingAdvisor extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Send this and up to 1 other character home',
+            condition: context => context.source.isParticipating(),
+            target: {
+                controller: Players.Self,
+                cardType: CardTypes.Character,
+                optional: true,
+                cardCondition: (card, context) => card !== context.source,
+                gameAction: AbilityDsl.actions.multiple([
+                    AbilityDsl.actions.sendHome(),
+                    AbilityDsl.actions.sendHome(context => ({ target: context.source }))
+                ])
+            },
+            effect: 'send {0}{1}{2} home',
+            effectArgs: (context) => context.target ? [' and ', context.source] : []
+        });
+    }
+}
+
+VisitingAdvisor.id = 'visiting-advisor';
+
+module.exports = VisitingAdvisor;
---
+++ test/server/cards/07-WotW/VisitingAdvisor.spec.js
@@ -0,0 +1,114 @@
+describe('Visiting Advisor', function() {
+    integration(function() {
+        describe('Visiting Advisor\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['visiting-advisor', 'battle-maiden-recruit', 'border-rider']
+                    },
+                    player2: {
+                        inPlay: ['wandering-ronin']
+                    }
+                });
+
+                this.visitingAdvisor = this.player1.findCardByName('visiting-advisor');
+                this.battleMaidenRecruit = this.player1.findCardByName('battle-maiden-recruit');
+                this.borderRider = this.player1.findCardByName('border-rider');
+
+                this.wanderingRonin = this.player2.findCardByName('wandering-ronin');
+            });
+
+            it('should not trigger outside of a conflict', function() {
+                expect(this.player1).toHavePrompt('Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                expect(this.player1).toHavePrompt('Action Window');
+            });
+
+            it('should not trigger in a conflict if Visiting Advisor is not participating', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.battleMaidenRecruit],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should trigger during a conflict where Visiting Advisor is participating', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.visitingAdvisor, this.battleMaidenRecruit, this.borderRider],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                expect(this.player1).toHavePrompt('Visiting Advisor');
+            });
+
+            it('should allow you to choose 0 characters', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.visitingAdvisor, this.battleMaidenRecruit, this.borderRider],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                expect(this.player1).toHavePromptButton('No more targets');
+            });
+
+            it('should allow you to choose up to 1 other character you control', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.visitingAdvisor, this.battleMaidenRecruit, this.borderRider],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                expect(this.player1).toHavePrompt('Choose a character');
+                expect(this.player1).not.toBeAbleToSelect(this.visitingAdvisor);
+                expect(this.player1).toBeAbleToSelect(this.battleMaidenRecruit);
+                expect(this.player1).toBeAbleToSelect(this.borderRider);
+                expect(this.player1).not.toBeAbleToSelect(this.wanderingRonin);
+            });
+
+            it('should send Visiting Advisor home if 0 characters chosen', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.visitingAdvisor, this.battleMaidenRecruit, this.borderRider],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                this.player1.clickPrompt('No more targets');
+                expect(this.visitingAdvisor.isParticipating()).toBe(false);
+                expect(this.battleMaidenRecruit.isParticipating()).toBe(true);
+                expect(this.borderRider.isParticipating()).toBe(true);
+                expect(this.getChatLogs(3)).toContain('player1 uses Visiting Advisor to send Visiting Advisor home');
+            });
+
+            it('should send both Visiting Advisor and the chosen character home if 1 character is chosen', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.visitingAdvisor, this.battleMaidenRecruit, this.borderRider],
+                    defenders: [this.wanderingRonin]
+                });
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.visitingAdvisor);
+                this.player1.clickCard(this.borderRider);
+                expect(this.visitingAdvisor.isParticipating()).toBe(false);
+                expect(this.battleMaidenRecruit.isParticipating()).toBe(true);
+                expect(this.borderRider.isParticipating()).toBe(false);
+                expect(this.getChatLogs(3)).toContain('player1 uses Visiting Advisor to send Border Rider and Visiting Advisor home');
+            });
+        });
+    });
+});
+
