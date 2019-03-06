11b6361d9925063a3559739a6c470f03cb7c272a
Kwaice <kwaice@gmail.com>
2019-03-06 18:20:30 +0100

    Update to AbilityDsl

--- server/game/cards/07-WotW/SpeakToTheHeart.js
+++ server/game/cards/07-WotW/SpeakToTheHeart.js
@@ -2,19 +2,19 @@ const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
 
 class SpeakToTheHeart extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: 'give +1 political to a character for each faceup province',
             condition: () => this.game.isDuringConflict(),
             max: AbilityDsl.limit.perConflict(1),
             target: {
                 cardCondition: card => card.isFaction('unicorn'),
-                gameAction: ability.actions.cardLastingEffect(context => ({
-                    effect: ability.effects.modifyPoliticalSkill(context.player.getNumberOfOpponentsFaceupProvinces())
+                gameAction: AbilityDsl.actions.cardLastingEffect(context => ({
+                    effect: AbilityDsl.effects.modifyPoliticalSkill(context.player.getNumberOfOpponentsFaceupProvinces())
                 }))
             },
-            effect: 'give {0} +1{1} for each faceup non-stronghold province their opponent controls',
-            effectArgs: () => ['political']
+            effect: 'give {0} +1{1} for each faceup non-stronghold province their opponent controls (+{2}{1})',
+            effectArgs: context => ['political', context.player.getNumberOfOpponentsFaceupProvinces()]
         });
     }
 }
