4098c1bda28e2fb4e22b8ce3e14a391c46f8eafb
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-03-06 11:48:36 +0000

    Update Iuchi Daiyu to use AbilityDsl

--- server/game/cards/07-WotW/IuchiDaiyu.js
+++ server/game/cards/07-WotW/IuchiDaiyu.js
@@ -1,13 +1,14 @@
 const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
 
 class IuchiDaiyu extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: '+1 military for each faceup province',
             condition: () => this.game.isDuringConflict(),
             target: {
-                gameAction: ability.actions.cardLastingEffect(context => ({
-                    effect: ability.effects.modifyMilitarySkill(context.player.getNumberOfOpponentsFaceupProvinces())
+                gameAction: AbilityDsl.actions.cardLastingEffect(context => ({
+                    effect: AbilityDsl.effects.modifyMilitarySkill(context.player.getNumberOfOpponentsFaceupProvinces())
                 }))
             },
             effect: 'give {0} +1{1} for each faceup non-stronghold province their opponent controls (+{2}{1})',
