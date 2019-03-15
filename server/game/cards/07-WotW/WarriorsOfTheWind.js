d193071d0348d4face33be7c09c41ed9b475c6d3
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-03-15 12:06:47 +0000

    Add implementation and tests for Warriors of the Wind

---
+++ server/game/cards/07-WotW/WarriorsOfTheWind.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes, Players } = require('../../Constants');
+
+class WarriorsOfTheWind extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Re-arrange participating cavalry characters',
+            condition: () => this.game.isDuringConflict(),
+            gameAction: AbilityDsl.actions.sendHome(context => ({
+                target: context.player.filterCardsInPlay(card => card.hasTrait('cavalry') && card.isParticipating())
+            })),
+            then: {
+                target: {
+                    activePromptTitle: 'Choose characters',
+                    numCards: 0,
+                    //optional: true,
+                    cardType: CardTypes.Character,
+                    controller: Players.Self,
+                    cardCondition: card => card.hasTrait('cavalry'),
+                    gameAction: AbilityDsl.actions.moveToConflict()
+                },
+                message: '{0} chooses to move {2} to the conflict'
+            }
+        });
+    }
+}
+
+WarriorsOfTheWind.id = 'warriors-of-the-wind';
+
+module.exports = WarriorsOfTheWind;
