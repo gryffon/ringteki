924dda5b3423c9b1657f791cb9b201e922b65c7c
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-03-16 12:00:17 +0000

    Update ShiotomeHeroine.js

--- server/game/cards/06-CotE/ShiotomeHeroine.js
+++ server/game/cards/06-CotE/ShiotomeHeroine.js
@@ -7,8 +7,8 @@ class ShiotomeHeroine extends DrawCard {
         this.reaction({
             title: 'Ready this character',
             when: {
-                onModifyHonor: (event, context) => 
-                    event.amount > 0 && context.player.opponent && 
+                onModifyHonor: (event, context) =>
+                    event.amount > 0 && context.player.opponent &&
                     event.player === context.player.opponent && event.context.stage === Stages.Effect
             },
             gameAction: AbilityDsl.actions.ready()
