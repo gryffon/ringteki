955ea533ec07c0866ed408845c71b0609c01eb6b
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-03-16 09:14:36 +0000

    Shiotome Heroine bugfix

--- server/game/cards/06-CotE/ShiotomeHeroine.js
+++ server/game/cards/06-CotE/ShiotomeHeroine.js
@@ -1,12 +1,14 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl.js');
+const { Stages } = require('../../Constants');
 
 class ShiotomeHeroine extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             title: 'Ready this character',
             when: {
-                onModifyHonor: (event, context) => event.amount > 0 && context.player.opponent && event.player === context.player.opponent
+                onModifyHonor: (event, context) => 
+                    event.amount > 0 && context.player.opponent && event.player === context.player.opponent && event.context.stage === Stages.Effect
             },
             gameAction: AbilityDsl.actions.ready()
         });
