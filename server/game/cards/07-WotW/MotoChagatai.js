07efe8d0428c52d860534c428eb3d633fc62040e
Matthew Griffith <matthew-griffith@protonmail.com>
2019-04-08 22:11:45 -0400

    added the first draft of the cards class

--- server/game/cards/07-WotW/MotoChagatai.js
+++ server/game/cards/07-WotW/MotoChagatai.js
@@ -1,14 +1,29 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
-const { Players } = require('../../Constants');
+const EventRegistrar = require('../../eventregistrar.js');
+const { EventNames } = require('../../Constants');
 
 
 class MotoChagatai extends DrawCard {
     setupCardAbilities() {
+        this.provinceBroken = false;
+        this.eventRegistrar = new EventRegistrar(this.game, this);
+        this.eventRegistrar.register([EventNames.OnBreakProvince, EventNames.OnConflictFinished]);
+
         this.persistentEffect({
-            condition: () => true,
+            condition: context => (
+                context.source.isAttacking()
+            ),
             effect: AbilityDsl.effects.doesNotBow()
         });
+    }
+
+    onBreakProvince() {
+        this.provinceBroken = true;
+    }
+
+    onConflictFinished() {
+        this.provinceBroken = false;
     }
 }
 
