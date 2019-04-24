40a3f89b5144d0940d3f7d7ceb15abcc7191ed6e
Matthew Griffith <matthew-griffith@protonmail.com>
2019-04-23 23:40:06 -0400

    first version of the card

---
+++ server/game/cards/08-MotC/PoliticalSanctions.js
@@ -0,0 +1,22 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class PoliticalSanctions extends DrawCard {
+    setupCardAbilities() {
+        this.whileAttached({
+            effect: AbilityDsl.effects.cardCannot('triggerAbilities')
+        });
+    }
+
+    canAttach(card, context) {
+        let diff = this.game.currentConflict.attackerSkill - this.game.currentConflict.defenderSkill;
+        return context.game.isDuringConflict('political') &&
+            context.player.isAttackingPlayer() ? diff < 0 : diff > 0 &&
+            context.source.parent.isParticipating() &&
+            super.canAttach(card, context);
+    }
+}
+
+PoliticalSanctions.id = 'political-sanctions';
+
+module.exports = PoliticalSanctions;
