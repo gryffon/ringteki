e03c58c6534868a64813e4626dcbacb7ec09ba4e
Matthew Griffith <matthew-griffith@protonmail.com>
2019-04-23 20:32:06 -0400

    first version of the card and a simple test

---
+++ test/server/cards/08-MotC/WinterCourtHosts.spec.js
@@ -0,0 +1,38 @@
+describe('Winter Court Hosts', function() {
+    integration(function() {
+        describe('Winter Court Hosts\' ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['winter-court-hosts'],
+                        hand: []
+                    },
+                    player2: {
+                        inPlay: ['shiba-tetsu'],
+                        hand: ['fine-katana', 'shrine-maiden', 'a-legion-of-one']
+                    }
+                });
+
+                this.winterCourt = this.player1.findCardByName('winter-court-hosts');
+                this.shrineMaiden = this.player2.findCardByName('shrine-maiden');
+                this.tetsu = this.player2.findCardByName('shiba-tetsu');
+
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.winterCourt],
+                    defenders: [this.tetsu]
+                });
+            });
+
+            it('should trigger when your opponent plays a attachment', function() {
+                this.player2.playAttachment('fine-katana', this.tetsu);
+                expect(this.player1).toHavePrompt('Winter Court Hosts');
+                this.player1.clickCard(this.winterCourt);
+                expect(this.player1.hand.length).toBe(1);
+            });
+
+        });
+    });
+});
