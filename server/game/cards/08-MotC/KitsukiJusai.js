432fc4e076cc7c1f5f72c2e406d76dee2bf36b81
Kwaice <kwaice@gmail.com>
2019-04-26 18:44:45 +0200

    MotC: Kitsuki Jusai

---
+++ server/game/cards/08-MotC/KitsukiJusai.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class KitsukiJusai extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Set your opponent\'s dial to equal yours',
+            when: {
+                onHonorDialsRevealed: (event, context) =>
+                    context.player.opponent &&
+                    context.player.honorBid === context.player.opponent.honorBid &&
+                    context.player.opponent.fate > 0
+            },
+            gameAction: AbilityDsl.actions.selectRing(context => ({
+                activePromptTitle: 'Choose an unclaimed ring to move fate to',
+                ringCondition: ring => ring.isUnclaimed() && ring !== context.ring,
+                message: '{0} moves a fate from {1}\'s fate pool to the {2}',
+                messageArgs: ring => [context.player, context.player.opponent, ring],
+                gameAction: AbilityDsl.actions.placeFateOnRing({ origin: context.player.opponent })
+            })),
+            effect: 'move 1 fate from {1}\'s fate pool to an unclaimed ring',
+            effectArgs: context => [context.player.opponent]
+        });
+    }
+}
+
+KitsukiJusai.id = 'kitsuki-jusai';
+
+module.exports = KitsukiJusai;
