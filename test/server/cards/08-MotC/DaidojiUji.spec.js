a4bec1afa1e39482ff3d9fb3a63a9fd9c8d9c4d1
Kwaice <kwaice@gmail.com>
2019-04-28 11:20:18 +0200

    Added Toturi 2 test

--- test/server/cards/08-MotC/DaidojiUji.spec.js
+++ test/server/cards/08-MotC/DaidojiUji.spec.js
@@ -5,13 +5,10 @@ describe('Daidoji Uji', function() {
                 this.setupTest({
                     phase: 'dynasty',
                     player1: {
-                        inPlay: ['daidoji-uji'],
+                        inPlay: ['daidoji-uji','akodo-toturi-2'],
                         dynastyDiscard: ['doji-whisperer','kakita-kaezin'],
-                        hand: ['political-rival','way-of-the-crane'],
+                        hand: ['political-rival','way-of-the-crane','steward-of-law'],
                         fate: 10
-                    },
-                    player2: {
-                        inPlay: ['utaku-tetsuko','akodo-toturi-2']
                     }
                 });
 
@@ -19,10 +16,10 @@ describe('Daidoji Uji', function() {
                 this.kakitaKaezin = this.player1.placeCardInProvince('kakita-kaezin','province 2');
                 this.daidojiUji = this.player1.findCardByName('daidoji-uji');
                 this.politicalRival = this.player1.findCardByName('political-rival');
+                this.stewardOfLaw = this.player1.findCardByName('steward-of-law');
                 this.wayOfTheCrane = this.player1.findCardByName('way-of-the-crane');
+                this.akodoToturi2 = this.player1.findCardByName('akodo-toturi-2');
 
-                this.utakuTetsuko = this.player2.findCardByName('utaku-tetsuko');
-                this.akodoToturi2 = this.player2.findCardByName('akodo-toturi-2');
                 this.daidojiUji.honor();
             });
 
@@ -38,7 +35,7 @@ describe('Daidoji Uji', function() {
                 expect(this.player1.fate).toBe(9);
             });
 
-            it('let you play characters as if they were in your hand, reducing their cost by 1', function() {
+            it('should let you play characters as if they were in your hand, reducing their cost by 1', function() {
                 this.nextPhase();
                 this.nextPhase();
                 expect(this.game.currentPhase).toBe('conflict');
@@ -54,14 +51,33 @@ describe('Daidoji Uji', function() {
                 expect(this.player1.fate).toBe(7);
             });
 
-            it('should not discount characters played from hand', function() {
+            it('should not discount characters played directly from hand', function() {
                 this.nextPhase();
                 this.nextPhase();
-                expect(this.game.currentPhase).toBe('conflict');
                 this.player1.clickCard(this.politicalRival);
                 this.player1.clickPrompt('0');
                 expect(this.politicalRival.location).toBe('play area');
                 expect(this.player1.fate).toBe(7);
+                this.player2.pass();
+                this.player1.clickCard(this.stewardOfLaw);
+                this.player1.clickPrompt('0');
+                expect(this.stewardOfLaw.location).toBe('play area');
+                expect(this.player1.fate).toBe(6);
+            });
+
+            it('should not let you play characters as if they were in your hand if Toturi2 is participating', function() {
+                this.nextPhase();
+                this.nextPhase();
+                this.noMoreActions();
+                this.player1.player.imperialFavor = 'political';
+                expect(this.player1.player.imperialFavor).toBe('political');
+                this.initiateConflict({
+                    attackers: [this.akodoToturi2],
+                    defenders: []
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.kakitaKaezin);
+                expect(this.player1).not.toHavePrompt('Choose additional fate');
             });
         });
     });
