7b0fa1ffe9a9638cd8f5c45339408d8ef5d165d4
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-04-28 20:49:50 +0100

    Add nesting and cancel tests

--- test/server/cards/06-CotE/ChallengeOnTheFields.spec.js
+++ test/server/cards/06-CotE/ChallengeOnTheFields.spec.js
@@ -37,6 +37,70 @@ describe('Challenge on the Fields', function() {
             });
         });
 
+        describe('Challenge on the Fields\'s ability interaction with nested duels', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        honor: 11,
+                        inPlay: ['border-rider', 'battle-maiden-recruit', 'ide-messenger', 'aggressive-moto'],
+                        hand: ['challenge-on-the-fields', 'defend-your-honor']
+                    },
+                    player2: {
+                        honor: 11,
+                        inPlay: ['agasha-swordsmith', 'ancient-master', 'togashi-mitsu'],
+                        hand: ['contingency-plan']
+                    }
+                });
+                this.borderRider = this.player1.findCardByName('border-rider');
+                this.battleMaidenRecruit = this.player1.findCardByName('battle-maiden-recruit');
+                this.ideMessenger = this.player1.findCardByName('ide-messenger');
+                this.aggressiveMoto = this.player1.findCardByName('aggressive-moto');
+                this.challengeOnTheFields = this.player1.findCardByName('challenge-on-the-fields');
+                this.defendYourHonor = this.player1.findCardByName('defend-your-honor');
+
+                this.agashaSwordsmith = this.player2.findCardByName('agasha-swordsmith');
+                this.ancientMaster = this.player2.findCardByName('ancient-master');
+                this.togashiMitsu = this.player2.findCardByName('togashi-mitsu');
+                this.contingencyPlan = this.player2.findCardByName('contingency-plan');
+            });
+
+            it('the bonus to military skill should continue throughout all of the nested duels', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider, this.battleMaidenRecruit, this.ideMessenger],
+                    defenders: [this.agashaSwordsmith, this.ancientMaster],
+                    type: 'political'
+                });
+                this.player2.pass();
+                this.player1.clickCard(this.challengeOnTheFields);
+                this.player1.clickCard(this.battleMaidenRecruit);
+                this.player1.clickCard(this.agashaSwordsmith);
+                this.player1.clickPrompt('4');
+                this.player2.clickPrompt('3');
+                expect(this.player2).toHavePrompt('Triggered Abilities');
+                expect(this.player2).toBeAbleToSelect(this.contingencyPlan);
+                this.player2.clickCard(this.contingencyPlan);
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.defendYourHonor);
+                this.player1.clickCard(this.defendYourHonor);
+                this.player1.clickCard(this.borderRider);
+                this.player2.clickCard(this.agashaSwordsmith);
+                expect(this.borderRider.getMilitarySkill()).toBe(2);
+                expect(this.battleMaidenRecruit.getMilitarySkill()).toBe(0 + 2);
+                expect(this.agashaSwordsmith.getMilitarySkill()).toBe(1 + 1);
+                this.player1.clickPrompt('1');
+                this.player2.clickPrompt('2');
+                expect(this.getChatLogs(2)).toContain('Border Rider: 3 vs 4: Agasha Swordsmith'); // 2 (skill) + 1 (honor bid) vs. 1 (skill) + 1 (CotF) + 2 (honor bid)
+                expect(this.player2).toHavePrompt('Contingency Plan');
+                this.player2.clickPrompt('Increase honor bid');
+                expect(this.getChatLogs(4)).toContain('Battle Maiden Recruit: 3 vs 5: Agasha Swordsmith'); // 0 (skill) + 2 (CotF) + 1 (honor bid) vs. 1 (skill) + 1 (CotF) + 3 (honor bid)
+                expect(this.battleMaidenRecruit.isParticipating()).toBe(false);
+                expect(this.battleMaidenRecruit.getMilitarySkill()).toBe(0);
+                expect(this.agashaSwordsmith.getMilitarySkill()).toBe(1);
+            });
+        });
+
         describe('Challenge on the Fields\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
@@ -48,7 +112,8 @@ describe('Challenge on the Fields', function() {
                     },
                     player2: {
                         honor: 11,
-                        inPlay: ['agasha-swordsmith', 'ancient-master', 'togashi-mitsu']
+                        inPlay: ['agasha-swordsmith', 'ancient-master', 'togashi-mitsu'],
+                        hand: ['stay-your-hand']
                     }
                 });
                 this.borderRider = this.player1.findCardByName('border-rider');
@@ -120,6 +185,7 @@ describe('Challenge on the Fields', function() {
                 this.player1.clickCard(this.challengeOnTheFields);
                 this.player1.clickCard(this.borderRider);
                 this.player1.clickCard(this.agashaSwordsmith);
+                this.player2.clickPrompt('Pass');
                 expect(this.borderRider.getMilitarySkill()).toBe(2 + 2);
                 expect(this.agashaSwordsmith.getMilitarySkill()).toBe(1 + 1);
                 this.player1.clickPrompt('1');
@@ -139,12 +205,31 @@ describe('Challenge on the Fields', function() {
                 this.player1.clickCard(this.challengeOnTheFields);
                 this.player1.clickCard(this.borderRider);
                 this.player1.clickCard(this.agashaSwordsmith);
+                this.player2.clickPrompt('Pass');
                 this.player1.clickPrompt('1');
                 this.player2.clickPrompt('1');
                 expect(this.borderRider.inConflict).toBe(true);
                 expect(this.agashaSwordsmith.inConflict).toBe(false);
             });
 
+            it('should remove the bonuses if the duel is canceled', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.borderRider, this.battleMaidenRecruit, this.ideMessenger],
+                    defenders: [this.agashaSwordsmith, this.ancientMaster],
+                    type: 'political'
+                });
+                this.player2.pass();
+                expect(this.borderRider.getMilitarySkill()).toBe(2);
+                expect(this.agashaSwordsmith.getMilitarySkill()).toBe(1);
+                this.player1.clickCard(this.challengeOnTheFields);
+                this.player1.clickCard(this.borderRider);
+                this.player1.clickCard(this.agashaSwordsmith);
+                this.player2.clickCard('stay-your-hand');
+                expect(this.player2).toHavePrompt('Conflict Action Window');
+                expect(this.borderRider.getMilitarySkill()).toBe(2);
+                expect(this.agashaSwordsmith.getMilitarySkill()).toBe(1);
+            });
         });
     });
 });
