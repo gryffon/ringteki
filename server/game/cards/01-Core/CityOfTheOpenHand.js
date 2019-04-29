02086959e0fd748f130dd03fb1c001f42dd03d93
Kwaice <kwaice@gmail.com>
2019-04-29 19:32:14 +0200

    City of the Open Hand Errata

--- server/game/cards/01-Core/CityOfTheOpenHand.js
+++ server/game/cards/01-Core/CityOfTheOpenHand.js
@@ -1,15 +1,25 @@
 const StrongholdCard = require('../../strongholdcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { TargetModes } = require('../../Constants');
 
 class CityOfTheOpenHand extends StrongholdCard {
     setupCardAbilities(ability) {
         this.action({
-            title: 'Steal an honor',
+            title: 'Gain 1 honor or make opponent lose 1 honor',
             cost: ability.costs.bowSelf(),
             condition: context => context.player.opponent && context.player.honor < context.player.opponent.honor,
-            gameAction: ability.actions.takeHonor()
+            target: {
+                mode: TargetModes.Select,
+                choices: {
+                    'Gain 1 Honor': AbilityDsl.actions.gainHonor()
+                    ,
+                    'Make opponent lose 1 honor': AbilityDsl.actions.loseHonor()
+                }
+            }
         });
     }
 }
+
 
 CityOfTheOpenHand.id = 'city-of-the-open-hand';
 
