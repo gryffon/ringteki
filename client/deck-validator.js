ce3787bd39f601101a7a9ee04bdbd993f1554345
jeremylarner <jeremyaroundtheworld@gmail.com>
2019-04-30 15:32:12 +0100

    RRG v9 role update

--- client/deck-validator.js
+++ client/deck-validator.js
@@ -5,13 +5,13 @@ const moment = require('moment');
 const RestrictedList = require('./RestrictedList');
 
 const officialRoles = {
-    crab: ['keeper-of-water', 'seeker-of-earth'],
-    crane: ['seeker-of-fire', 'keeper-of-air'],
-    dragon: ['seeker-of-void', 'keeper-of-void'],
-    lion: ['keeper-of-earth', 'seeker-of-void'],
-    phoenix: ['keeper-of-air', 'seeker-of-air'],
-    scorpion: ['seeker-of-air', 'keeper-of-earth'],
-    unicorn: ['seeker-of-water', 'keeper-of-fire']
+    crab: ['keeper-of-water', 'seeker-of-void'],
+    crane: ['seeker-of-fire', 'seeker-of-void'],
+    dragon: ['seeker-of-void', 'keeper-of-water'],
+    lion: ['keeper-of-earth', 'seeker-of-air'],
+    phoenix: ['keeper-of-air', 'seeker-of-void'],
+    scorpion: ['seeker-of-air', 'keeper-of-air'],
+    unicorn: ['seeker-of-water', 'keeper-of-water']
 };
 
 const openRoles = [
