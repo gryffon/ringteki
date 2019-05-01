3c98fa3fa30a00f95eaa131b518894fbaeeccb88
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-05-01 21:47:45 +0100

    Update to use subThenContext

--- server/game/cards/08-MotC/DistinguishedDojo.js
+++ server/game/cards/08-MotC/DistinguishedDojo.js
@@ -1,6 +1,6 @@
 const DrawCard = require('../../drawcard');
 const AbilityDsl = require('../../abilitydsl');
-const { TargetModes } = require('../../Constants');
+const { TargetModes, TokenTypes } = require('../../Constants');
 
 class DistinguishedDojo extends DrawCard {
     setupCardAbilities() {
@@ -11,26 +11,23 @@ class DistinguishedDojo extends DrawCard {
             },
             limit: AbilityDsl.limit.perRound(3),
             gameAction: AbilityDsl.actions.addToken(),
-            then: context => {
-                let tokenCount = context.source.getTokenCount('honor');
-                return {
-                    target: {
-                        mode: TargetModes.Select,
-                        activePromptTitle: 'Sacrifice ' + context.source.name + '?',
-                        choices: {
-                            'Yes': AbilityDsl.actions.sacrifice({ target: context.source }),
-                            'No': () => true
-                        }
-                    },
-                    message: '{0} chooses {3}to sacrifice {1}',
-                    messageArgs: context => context.select === 'No' ? 'not ' : '',
-                    then: {
-                        gameAction: AbilityDsl.actions.gainHonor({ amount: tokenCount + 1 }),
-                        message: '{0} uses {1} to gain {3} honor',
-                        messageArgs: [tokenCount + 1]
+            then: context => ({
+                target: {
+                    mode: TargetModes.Select,
+                    activePromptTitle: 'Sacrifice ' + context.source.name + '?',
+                    choices: {
+                        'Yes': AbilityDsl.actions.sacrifice({ target: context.source }),
+                        'No': () => true
                     }
-                };
-            }
+                },
+                message: '{0} chooses {3}to sacrifice {1}',
+                messageArgs: context => context.select === 'No' ? 'not ' : '',
+                then: subThenContext => ({
+                    gameAction: AbilityDsl.actions.gainHonor({ amount: subThenContext.source.getTokenCount(TokenTypes.Honor) }),
+                    message: '{0} uses {1} to gain {3} honor',
+                    messageArgs: [subThenContext.source.getTokenCount(TokenTypes.Honor)]
+                })
+            })
         });
     }
 }
