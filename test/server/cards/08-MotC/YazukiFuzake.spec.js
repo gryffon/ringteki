f5464788a55924c5796fe738b15c7382c21433e3
Kwaice <kwaice@gmail.com>
2019-05-01 11:52:52 +0200

    Added more tests

--- test/server/cards/08-MotC/YazukiFuzake.spec.js
+++ test/server/cards/08-MotC/YazukiFuzake.spec.js
@@ -5,7 +5,8 @@ describe('Yasuki Fuzake', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: ['yasuki-fuzake','eager-scout']
+                        inPlay: ['yasuki-fuzake','eager-scout'],
+                        honor: 10
                     },
                     player2: {
                         inPlay: ['asahina-storyteller', 'doji-challenger', 'callow-delegate'],
@@ -22,18 +23,58 @@ describe('Yasuki Fuzake', function() {
                 this.eagerScout.honor();
                 this.asahinaStoryteller.honor();
                 this.yasukiFuzake.dishonor();
-            });
-
-            it('should trigger when leaving play', function() {
                 this.player1.pass();
                 expect(this.dojiChallenger.isHonored).toBe(true);
                 expect(this.yasukiFuzake.isDishonored).toBe(true);
                 this.player2.clickCard('noble-sacrifice');
                 this.player2.clickCard(this.yasukiFuzake);
                 this.player2.clickCard(this.dojiChallenger);
+            });
+
+            it('should trigger when leaving play', function() {
                 expect(this.player1).toHavePrompt('Triggered Abilities');
                 expect(this.player1).toBeAbleToSelect(this.yasukiFuzake);
-                expect(this.yasukiFuzake.location).not.toBe('dynasty discard pile');
+            });
+
+            it('should prompt to choose up to 1 character controlled by each player', function() {
+                this.player1.clickCard(this.yasukiFuzake);
+                expect(this.player1).toHavePrompt('Choose a character');
+                expect(this.player1).toBeAbleToSelect(this.eagerScout);
+                expect(this.player1).toBeAbleToSelect(this.yasukiFuzake);
+                expect(this.player1).toBeAbleToSelect(this.asahinaStoryteller);
+                expect(this.player1).not.toBeAbleToSelect(this.callowDelegate);
+            });
+
+            it('should allow the player to select only 1 character', function() {
+                this.player1.clickCard(this.yasukiFuzake);
+                this.player1.clickCard(this.eagerScout);
+                this.player1.clickPrompt('Done');
+                expect(this.eagerScout.isHonored).toBe(false);
+                expect(this.yasukiFuzake.location).toBe('dynasty discard pile');
+            });
+
+            it('should not allow the player to select 2 characters controlled by the same player', function() {
+                this.player1.clickCard(this.yasukiFuzake);
+                this.player1.clickCard(this.eagerScout);
+                expect(this.player1).not.toBeAbleToSelect(this.yasukiFuzake);
+                expect(this.player1).toBeAbleToSelect(this.asahinaStoryteller);
+            });
+
+            it('should be able to select himself', function() {
+                this.player1.clickCard(this.yasukiFuzake);
+                this.player1.clickCard(this.yasukiFuzake);
+                this.player1.clickPrompt('Done');
+                expect(this.player1.honor).toBe(10);
+                expect(this.yasukiFuzake.location).toBe('dynasty discard pile');
+            });
+
+            it('should remove the status tokens of chosen characters', function() {
+                this.player1.clickCard(this.yasukiFuzake);
+                this.player1.clickCard(this.eagerScout);
+                this.player1.clickCard(this.asahinaStoryteller);
+                expect(this.eagerScout.isHonored).toBe(false);
+                expect(this.asahinaStoryteller.isHonored).toBe(false);
+                expect(this.yasukiFuzake.location).toBe('dynasty discard pile');
             });
         });
     });
