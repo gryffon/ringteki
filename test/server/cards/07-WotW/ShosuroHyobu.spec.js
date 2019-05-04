c2a2e8228dcf9a03b7bb290899e5bc9e4339dae9
Matthew Griffith <matthew-griffith@protonmail.com>
2019-05-04 14:48:25 -0400

    added test for dupe character and spies at court

--- test/server/cards/07-WotW/ShosuroHyobu.spec.js
+++ test/server/cards/07-WotW/ShosuroHyobu.spec.js
@@ -1,11 +1,41 @@
 describe('Shosuro Hyobu', function() {
     integration(function() {
+        describe('Shosuro\'s ability during dynasty phase', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'dynasty',
+                    player1: {
+                        inPlay: ['shosuro-hyobu']
+                    },
+                    player2: {
+                        inPlay: ['isawa-uona'],
+                        hand: [],
+                        dynastyDiscard: ['isawa-uona']
+                    }
+                });
+
+                this.shosuroHyobu = this.player1.findCardByName('shosuro-hyobu');
+
+                this.dupeUona = this.player2.findCardByName('isawa-uona', location='dynasty discard pile')
+                this.uona = this.player2.findCardByName('isawa-uona', location='play area');
+                this.player2.placeCardInProvince(this.dupeUona, 'province 1');
+            });
+
+            it('should not trigger from discarding a unique character in province row', function() {
+                this.player1.pass();
+                this.player2.clickCard(this.dupeUona);
+                expect(this.player1).not.toHavePrompt('Triggered Abilities');
+
+            });
+        });
+
         describe('Shosuro Hyobu\'s ability', function() {
             beforeEach(function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
-                        inPlay: ['shosuro-hyobu']
+                        inPlay: ['shosuro-hyobu'],
+                        hand: ['spies-at-court']
                     },
                     player2: {
                         inPlay: ['adept-of-the-waves'],
@@ -20,6 +50,21 @@ describe('Shosuro Hyobu', function() {
                 this.oracleOfStone = this.player2.findCardByName('oracle-of-stone');
                 this.assassination = this.player2.findCardByName('assassination');
                 this.heavyBallista = this.player2.placeCardInProvince('heavy-ballista', 'province 1');
+            });
+
+            it('should trigger from spies at court', function() {
+                this.noMoreActions();
+                this.initiateConflict({
+                    attackers: [this.shosuroHyobu],
+                    defenders: [],
+                    type: 'political'
+                });
+                this.noMoreActions();
+                this.player1.clickCard('spies-at-court');
+                this.player1.clickCard(this.shosuroHyobu);
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.shosuroHyobu);
             });
 
             it('should trigger when opponent discards due to a cost (heavy ballista)', function() {
@@ -38,7 +83,7 @@ describe('Shosuro Hyobu', function() {
             it('should trigger when opponent discards due to an effect (oracle of stone)', function() {
                 this.player1.pass();
                 this.player2.clickCard(this.oracleOfStone);
-                expect(this.player1.hand.length).toBe(2);
+                expect(this.player1.hand.length).toBe(3);
                 expect(this.player2.hand.length).toBe(3);
                 expect(this.player1).toHavePrompt('Choose 2 cards to discard');
                 this.player1.clickCard(this.player1.hand[0]);
@@ -58,9 +103,10 @@ describe('Shosuro Hyobu', function() {
                     attackers: [this.shosuroHyobu],
                     defenders: [],
                     ring: 'earth',
-                    type: 'political'
+                    type: 'military'
                 });
                 this.noMoreActions();
+                this.player1.clickPrompt('No');
                 expect(this.player1).toHavePrompt('Earth Ring');
                 this.player1.clickPrompt('Draw a card and opponent discards');
                 expect(this.player1).not.toHavePrompt('Triggered Abilities');
