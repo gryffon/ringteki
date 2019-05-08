509bd32ce0f22795fb10a4b22773daed509d05bb
Kwaice <kwaice@gmail.com>
2019-05-08 20:19:31 +0200

    Changed to AbilityDsl

--- server/game/cards/09.1-FtE/HanteiSotorii.js
+++ server/game/cards/09.1-FtE/HanteiSotorii.js
@@ -1,16 +1,17 @@
 const DrawCard = require('../../drawcard.js');
 const { CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
 
 class HanteiSotorii extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: 'Give a participating character +3 glory',
             condition: context => context.source.isParticipating() && this.game.currentConflict.conflictType === 'military',
             target: {
                 cardType: CardTypes.Character,
                 cardCondition: card => card.isParticipating(),
-                gameAction: ability.actions.cardLastingEffect(() => ({
-                    effect: ability.effects.modifyGlory(3)
+                gameAction: AbilityDsl.actions.cardLastingEffect(() => ({
+                    effect: AbilityDsl.effects.modifyGlory(3)
                 }))
             },
             effect: 'give {0} +3 glory until the end of the conflict'
