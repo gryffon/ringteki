65f3a156809f467beff991e41ef1232d663e72bd
Tom Nys <50174301+The-Outcast@users.noreply.github.com>
2019-05-11 11:03:45 +0200

    BoB: Earth Becomes Sky (#3175)
    * Add Earth Becomes Sky implementation and tests.
    
    * Fix code styling

---
+++ server/game/cards/09.2-BoB/EarthBecomesSky.js
@@ -0,0 +1,22 @@
+import { CardTypes } from '../../Constants.js';
+
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class EarthBecomesSky extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Bow a character that just readied',
+            when: {
+                onCardReadied: (event, context) =>
+                    event.card.type === CardTypes.Character &&
+                    event.card.controller === context.player.opponent
+            },
+            gameAction: AbilityDsl.actions.bow(context => ({ target: context.event.card }))
+        });
+    }
+}
+
+EarthBecomesSky.id = 'earth-becomes-sky';
+
+module.exports = EarthBecomesSky;
