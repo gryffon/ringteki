ebac43691a2187e20fe0ee7540cac5b14c70fe76
Matthew Griffith <matthew-griffith@protonmail.com>
2019-06-03 15:56:41 -0400

    first version of the card with a test

---
+++ test/server/cards/09.1-FtE/FalseLoyalities.spec.js
@@ -0,0 +1,37 @@
+describe('False Loyalites', function() {
+    integration(function() {
+        describe('False Loyalites\'s ability', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['matsu-berserker','shinjo-shono'],
+                        honor: 10
+                    },
+                    player2: {
+                        inPlay: ['venerable-historian'],
+                        hand: ['false-loyalties'],
+                        honor: 8
+                    }
+                });
+
+                this.berserker = this.player1.findCardByName('matsu-berserker');
+                this.shono = this.player1.findCardByName('shinjo-shono');
+
+                this.historian = this.player2.findCardByName('venerable-historian');
+
+                this.noMoreActions();
+            });
+
+            it('should trigger after a opponent wins a conflict', function() {
+                this.initiateConflict({
+                    type: 'political',
+                    attackers: [this.shono]
+                });
+                this.noMoreActions();
+                expect(this.player2).toHavePrompt('Any Reactions');
+                expect(this.player2).toBeAbleToSelect('false-loyalities');
+            });
+        });
+    });
+});
