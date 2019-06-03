499c2fae95151b1d7acc222e8929b39231b4f7c2
Worker-Bee <martin.schroeder@maibornwolff.de>
2019-06-03 09:44:27 +0200

    implemented Hiruma Signaller

---
+++ server/game/cards/09.1-FtE/HirumaSignaller.js
@@ -0,0 +1,24 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { Players, CardTypes } = require('../../Constants');
+
+class HirumaSignaller extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Sacrifice this card to ready and move a character to the conflict',
+            cost: AbilityDsl.costs.sacrificeSelf(),
+            condition: context => context.source.isDefending(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                gameAction: [AbilityDsl.actions.ready(), AbilityDsl.actions.moveToConflict()]
+            },
+            effect: 'ready and move {0} to the conflict'
+        });
+    }
+}
+
+HirumaSignaller.id = 'hiruma-signaller';
+
+module.exports = HirumaSignaller;
+
