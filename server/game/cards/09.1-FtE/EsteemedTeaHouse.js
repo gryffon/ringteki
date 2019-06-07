e11f231b93825a3b7873cc143f689a05c5ca8ba6
Nathan Groover <nathan.groover@gmail.com>
2019-06-07 04:26:59 +0000

    FtE - Esteemed Tea House (#3215)

---
+++ server/game/cards/09.1-FtE/EsteemedTeaHouse.js
@@ -0,0 +1,32 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes, PlayTypes, Players, Durations } = require('../../Constants');
+
+class EsteemedTeaHouse extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Return attachment to owners hand',
+            condition: context => this.game.isDuringConflict() &&
+                    context.player.anyCardsInPlay(card => card.isParticipating() && card.hasTrait('courtier')),
+            target: {
+                cardType: CardTypes.Attachment,
+                cardCondition: card => card.parent && card.parent.isParticipating(),
+                gameAction: AbilityDsl.actions.returnToHand()
+            },
+            gameAction: AbilityDsl.actions.playerLastingEffect(context => ({
+                duration: Durations.UntilEndOfPhase,
+                targetController: Players.Opponent,
+                effect: AbilityDsl.effects.playerCannot({
+                    cannot: PlayTypes.PlayFromHand,
+                    restricts: 'copiesOfX',
+                    params: context.target.name
+                })
+            }))
+        });
+    }
+}
+
+
+EsteemedTeaHouse.id = 'esteemed-tea-house';
+
+module.exports = EsteemedTeaHouse;
