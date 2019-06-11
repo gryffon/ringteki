366b76ac3e24ac981cd70120b4fbb7020df5be9f
Worker-Bee <martin.schroeder@maibornwolff.de>
2019-06-11 17:48:53 +0200

    Introduced AbilityDsl for Bayushi Manipulator, Bayushi Shoju, Bayushi Yunako, Blackmail, Blackmail Artist, Borderlands Defender, Border Rider, Born in War and Brash Samurai

--- server/game/cards/01-Core/BayushiYunako.js
+++ server/game/cards/01-Core/BayushiYunako.js
@@ -1,20 +1,21 @@
 const DrawCard = require('../../drawcard.js');
 const { CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
 
 class BayushiYunako extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: 'Switch a character\'s M and P skill',
             condition: context => context.source.isParticipating(),
             target: {
                 cardType: CardTypes.Character,
                 cardCondition: card => !card.hasDash(),
-                gameAction: ability.actions.cardLastingEffect(context => {
+                gameAction: AbilityDsl.actions.cardLastingEffect(context => {
                     let diff = context.target.baseMilitarySkill - context.target.basePoliticalSkill;
                     return {
                         effect: [
-                            ability.effects.modifyBaseMilitarySkill(-diff),
-                            ability.effects.modifyBasePoliticalSkill(diff)
+                            AbilityDsl.effects.modifyBaseMilitarySkill(-diff),
+                            AbilityDsl.effects.modifyBasePoliticalSkill(diff)
                         ]
                     };
                 })
