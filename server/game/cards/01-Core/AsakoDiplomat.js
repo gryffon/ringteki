e02a9a2f32cdfa44337471304fe1eed0cd24ca76
Worker-Bee <martin.schroeder@maibornwolff.de>
2019-06-11 16:24:33 +0200

    Introduced AbilityDsl to Ambush, Ancestral Lands, Artisan Academy, Asahina Artisan, Asahina Storyteller, Asako Diplomat, Ascetic Visionary and Assassination

--- server/game/cards/01-Core/AsakoDiplomat.js
+++ server/game/cards/01-Core/AsakoDiplomat.js
@@ -1,8 +1,9 @@
 const DrawCard = require('../../drawcard.js');
 const { CardTypes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
 
 class AsakoDiplomat extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.reaction({
             title: 'Honor or dishonor a character',
             when: {
@@ -12,14 +13,14 @@ class AsakoDiplomat extends DrawCard {
             target: {
                 activePromptTitle: 'Choose a character to honor or dishonor',
                 cardType: CardTypes.Character,
-                gameAction: ability.actions.chooseAction({
+                gameAction: AbilityDsl.actions.chooseAction({
                     messages: {
                         'Honor this character': '{0} chooses to honor {1}',
                         'Dishonor this character': '{0} chooses to honor {1}'
                     },
                     choices: {
-                        'Honor this character': ability.actions.honor(),
-                        'Dishonor this character': ability.actions.dishonor()
+                        'Honor this character': AbilityDsl.actions.honor(),
+                        'Dishonor this character': AbilityDsl.actions.dishonor()
                     }
                 })
             }
