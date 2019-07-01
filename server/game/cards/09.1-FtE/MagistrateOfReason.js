bbd701b1e07c0c46ff582f7bf2f64a5a00397e52
James Oddy <james.oddy@adeysteelgroup.co.uk>
2019-07-01 16:12:46 +0100

    Add implementation and tests for Magistrate of Reason (#3288)

---
+++ server/game/cards/09.1-FtE/MagistrateOfReason.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+const { CardTypes, Players } = require('../../Constants');
+
+class MagistrateOfReason extends DrawCard {
+    setupCardAbilities() {
+        this.persistentEffect({
+            condition: context => context.source.isAttacking(),
+            targetController: Players.Opponent,
+            effect: AbilityDsl.effects.additionalCost(context =>
+                context.source.type === CardTypes.Character ? [AbilityDsl.costs.payFateToRing(1)] : []
+            )
+        });
+    }
+}
+
+MagistrateOfReason.id = 'magistrate-of-reason';
+
+module.exports = MagistrateOfReason;
