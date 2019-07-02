e870e87271060c0c2d0d2193dae327de2584b4c5
jeremylarner <31585532+jeremylarner@users.noreply.github.com>
2019-07-02 10:50:03 +0100

    Fix Sententious Poet to use isCardPlayed()

--- server/game/cards/09.1-FtE/SententiousPoet.js
+++ server/game/cards/09.1-FtE/SententiousPoet.js
@@ -1,6 +1,6 @@
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
-const { CardTypes, Stages } = require('../../Constants');
+const { Stages } = require('../../Constants');
 
 class SententiousPoet extends DrawCard {
     setupCardAbilities() {
@@ -8,13 +8,14 @@ class SententiousPoet extends DrawCard {
             title: 'Gain 1 fate',
             when: {
                 onSpendFate: (event, context) =>
-                    event.context.player === context.source.controller.opponent &&
+                    event.context.player === context.player.opponent &&
                     event.amount > 0 &&
                     event.context.stage === Stages.Cost &&
+                    event.context.ability.isCardPlayed() &&
                     context.source.isParticipating(),
                 onMoveFate: (event, context) =>
-                    event.context.source.type === CardTypes.Event &&
-                    event.origin === context.source.controller.opponent &&
+                    event.context.ability.isCardPlayed() &&
+                    event.context.player === context.player.opponent &&
                     event.fate > 0 &&
                     context.source.isParticipating() &&
                     event.context.stage === Stages.Cost &&
