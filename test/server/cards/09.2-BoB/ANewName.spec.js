b72c8554865884fee4ab96e2bb1eabea3c932c79
Nathan Groover <nathan.groover@gmail.com>
2019-07-10 03:03:45 +0000

    BoB - A New Name (#3304)

---
+++ test/server/cards/09.2-BoB/ANewName.spec.js
@@ -0,0 +1,35 @@
+describe('A New Name', function() {
+    integration(function() {
+        describe('A New Name', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['asahina-artisan', 'doji-challenger'],
+                        hand: ['a-new-name']
+                    },
+                    player2: {
+                        inPlay: ['adept-of-the-waves', 'miya-mystic'],
+                    }
+                });
+
+                this.player1.playAttachment('a-new-name', 'asahina-artisan');
+                this.artisan = this.player1.findCardByName('asahina-artisan');
+                this.noMoreActions();
+            });
+
+            it('should have bushi trait', function() {
+                expect(this.artisan.getTraits()).toContain('bushi');
+            });
+
+            it('should have courtier trait', function() {
+                expect(this.artisan.getTraits()).toContain('courtier');
+            });
+
+            it('should have +1/+1', function() {
+                expect(this.artisan.getMilitarySkill()).toBe(1);
+                expect(this.artisan.getPoliticalSkill()).toBe(1);
+            });
+        });
+    });
+});
