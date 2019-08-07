a36e4a246fa587a5fd20d066e586c9b5ccb37ff4
Matt Griffith <matthew-griffith@protonmail.com>
2019-08-07 00:56:15 -0400

    wrote the first version of the card

---
+++ server/game/cards/09.3-JfS/IsawaHeiko.js
@@ -0,0 +1,30 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, Elements } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class IsawaHeiko extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Switch a character\'s M and P skill',
+            when: {
+                onCardPlayed: (event, context) => {
+                    return event.card.hasTrait(Elements.Water) &&
+                        context.game.isDuringConflict() &&
+                        event.player === context.player;
+                }
+            },
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: card => !card.hasDash(),
+                gameAction: AbilityDsl.actions.cardLastingEffect({
+                    effect: AbilityDsl.effects.switchBaseSkills()
+                })
+            },
+            effect: 'switch {0}\'s military and political skill'
+        });
+    }
+}
+
+IsawaHeiko.id = 'isawa-heiko';
+
+module.exports = IsawaHeiko;
