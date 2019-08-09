a6a297b11b12b4850fbd79ec44988d39020e093f
Matthew-Griffith <matthew-griffith@protonmail.com>
2019-08-09 08:32:35 -0400

    JfS - Isawa Heiko (#3406)
    * wrote the first version of the card
    
    * added tests for the card
    
    * added the correct duration and changed the title to say base skills and fixed a typo in one of the tests

---
+++ server/game/cards/09.3-JfS/IsawaHeiko.js
@@ -0,0 +1,31 @@
+const DrawCard = require('../../drawcard.js');
+const { CardTypes, Elements, Durations } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class IsawaHeiko extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Switch a character\'s base skills',
+            when: {
+                onCardPlayed: (event, context) => {
+                    return event.card.hasTrait(Elements.Water) &&
+                        context.game.isDuringConflict() &&
+                        event.player === context.player;
+                }
+            },
+            target: {
+                cardType: CardTypes.Character,
+                cardCondition: card => !card.hasDash() && card.isParticipating(),
+                gameAction: AbilityDsl.actions.cardLastingEffect({
+                    duration: Durations.UntilEndOfPhase,
+                    effect: AbilityDsl.effects.switchBaseSkills()
+                })
+            },
+            effect: 'switch {0}\'s military and political skill'
+        });
+    }
+}
+
+IsawaHeiko.id = 'isawa-heiko';
+
+module.exports = IsawaHeiko;
