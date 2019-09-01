9397a4d41928afcfe6de9a432220c91bf3eaf22d
Tom_Nys <tom-nys@hotmail.com>
2019-09-01 17:40:25 +0200

    Implementation of Doji Hotaru

---
+++ server/game/cards/09.3-JfS/DojiHotaru2.js
@@ -0,0 +1,35 @@
+const DrawCard = require('../../drawcard.js');
+const { TargetModes } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl');
+
+class DojiHotaru2 extends DrawCard {
+    setupCardAbilities() {
+        this.persistentEffect({
+            effect: AbilityDsl.effects.delayedEffect({
+                condition:  context => context.player && !!context.player.cardsInPlay.includes(card => card.name === 'Doji Kuwanan'),
+                message: '{1} is discarded from play as its controller controls {0}',
+                messageArgs: context => [context.source, context.player.cardsInPlay.find(card => card.name === 'Doji Kuwanan')],
+                gameAction: AbilityDsl.actions.discardFromPlay(context => ({
+                    target: context.player.cardsInPlay.find(card => card.name === 'Doji Kuwanan')
+                }))
+            })
+        });
+        this.reaction({
+            title: 'Gain 1 honor',
+            when: {
+                onCardPlayed: (event, context) => {
+                    return context.game.isDuringConflict() &&
+                        context.source.isParticipating() &&
+                        event.player === context.player.opponent;
+                }
+            },
+            gameAction: AbilityDsl.actions.gainHonor(),
+            limit: AbilityDsl.limit.unlimitedPerConflict(),
+            effect: 'gain 1 honor'
+        });
+    }
+}
+
+DojiHotaru2.id = 'doji-hotaru-2';
+
+module.exports = DojiHotaru2;
