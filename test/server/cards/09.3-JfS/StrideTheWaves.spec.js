b489a633333f3967b37cc66fd7859864f381b381
Tom_Nys <tom-nys@hotmail.com>
2019-09-01 15:23:04 +0200

    - fix review remarks. - add additional test for only friendly attach.

--- test/server/cards/09.3-JfS/StrideTheWaves.spec.js
+++ test/server/cards/09.3-JfS/StrideTheWaves.spec.js
@@ -1,4 +1,4 @@
-describe('Isawa Heiko', function () {
+describe('Stride the waves', function () {
     integration(function () {
         describe('Stride the waves\' ability', function () {
             beforeEach(function () {
@@ -20,8 +20,6 @@ describe('Isawa Heiko', function () {
 
                 this.player1.claimRing('water');
 
-                this.player1.playAttachment(this.strideTheWaves, this.solemnScholar);
-
                 this.noMoreActions();
                 this.initiateConflict({
                     type: 'political',
@@ -30,7 +28,17 @@ describe('Isawa Heiko', function () {
                 });
             });
 
+            it('should not be allowed to be attached to an opponents character', function() {
+                this.player2.pass();
+                this.player1.clickCard(this.strideTheWaves);
+
+                expect(this.player1).not.toBeAbleToSelect(this.akodoKage);
+                expect(this.player1).toBeAbleToSelect(this.solemnScholar);
+            });
+
             it('should allow you to move from and back to the conflict', function() {
+                this.player2.pass();
+                this.player1.playAttachment(this.strideTheWaves, this.solemnScholar);
                 this.player2.pass();
 
                 expect(this.player1).toHavePrompt('Conflict Action Window');
