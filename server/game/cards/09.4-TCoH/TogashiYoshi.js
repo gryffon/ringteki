c7e4cbdc4f77bebbb542bfb3c0616676050aed7e
Tom_Nys <tom-nys@hotmail.com>
2019-09-04 01:03:23 +0200

    - Implementation of Togashi Yoshi

---
+++ server/game/cards/09.4-TCoH/TogashiYoshi.js
@@ -0,0 +1,32 @@
+import { TargetModes } from '../../Constants.js';
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes } = require('../../Constants');
+
+class TogashiYoshi extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Gain 1 fate of an unclaimed ring',
+            when: {
+                afterConflict: (event, context) => event.conflict.winner === context.player &&
+                    context.source.isParticipating()
+            },
+            target: {
+                mode: TargetModes.Ring,
+                ringCondition:  ring => ring.fate >= 1 &&
+                    ring.contested === false &&
+                    ring.claimed === false
+            },
+            effect: 'gain 1 fate from the {1}',
+            effectArgs: context => context.ring,
+            gameAction: AbilityDsl.actions.takeFateFromRing(context => ({
+                target: context.ring
+            }))
+        });
+    }
+}
+
+TogashiYoshi.id = 'togashi-yoshi';
+
+module.exports = TogashiYoshi;
+
