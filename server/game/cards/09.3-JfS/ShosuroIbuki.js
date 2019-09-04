52cdaef0d28bf9673d0a87b837f6f2eb1ced2c17
Tom Nys <50174301+The-Outcast@users.noreply.github.com>
2019-09-04 12:03:13 +0200

    JfS - Shosuro Ibuki (#3410)
    * - Implement Shosuro Ibuki
    
    * - triple equals checks duh!
    
    * - Removed unnecessary code

---
+++ server/game/cards/09.3-JfS/ShosuroIbuki.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class ShosuroIbuki extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Remove one fate from each other participating character',
+            when: {
+                afterConflict: (event, context) =>
+                    event.conflict.winner === context.player &&
+                    context.source.isParticipating()
+            },
+            gameAction: AbilityDsl.actions.removeFate(context => ({
+                target: context.game.currentConflict.getParticipants(participant => participant !== context.source)
+            })),
+            effect: 'remove one fate from each other participating character'
+        });
+    }
+}
+
+ShosuroIbuki.id = 'shosuro-ibuki';
+
+module.exports = ShosuroIbuki;
