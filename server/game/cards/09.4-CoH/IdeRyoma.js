f7097ae493956ba02238be8eda346f3b3da2f9b5
Matt Grifith <matthew-griffith@protonmail.com>
2019-09-10 18:29:28 -0400

    added the check for one of the characters to be unicorn and the other to not be

--- server/game/cards/09.4-CoH/IdeRyoma.js
+++ server/game/cards/09.4-CoH/IdeRyoma.js
@@ -5,7 +5,7 @@ const AbilityDsl = require('../../abilitydsl');
 class IdeRyoma extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'choose one character to bow and one to ready',
+            title: 'Choose one character to bow and one to ready',
             condition: (context) => context.source.isParticipating(),
             targets: {
                 bowedCharacter: {
@@ -19,7 +19,18 @@ class IdeRyoma extends DrawCard {
                     activePromptTitle: 'Choose a character to bow controlled by the same player',
                     cardType: CardTypes.Character,
                     controller: Players.Any,
-                    cardCondition: (card, context) => !card.bowed && card.controller === context.target.bowedCharacter.controller,
+                    cardCondition: (card, context) => {
+                        let bowedChar = context.targets.bowedCharacter;
+                        if(!card.bowed && card.controller === bowedChar.controller) {
+                            if(bowedChar.isFaction('unicorn') && !card.isFaction('unicorn')) {
+                                return true;
+                            } else if(!bowedChar.isFaction('unicorn') && card.isFaction('unicorn')) {
+                                return true;
+                            }
+                            return false;
+                        }
+                        return false;
+                    },
                     gameAction: AbilityDsl.actions.joint([
                         AbilityDsl.actions.ready(context => ({ target: context.targets.bowedCharacter })),
                         AbilityDsl.actions.bow()
