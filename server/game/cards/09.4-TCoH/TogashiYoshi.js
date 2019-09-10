b05a1a50c56c8d3daaedcf31c27d363a004be505
Tom_Nys <tom-nys@hotmail.com>
2019-09-10 21:22:45 +0200

    - Fix typo in title and use SelectRingAction with GainFateFromRing instead of targeting as card doesn't mention choose.

--- server/game/cards/09.4-TCoH/TogashiYoshi.js
+++ server/game/cards/09.4-TCoH/TogashiYoshi.js
@@ -5,21 +5,19 @@ const AbilityDsl = require('../../abilitydsl');
 class TogashiYoshi extends DrawCard {
     setupCardAbilities() {
         this.reaction({
-            title: 'Gain 1 fate of an unclaimed ring',
+            title: 'Gain 1 fate from an unclaimed ring',
             when: {
                 afterConflict: (event, context) => event.conflict.winner === context.player &&
                     context.source.isParticipating()
             },
-            target: {
-                mode: TargetModes.Ring,
-                ringCondition:  ring => ring.fate >= 1 &&
-                    ring.contested === false &&
-                    ring.claimed === false
-            },
             effect: 'gain 1 fate from the {1}',
             effectArgs: context => context.ring,
-            gameAction: AbilityDsl.actions.takeFateFromRing(context => ({
-                target: context.ring
+            gameAction: AbilityDsl.actions.selectRing(context => ({
+                ringCondition:  ring => ring.fate >= 1 &&
+                    ring.contested === false &&
+                    ring.claimed === false,
+                target: context.ring,
+                gameAction: AbilityDsl.actions.takeFateFromRing()
             }))
         });
     }
