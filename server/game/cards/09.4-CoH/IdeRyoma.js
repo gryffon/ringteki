f3d381d9df90ac0e11d666bffdf84fdafd231171
Matt Grifith <matthew-griffith@protonmail.com>
2019-09-10 22:08:54 -0400

    working to change the charater to be selected by prompts

--- server/game/cards/09.4-CoH/IdeRyoma.js
+++ server/game/cards/09.4-CoH/IdeRyoma.js
@@ -8,20 +8,19 @@ class IdeRyoma extends DrawCard {
             title: 'Choose one character to bow and one to ready',
             condition: (context) => context.source.isParticipating(),
             targets: {
-                bowedCharacter: {
+                toBeReadied: {
                     activePromptTitle: 'Choose a character to ready',
                     cardType: CardTypes.Character,
-                    controller: Players.Any,
-                    cardCondition: card => card.bowed
+                    controller: Players.Any
                 },
-                readiedCharacter: {
-                    dependsOn: 'bowedCharacter',
+                toBeBowed: {
+                    dependsOn: 'toBeReadied',
                     activePromptTitle: 'Choose a character to bow controlled by the same player',
                     cardType: CardTypes.Character,
                     controller: Players.Any,
                     cardCondition: (card, context) => {
-                        let bowedChar = context.targets.bowedCharacter;
-                        if(!card.bowed && card.controller === bowedChar.controller) {
+                        let bowedChar = context.targets.toBeReadied;
+                        if(card.controller === bowedChar.controller) {
                             if(bowedChar.isFaction('unicorn') && !card.isFaction('unicorn')) {
                                 return true;
                             } else if(!bowedChar.isFaction('unicorn') && card.isFaction('unicorn')) {
@@ -32,13 +31,13 @@ class IdeRyoma extends DrawCard {
                         return false;
                     },
                     gameAction: AbilityDsl.actions.joint([
-                        AbilityDsl.actions.ready(context => ({ target: context.targets.bowedCharacter })),
+                        AbilityDsl.actions.ready(context => ({ target: context.targets.toBeReadied })),
                         AbilityDsl.actions.bow()
                     ])
                 }
             },
             effect: 'readies {1} and bows {2}',
-            effectArgs: context => [context.targets.bowedCharacter, context.targets.readiedCharacter]
+            effectArgs: context => [context.targets.toBeReadied, context.targets.toBeBowed]
         });
     }
 }
