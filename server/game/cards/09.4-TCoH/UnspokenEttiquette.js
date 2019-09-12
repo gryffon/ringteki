97f24062dc413cd9ee578e727b90bcf8d8fda0c6
Tom Nys <tom-nys@hotmail.com>
2019-09-12 15:01:54 +0200

    Resolve review comments

--- server/game/cards/09.4-TCoH/UnspokenEttiquette.js
+++ server/game/cards/09.4-TCoH/UnspokenEttiquette.js
@@ -6,7 +6,7 @@ class UnspokenEtiquette extends DrawCard {
         this.action({
             title: 'Dishonor each participating non-courtier',
             effect: 'dishonor each participating non-courtier.',
-            condition: context => context.game.currentConflict && context.game.currentConflict.conflictType === 'political',
+            condition: context => context.game.isDuringConflict('political'),
             gameAction: AbilityDsl.actions.dishonor(context => ({
                 target: context.game.currentConflict.getParticipants(card => !card.hasTrait('courtier')),
             }))
