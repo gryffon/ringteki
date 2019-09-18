662f4bf8988d78da287158743d5d820a746cdae3
Tom Nys <tom-nys@hotmail.com>
2019-09-18 13:43:51 +0200

    - Clean Vanguard Warrior use of ability instead of AbilityDsl

--- server/game/cards/01-Core/VanguardWarrior.js
+++ server/game/cards/01-Core/VanguardWarrior.js
@@ -1,10 +1,9 @@
 const AbilityDsl = require('../../abilitydsl.js');
-
 const DrawCard = require('../../drawcard.js');
 const { CardTypes } = require('../../Constants');
 
 class VanguardWarrior extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: 'Sacrifice to put fate on one character',
             cost: AbilityDsl.costs.sacrificeSelf(),
