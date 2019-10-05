6e6544727a0e024b56b04a5144c79ecd4226d7bd
Tom Nys <tom-nys@hotmail.com>
2019-10-06 01:52:38 +0200

    TEL - Ardent Omoidasu - Implement Ardent Omoidasu

---
+++ server/game/cards/10-TEL/ArdentOmoidasu.js
@@ -0,0 +1,21 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+
+class ArdentOmoidasu extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Steal 2 honor',
+            when: {
+                onCardDishonored: (event, context) => event.card.controller == context.player
+                    && event.context.player === context.player.opponent
+            },
+            gameAction: AbilityDsl.actions.takeHonor({
+                amount: 2
+            })
+        });
+    }
+}
+
+ArdentOmoidasu.id = 'ardent-omoidasu';
+
+module.exports = ArdentOmoidasu;
---
+++ test/server/cards/10-TEL/ArdentOmoidasu.spec.js
@@ -0,0 +1,114 @@
+describe('Ardent Omoidasu', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['ardent-omoidasu'],
+                    honor: 10
+                },
+                player2: {
+                    inPlay: ['mirumoto-raitsugu'],
+                    hand: ['way-of-the-scorpion'],
+                    honor: 10,
+                    provinces: ['shameful-display']
+                }
+            });
+
+            this.ardentOmoidasu = this.player1.findCardByName('ardent-omoidasu');
+
+            this.mirumotoRaitsugu = this.player2.findCardByName('mirumoto-raitsugu');
+            this.wayOfTheScorpion = this.player2.findCardByName('way-of-the-scorpion');
+            this.shamefulDisplay = this.player2.findCardByName('shameful-display', 'province 1');
+        });
+
+        it('should allow to trigger on opponents dishonoring your characters with events', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                type: 'political',
+                attackers: [this.ardentOmoidasu],
+                defenders: [this.mirumotoRaitsugu],
+                province: this.shamefulDisplay,
+                ring: 'fire'
+            });
+
+            this.player2.clickCard(this.wayOfTheScorpion);
+            this.player2.clickCard(this.ardentOmoidasu);
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            expect(this.player1).toBeAbleToSelect(this.ardentOmoidasu);
+
+            this.player1.clickCard(this.ardentOmoidasu);
+            expect(this.player1.honor).toBe(12);
+            expect(this.player2.honor).toBe(8);
+        });
+
+        it('should allow to trigger on opponents dishonoring your characters with a card ability', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                type: 'political',
+                attackers: [this.ardentOmoidasu],
+                defenders: [this.mirumotoRaitsugu],
+                province: this.shamefulDisplay,
+                ring: 'fire'
+            });
+            
+            this.player2.clickCard(this.shamefulDisplay);
+            this.player2.clickCard(this.ardentOmoidasu);
+            this.player2.clickCard(this.mirumotoRaitsugu);
+            this.player2.clickPrompt('Done');
+            this.player2.clickPrompt('honor');
+            this.player2.clickCard(this.mirumotoRaitsugu);
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            expect(this.player1).toBeAbleToSelect(this.ardentOmoidasu);
+
+            this.player1.clickCard(this.ardentOmoidasu);
+            expect(this.player1.honor).toBe(12);
+            expect(this.player2.honor).toBe(8);
+        });
+
+        it('should not allow to trigger when the controller itself dishonors him', function() {
+            this.noMoreActions();
+            this.player1.passConflict();
+            this.noMoreActions();
+            
+            this.initiateConflict({
+                type: 'military',
+                defenders: [this.ardentOmoidasu],
+                attackers: [this.mirumotoRaitsugu],
+                ring: 'fire'
+            });
+            
+            this.noMoreActions();
+
+            this.player2.clickCard(this.ardentOmoidasu);
+            this.player2.clickPrompt('Dishonor Ardent Omoidasu');
+
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            expect(this.player1).toBeAbleToSelect(this.ardentOmoidasu);
+
+            this.player1.clickCard(this.ardentOmoidasu);
+            expect(this.player1.honor).toBe(12);
+            expect(this.player2.honor).toBe(8);
+        });
+
+        it('should not allow to trigger when the controller itself dishonors him', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                type: 'political',
+                attackers: [this.ardentOmoidasu],
+                defenders: [this.mirumotoRaitsugu],
+                province: this.shamefulDisplay,
+                ring: 'fire'
+            });
+
+            this.noMoreActions();
+
+            this.player1.clickCard(this.ardentOmoidasu);
+            this.player1.clickPrompt('Dishonor Ardent Omoidasu');
+
+            expect(this.player1).toHavePrompt('Action Window');
+        });
+    });
+});
