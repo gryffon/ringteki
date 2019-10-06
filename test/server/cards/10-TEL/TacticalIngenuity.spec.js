583e3424c82f9667c5bb817bce947fc931bfae54
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2019-10-07 00:51:09 +0200

    TEL - Tacitical Ingenuity
    - Implement Tactical Ingenuity
    - Tests for Tactical Ingenuity

---
+++ test/server/cards/10-TEL/TacticalIngenuity.spec.js
@@ -0,0 +1,48 @@
+describe('Tactical Ingenuity', function() {
+    integration(function() {
+        beforeEach(function() {
+            this.setupTest({
+                phase: 'conflict',
+                player1: {
+                    inPlay: ['akodo-zentaro', 'matsu-berserker'],
+                    hand: ['tactical-ingenuity'],
+                    conflictDeck: ['way-of-the-lion', 'fine-katana', 'strength-in-numbers', 'censure'],
+                    conflictDeckSize: 4
+                }
+            });
+
+            this.zentaro = this.player1.findCardByName('akodo-zentaro');
+            this.matsuBerseker = this.player1.findCardByName('matsu-berserker');
+            this.tacticalIngenuity = this.player1.findCardByName('tactical-ingenuity');
+            this.wayOfTheLion = this.player1.findCardByName('way-of-the-lion', 'conflict deck')
+        });
+
+        it('should only be allowed to trigger in a conflict', function() {
+            this.player1.playAttachment(this.tacticalIngenuity, this.zentaro);
+            this.player2.pass();
+
+            this.player1.clickCard(this.zentaro);
+            expect(this.player1).toHavePrompt('Action Window');
+        });
+
+        it('should allow you to look at the top 4 and draw an event', function() {
+            this.player1.playAttachment(this.tacticalIngenuity, this.zentaro);
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.zentaro],
+                defenders: []
+            });
+
+            this.player2.pass();
+            this.player1.clickCard(this.zentaro);
+            expect(this.player1).toHavePrompt('Select a card to reveal and put in your hand');
+            expect(this.player1).toHavePromptButton('Way of the Lion');
+            expect(this.player1).toHaveDisabledPromptButton('Fine Katana');
+            expect(this.player1).toHavePromptButton('Strength in Numbers');
+            expect(this.player1).toHavePromptButton('Censure');
+
+            this.player1.clickPrompt('Way of the Lion');
+            expect(this.wayOfTheLion.location).toBe('hand');
+        });
+    });
+});
