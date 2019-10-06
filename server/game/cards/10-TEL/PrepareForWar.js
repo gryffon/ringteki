ce39dcaa8eb0705726d1058b4d06720491f407da
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2019-10-06 19:49:58 +0200

    Update Prepare for War
    Made sure the option to discard the statustoken is presented.

--- server/game/cards/10-TEL/PrepareForWar.js
+++ server/game/cards/10-TEL/PrepareForWar.js
@@ -10,32 +10,36 @@ class PrepareForWar extends DrawCard {
             target: {
                 cardType: CardTypes.Character,
                 controller: Players.Self,
-                gameAction: AbilityDsl.actions.multiple([
-                    AbilityDsl.actions.selectCard(context => ({
-                        mode: TargetModes.Unlimited,
-                        cardType: CardTypes.Attachment,
-                        controller: Players.Self,
-                        cardCondition: card => card.parent === context.target,
-                        activePromptTitle: 'Choose any amount of attachments',
-                        optional: true,
-                        gameAction: AbilityDsl.actions.discardCard(),
-                        message: '{0} chooses to discard {1} from {2}',
-                        messageArgs: cards => [context.player, cards.length === 0 ? 'nothing' : cards, context.target]
-                    })),
-                    AbilityDsl.actions.menuPrompt(context => ({
-                        activePromptTitle: 'Do you wish to discard the status token?',
-                        choices: ['Yes', 'No'],
-                        choiceHandler: (choice, displayMessage) => {
-                            if(displayMessage && choice === 'Yes') {
-                                this.game.addMessage('{0} chooses to discard the status token from {1}', context.player, context.target);
-                            };
+                gameAction: AbilityDsl.actions.sequential([
+                    AbilityDsl.actions.multiple([
+                        AbilityDsl.actions.selectCard(context => ({
+                            mode: TargetModes.Unlimited,
+                            cardType: CardTypes.Attachment,
+                            controller: Players.Self,
+                            cardCondition: card => card.parent === context.target,
+                            activePromptTitle: 'Choose any amount of attachments',
+                            optional: true,
+                            gameAction: AbilityDsl.actions.discardCard(),
+                            message: '{0} chooses to discard {1} from {2}',
+                            messageArgs: cards => [context.player, cards.length === 0 ? 'no attachments' : cards, context.target]
+                        })),
+                        AbilityDsl.actions.menuPrompt(context => ({
+                            activePromptTitle: 'Do you wish to discard the status token?',
+                            choices: ['Yes', 'No'],
+                            choiceHandler: (choice, displayMessage) => {
+                                if(displayMessage && choice === 'Yes') {
+                                    this.game.addMessage('{0} chooses to discard the status token from {1}', context.player, context.target);
+                                };
 
-                            return { target: (choice === 'Yes' ? context.target : []) };
-                        },
-                        player: Players.Self,
-                        gameAction: AbilityDsl.actions.discardStatusToken()
-                    })),
-                    AbilityDsl.actions.honor(context => ({ target: context.target.hasTrait('commander') ? context.target : [] }))
+                                return { target: (choice === 'Yes' ? context.target.personalHonor : []) };
+                            },
+                            player: Players.Self,
+                            gameAction: AbilityDsl.actions.discardStatusToken()
+                        }))
+                    ]),
+                    AbilityDsl.actions.honor(context => ({
+                        target: context.target.hasTrait('commander') ? context.target : []
+                    }))
                 ])
             }
         })
