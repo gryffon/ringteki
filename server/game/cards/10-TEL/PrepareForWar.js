bc86572e39dd8c5d8be14c1651c967297fe946cb
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2019-10-06 18:47:43 +0200

    TEL - Prepare for War
    - w.i.p. implementation Prepare for War

---
+++ server/game/cards/10-TEL/PrepareForWar.js
@@ -0,0 +1,47 @@
+import { CardTypes, Players, TargetModes } from '../../Constants.js';
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
+
+
+class PrepareForWar extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: "Remove honor token and any attachment",
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                gameAction: AbilityDsl.actions.multiple([
+                    AbilityDsl.actions.selectCard(context => ({
+                        mode: TargetModes.Unlimited,
+                        cardType: CardTypes.Attachment,
+                        controller: Players.Self,
+                        cardCondition: card => card.parent === context.target,
+                        activePromptTitle: 'Choose any amount of attachments',
+                        optional: true,
+                        gameAction: AbilityDsl.actions.discardCard(),
+                        message: '{0} chooses to discard {1} from {2}',
+                        messageArgs: cards => [context.player, cards.length === 0 ? 'nothing' : cards, context.target]
+                    })),
+                    AbilityDsl.actions.menuPrompt(context => ({
+                        activePromptTitle: 'Do you wish to discard the status token?',
+                        choices: ['Yes', 'No'],
+                        choiceHandler: (choice, displayMessage) => {
+                            if(displayMessage && choice === 'Yes') {
+                                this.game.addMessage('{0} chooses to discard the status token from {1}', context.player, context.target);
+                            };
+
+                            return { target: (choice === 'Yes' ? context.target : []) };
+                        },
+                        player: Players.Self,
+                        gameAction: AbilityDsl.actions.discardStatusToken()
+                    })),
+                    AbilityDsl.actions.honor(context => ({ target: context.target.hasTrait('commander') ? context.target : [] }))
+                ])
+            }
+        })
+    }
+}
+
+PrepareForWar.id = 'prepare-for-war';
+
+module.exports = PrepareForWar;
