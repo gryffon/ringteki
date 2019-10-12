11979ce96b45ed161539c280a35c597c368bd46a
Tim van der Staaij <git@tim.vanderstaaij.email>
2019-10-12 12:21:25 +0200

    Implement Those Who Serve

---
+++ server/game/cards/10-TEL/ThoseWhoServe.js
@@ -0,0 +1,26 @@
+const DrawCard = require('../../drawcard');
+const {CardTypes, Durations, Phases, PlayTypes} = require('../../Constants');
+
+class ThoseWhoServe extends DrawCard {
+    setupCardAbilities(ability) {
+        this.action({
+            title: 'Reduce the cost of your characters by 1 this phase',
+            condition: context => context.game.currentPhase === Phases.Dynasty,
+            effect: 'reduce the cost of their characters by 1 this phase',
+            gameAction: ability.actions.playerLastingEffect({
+                duration: Durations.UntilEndOfPhase,
+                effect: ability.effects.reduceCost({
+                    // RRG does not allow characters to be played from hand in dynasty, but does not use the term "cannot" for this,
+                    // so future card effects could nullify this restriction. In this case conflict characters would work with Those Who Serve.
+                    playingTypes: [PlayTypes.PlayFromHand, PlayTypes.PlayFromProvince],
+                    match: card => card.type === CardTypes.Character,
+                    amount: 1
+                })
+            })
+        });
+    }
+}
+
+ThoseWhoServe.id = 'those-who-serve';
+
+module.exports = ThoseWhoServe;
