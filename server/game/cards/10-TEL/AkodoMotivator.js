66839c16647df05a156b8b7a475470f747b4a418
Tim van der Staaij <git@tim.vanderstaaij.email>
2019-10-16 20:59:13 +0200

    Implement Akodo Motivator

---
+++ server/game/cards/10-TEL/AkodoMotivator.js
@@ -0,0 +1,27 @@
+const _ = require('underscore');
+const DrawCard = require('../../drawcard');
+const AbilityDsl = require('../../abilitydsl');
+
+class AkodoMotivator extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Opponent discards an equal number of cards at random',
+            when: {
+                onCardsDiscardedFromHand: (event, context) => {
+                    const discardedFromOwnHand = (event.player === context.player);
+                    const discardedByOpponentsEffect = (event.player === event.context.player.opponent);
+                    const discardedByRingEffect = (event.context.source.type === 'ring');
+                    const discardedByCardEffect = event.context.ability.isCardAbility();
+                    return discardedFromOwnHand && discardedByOpponentsEffect && (discardedByRingEffect || discardedByCardEffect);
+                }
+            },
+            gameAction: AbilityDsl.actions.discardAtRandom(context => ({
+                amount: _.isArray(context.event.cards) ? context.event.cards.length : context.event.amount
+            }))
+        });
+    }
+}
+
+AkodoMotivator.id = 'akodo-motivator';
+
+module.exports = AkodoMotivator;
