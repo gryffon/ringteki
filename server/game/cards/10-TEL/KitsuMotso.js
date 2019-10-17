53418bb9c999e7d668a27b93edd86fee9f0c9755
Tom Nys <tom-nys@hotmail.com>
2019-10-17 23:32:41 +0200

    Fix review remarks: - remove redundant isDuringConflict - remove activePromptitle

--- server/game/cards/10-TEL/KitsuMotso.js
+++ server/game/cards/10-TEL/KitsuMotso.js
@@ -6,12 +6,10 @@ class KitsuMotso extends DrawCard {
     setupCardAbilities() {
         this.action({
             title: 'Move a character in',
-            condition: context => context.game.isDuringConflict()
-                && context.source.isParticipating()
+            condition: context => context.source.isParticipating()
                 && context.player.hand.size() < context.player.opponent.hand.size(),
             target: {
                 cardType: CardTypes.Character,
-                activePromptTitle: 'Choose a character to move in',
                 controller: Players.Opponent,
                 gameAction: AbilityDsl.actions.moveToConflict()
             }
