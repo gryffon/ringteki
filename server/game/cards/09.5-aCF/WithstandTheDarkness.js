e0afa6d3a0e3ab5c77d9bae878a159eb1b82556d
a <a>
2019-10-19 10:09:41 -0500

    Getting closer

--- server/game/cards/09.5-aCF/WithstandTheDarkness.js
+++ server/game/cards/09.5-aCF/WithstandTheDarkness.js
@@ -6,27 +6,22 @@ class WithstandTheDarkness extends DrawCard {
     setupCardAbilities() {
         this.reaction({
             when: {
-                onCardPlayed: (event, context) => (
-                    // event.player === context.player.opponent &&
-                    event.card.type === CardTypes.Event //&& 
-                    // event.cardTargets &&
-                    // event.cardTargets.some(card => (
-                    //     card.type === CardTypes.Character &&
-                    //     card.isFaction('crab') && 
-                    //     card.controller === context.player && 
-                    //     card.location === Locations.PlayArea))
+                onInitiateAbilityEffects: (event, context) => (
+                    event.card.controller === context.player.opponent &&
+                    event.card.type === CardTypes.Event && 
+                    event.cardTargets.some(card => (
+                        card.type === CardTypes.Character &&
+                        card.isFaction('crab') && 
+                        card.controller === context.player && 
+                        card.location === Locations.PlayArea))
                 )},
             title: 'Place a fate on a character',
             target: {
                 activePromptTitle: 'Choose a character to receive a fate.',
                 cardType: CardTypes.Character,
-                location: Locations.PlayArea,
                 controller: Players.Self,
-                // cardCondition: (card, event, context) => (
-                //     card.isFaction('crab') && 
-                //     card.controller === context.player && 
-                //     card.location === Locations.PlayArea &&
-                //     event.cardTargets.includes(card)),
+                cardCondition: card => card.isFaction('crab'), 
+                    // event.cardTargets.includes(card)),
                 gameAction: AbilityDsl.actions.placeFate()
             },
         });
--- test/server/cards/09.5-aCF/WithstandTheDarkness.spec.js
+++ test/server/cards/09.5-aCF/WithstandTheDarkness.spec.js
@@ -47,6 +47,13 @@ describe('Withstand The Darkness', function() {
                 expect(this.player1).toHavePrompt('Conflict Action Window');
             });
 
+            it('should react when a crab and non-crab character you control is targetted by an event', function() {
+                this.player2.clickCard(this.wayOfTheScorpion);
+                this.player2.clickCard(this.crisisBreaker);
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.withstandTheDarkness);
+            });
+
             it('should add a fate after a crab character you control is targetted by an event', function() {
                 this.player2.clickCard(this.wayOfTheScorpion);
                 this.player2.clickCard(this.crisisBreaker);
@@ -56,6 +63,79 @@ describe('Withstand The Darkness', function() {
                 this.player1.clickCard(this.crisisBreaker);
                 expect(this.crisisBreaker.fate).toBe(1);
                 expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+        });
+
+        describe('Withstand The Darkness\'s Reaction (Multiple Targets)', function() {
+            beforeEach(function() {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['eager-scout', 'hiruma-skirmisher', 'brash-samurai'],
+                        hand: ['withstand-the-darkness']
+                    },
+                    player2: {
+                        inPlay: ['eager-scout'],
+                        hand: ['unfulfilled-duty']
+                    }
+                });
+                this.eagerScout = this.player1.findCardByName('eager-scout');
+                this.hirumaSkirmisher = this.player1.findCardByName('hiruma-skirmisher');
+                this.brashSamurai = this.player1.findCardByName('brash-samurai');
+                this.withstandTheDarkness = this.player1.findCardByName('withstand-the-darkness');
+
+                this.eagerScoutP2 = this.player2.findCardByName('eager-scout');
+                this.unfulfilledDuty = this.player2.findCardByName('unfulfilled-duty');
+
+                this.eagerScout.fate = 0;
+                this.hirumaSkirmisher.fate = 0;
+                this.brashSamurai.fate = 0;
+                this.eagerScoutP2.fate = 0;
+
+                this.eagerScout.bowed = true;
+                this.hirumaSkirmisher.bowed = true;
+                this.brashSamurai.bowed = true;
+                this.eagerScoutP2.bowed = true;
+
+                this.player1.pass();
+                this.player2.clickCard(this.unfulfilledDuty);
+            });
+
+            it('should react when multiple crab characters you control are targetted by an event', function() {
+                this.player2.clickCard(this.eagerScout);
+                this.player2.clickCard(this.hirumaSkirmisher);
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.withstandTheDarkness);
+            });
+
+            it('should let you select only targetted crab characters you control', function() {
+                this.player2.clickCard(this.eagerScout);
+                this.player2.clickCard(this.hirumaSkirmisher);
+                this.player2.clickCard(this.brashSamurai);
+                this.player2.clickCard(this.eagerScoutP2);
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.withstandTheDarkness);
+                this.player1.clickCard(this.withstandTheDarkness);
+                expect(this.player1).toBeAbleToSelect(this.eagerScout);
+                expect(this.player1).toBeAbleToSelect(this.hirumaSkirmisher);
+                expect(this.player1).not.toBeAbleToSelect(this.brashSamurai);
+                expect(this.player1).not.toBeAbleToSelect(this.eagerScoutP2);
+            });
+
+            it('should not let you select crab characters you control that were not targetted', function() {
+                this.player2.clickCard(this.eagerScout);
+                this.player2.clickCard(this.brashSamurai);
+                this.player2.clickCard(this.eagerScoutP2);
+                this.player2.clickPrompt('Done');
+                expect(this.player1).toHavePrompt('Triggered Abilities');
+                expect(this.player1).toBeAbleToSelect(this.withstandTheDarkness);
+                this.player1.clickCard(this.withstandTheDarkness);
+                expect(this.player1).toBeAbleToSelect(this.eagerScout);
+                expect(this.player1).not.toBeAbleToSelect(this.hirumaSkirmisher);
+                expect(this.player1).not.toBeAbleToSelect(this.brashSamurai);
+                expect(this.player1).not.toBeAbleToSelect(this.eagerScoutP2);
             });
         });
     });
