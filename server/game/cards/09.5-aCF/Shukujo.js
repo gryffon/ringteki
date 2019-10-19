67e249efc1258bdcc521315cfd03514b61ab79a7
a <a>
2019-10-18 19:55:39 -0500

    Base implementation for Shukujo.

---
+++ server/game/cards/09.5-aCF/Shukujo.js
@@ -0,0 +1,29 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { AbilityTypes } = require('../../Constants');
+
+class Shukujo extends DrawCard {
+    setupCardAbilities(ability) {
+        this.grantedAbilityLimits = {};
+        this.whileAttached({
+            match: card => card.hasTrait('champion'),
+            effect: AbilityDsl.effects.gainAbility(AbilityTypes.Action, {
+                title: 'Switch the conflict type',
+                condition: context => context.source.isParticipating(),
+                printedAbility: false,
+                gameAction: AbilityDsl.actions.switchConflictType()
+            })
+        });
+    }
+
+    canAttach(card, context) {
+        if(card.controller !== context.player) {
+            return false;
+        }
+        return card.isUnique() && card.isFaction('crane') ? super.canAttach(card, context) : false;
+    }
+}
+
+Shukujo.id = 'shukujo';
+
+module.exports = Shukujo;
