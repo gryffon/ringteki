366e6ef7f05138a4f401e27857299513bd2a3447
marc-soiferman <n/a>
2019-10-22 22:24:14 -0500

    Fixed leftovers from Graceful Guardian

--- server/game/cards/09.5-aCF/CourtMusician.js
+++ server/game/cards/09.5-aCF/CourtMusician.js
@@ -6,9 +6,9 @@ const { Durations, Players } = require('../../Constants');
 class CourtMusician extends DrawCard {
     setupCardAbilities() {
         this.action({
-            title: 'Increase cost to play cards',
+            title: 'Decrease cost to play cards',
             condition: context => context.source.isParticipating(),
-            effect: 'increase the cost of cards played by 1 for each player\'s next action opportunity',
+            effect: 'decrease the cost of cards played by 1 for each player\'s next action opportunity',
             gameAction: AbilityDsl.actions.playerLastingEffect(context => {
                 const currentActionWindow = context.game.currentActionWindow;
                 const opportunityCounter = currentActionWindow.opportunityCounter;
