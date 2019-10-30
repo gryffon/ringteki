8ae23e5b37d9cbadeeeb4517729ad55c4dd87c3f
marc-soiferman <n/a>
2019-10-29 23:42:07 -0500

    Implementation and tests for frontline engineer

--- server/game/cards/09.5-aCF/FrontlineEngineer.js
+++ server/game/cards/09.5-aCF/FrontlineEngineer.js
@@ -5,26 +5,31 @@ const AbilityDsl = require('../../abilitydsl.js');
 class FrontlineEngineer extends DrawCard {
     setupCardAbilities(ability) {
         this.persistentEffect({
-            effect: ability.effects.modifyGlory(this.getHoldingsInPlay())
+            effect: ability.effects.modifyGlory(card => this.getHoldingsInPlay(card))
         });
 
         this.action({
-            title: 'Search top 5 card for a holding',
-            gameAction: AbilityDsl.actions.cardMenu(context => ({
-                cards: context.player.dynastyDeck.first(5).filter(card => card.type === CardTypes.Holding),
-                choices: ['Select nothing'],
-                handlers: [() => this.game.addMessage('{0} selects nothing from their deck', context.player)],
-                gameAction: AbilityDsl.actions.moveCard(context => ({
-                    destination: this.game.currentConflict.conflictProvince.location,
-                    message: '{1} chooses to place {2} in {0} discarding {3}',
-                    messageArgs: (card, player, properties) => [this.game.currentConflict.conflictProvince.location, player, properties.target, player.getDynastyCardInProvince(card.location)]
-                }))
-            })),
-            effect: 'choose a holding to place in the attacked province'
+            title: 'Place a holding from your deck faceup in the defending province',
+            condition: context => context.player.dynastyDeck.size() > 0 && context.player.isDefendingPlayer(),
+            effect: 'look at the top five cards of their dynasty deck',
+            handler: context => this.game.promptWithHandlerMenu(context.player, {
+                activePromptTitle: 'Choose a holding',
+                context: context,
+                cardCondition: card => card.getType() === CardTypes.Holding,
+                cards: context.player.dynastyDeck.first(5),
+                cardHandler: cardFromDeck => {
+                    let card = context.player.getDynastyCardInProvince(this.game.currentConflict.conflictProvince.location)
+                    this.game.addMessage('{0} discards {1}, replacing it with {2}', context.player, card, cardFromDeck);
+                    context.player.moveCard(cardFromDeck, this.game.currentConflict.conflictProvince.location);
+                    cardFromDeck.facedown = false;
+                    context.player.moveCard(card, Locations.DynastyDiscardPile);
+                    context.player.shuffleDynastyDeck();
+                }
+            })
         });
     }
 
-    getHoldingsInPlay() {
+    getHoldingsInPlay(source) {
         return this.game.allCards.reduce((sum, card) => {
             if(!card.facedown && (card.isInProvince() && card.type === CardTypes.Holding)) {
                 return sum + 1;
---
+++ test/server/cards/09.5-aCF/FrontlineEngineer.spec.js
@@ -0,0 +1,124 @@
+describe('Frontline Engineer', function() {
+    integration(function() {
+        describe('Frontline Engineer\'s passive effect', function() {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['frontline-engineer'],
+                        dynastyDeck: ['imperial-storehouse', 'favorable-ground', 'kakita-dojo', 'mirumoto-dojo']
+                    },
+                    player2: {
+                        inPlay: [],
+                        dynastyDeck: ['imperial-storehouse', 'favorable-ground', 'kakita-dojo', 'mirumoto-dojo']
+                    }
+                });
+
+                this.frontlineEngineer = this.player1.findCardByName('frontline-engineer');
+
+                this.holding1 = this.player1.placeCardInProvince('imperial-storehouse', 'province 1');
+                this.holding2 = this.player1.placeCardInProvince('favorable-ground', 'province 2');
+                this.holding3 = this.player1.placeCardInProvince('kakita-dojo', 'province 3');
+                this.holding4 = this.player1.placeCardInProvince('mirumoto-dojo', 'province 4');
+                this.holding5 = this.player2.placeCardInProvince('imperial-storehouse', 'province 1');
+                this.holding6 = this.player2.placeCardInProvince('favorable-ground', 'province 2');
+                this.holding7 = this.player2.placeCardInProvince('kakita-dojo', 'province 3');
+                this.holding8 = this.player2.placeCardInProvince('mirumoto-dojo', 'province 4');
+
+                this.holding1.facedown = true;
+                this.holding2.facedown = true;
+                this.holding3.facedown = true;
+                this.holding4.facedown = true;
+                this.holding5.facedown = true;
+                this.holding6.facedown = true;
+                this.holding7.facedown = true;
+                this.holding8.facedown = true;
+                this.game.checkGameState(true);
+            });
+
+            it('should have base zero glory', function () {
+                expect(this.frontlineEngineer.glory).toBe(0);
+            });
+
+            it('should have glory equal to the number of faceup holdings in play', function () {
+                this.holding1.facedown = false;
+                this.holding2.facedown = false;
+                this.holding3.facedown = false;
+                this.holding4.facedown = false;
+                this.game.checkGameState(true);
+                expect(this.frontlineEngineer.glory).toBe(4);
+
+                this.holding5.facedown = false;
+                this.holding6.facedown = false;
+                this.holding7.facedown = false;
+                this.holding8.facedown = false;
+                this.game.checkGameState(true);
+                expect(this.frontlineEngineer.glory).toBe(8);
+            });
+        });
+
+        describe('Frontline Engineer\'s active effect', function() {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'conflict',
+                    player1: {
+                        inPlay: ['frontline-engineer'],
+                    },
+                    player2: {
+                        inPlay: ['frontline-engineer'],
+                        dynastyDiscard: ['wandering-ronin', 'doji-challenger', 'kitsu-spiritcaller', 'akodo-toturi', 'iron-mine', 'matsu-tsuko'],
+                        dynastyDeckSize: 4
+                    }
+                });
+
+                this.frontlineEngineerAttacker = this.player1.findCardByName('frontline-engineer');
+                this.frontlineEngineerDefender = this.player2.findCardByName('frontline-engineer');
+                this.matsuTsuko = this.player2.placeCardInProvince('matsu-tsuko', 'province 1');
+                this.player2.moveCard('wandering-ronin', 'dynasty deck');
+                this.player2.moveCard('doji-challenger', 'dynasty deck');
+                this.player2.moveCard('kitsu-spiritcaller', 'dynasty deck');
+                this.player2.moveCard('akodo-toturi', 'dynasty deck');
+                this.player2.moveCard('iron-mine', 'dynasty deck');
+
+                this.holding = this.player2.findCardByName('iron-mine');
+
+                this.noMoreActions();
+                this.initiateConflict({
+                    type: 'military',
+                    attackers: [this.frontlineEngineerAttacker],
+                    defenders: [this.frontlineEngineerDefender]
+                });
+            });
+
+            it('should not be usable on attack', function () {
+                this.player2.pass();
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+                this.player1.clickCard(this.frontlineEngineerAttacker);
+                expect(this.player1).toHavePrompt('Conflict Action Window');
+            });
+
+            it('should allow you to choose a holding in the top five cards of your deck', function () {
+                this.player2.clickCard(this.frontlineEngineerDefender);
+                expect(this.player2).toHavePrompt('Choose a holding');
+                expect(this.player2).toHaveDisabledPromptButton('Wandering Ronin');
+                expect(this.player2).toHaveDisabledPromptButton('Doji Challenger');
+                expect(this.player2).toHaveDisabledPromptButton('Kitsu Spiritcaller');
+                expect(this.player2).toHaveDisabledPromptButton('Akodo Toturi');
+                expect(this.player2).toHavePromptButton('Iron Mine');
+            });
+
+            it('should put the chosen holding face up in the defending province and discard the card there', function () {
+                this.player2.clickCard(this.frontlineEngineerDefender);
+                expect(this.player2).toHavePrompt('Choose a holding');
+                this.player2.clickPrompt('Iron Mine');
+                expect(this.holding.location).toBe('province 1');
+                expect(this.holding.facedown).toBe(false);
+                expect(this.matsuTsuko.location).toBe('dynasty discard pile');
+                expect(this.getChatLogs(5)).toContain('player2 uses Frontline Engineer to look at the top five cards of their dynasty deck');
+                expect(this.getChatLogs(4)).toContain('player2 discards Matsu Tsuko, replacing it with Iron Mine');
+                expect(this.getChatLogs(3)).toContain('player2 is shuffling their dynasty deck');
+            });
+        });
+    });
+});
+
