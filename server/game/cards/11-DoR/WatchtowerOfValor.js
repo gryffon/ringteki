a767c213df24aa9ce24a0ef3682ec93c10c3be82
marc-soiferman <n/a>
2019-11-15 19:05:08 -0600

    Tests and implementation for watchtower of valor

---
+++ server/game/cards/11-DoR/WatchtowerOfValor.js
@@ -0,0 +1,23 @@
+const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl');
+const { CardTypes } = require('../../Constants');
+
+class WatchtowerOfValor extends DrawCard {
+    setupCardAbilities() {
+        this.reaction({
+            title: 'Draw a card',
+            when: {
+                afterConflict: (event, context) => {
+                    let cards = context.player.getDynastyCardsInProvince(event.conflict.conflictProvince.location);
+                    return cards.some((card) => !card.facedown && card.type === CardTypes.Holding && card.hasTrait('kaiu-wall'))
+                            && context.player.isDefendingPlayer() && event.conflict.winner === context.player;
+                }
+            },
+            gameAction: AbilityDsl.actions.draw()
+        });
+    }
+}
+
+WatchtowerOfValor.id = 'watchtower-of-valor';
+
+module.exports = WatchtowerOfValor;
