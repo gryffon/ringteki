44a9ac2c78d29044041f449a2055f9c93014a8d7
Tom Nys <tom-nys@hotmail.com>
2019-11-20 17:39:34 +0100

    Add a test for disguised and attachments with attachmentconditions that are false.

--- test/server/cards/10-TEL/TacticalIngenuity.spec.js
+++ test/server/cards/10-TEL/TacticalIngenuity.spec.js
@@ -4,8 +4,8 @@ describe('Tactical Ingenuity', function() {
             this.setupTest({
                 phase: 'conflict',
                 player1: {
-                    inPlay: ['akodo-zentaro', 'matsu-berserker'],
-                    hand: ['tactical-ingenuity'],
+                    inPlay: ['akodo-zentaro', 'matsu-berserker', 'honored-general'],
+                    hand: ['tactical-ingenuity', 'shosuro-miyako-2', 'seal-of-the-lion'],
                     conflictDeck: ['way-of-the-lion', 'fine-katana', 'strength-in-numbers', 'censure'],
                     conflictDeckSize: 4
                 },
@@ -16,7 +16,10 @@ describe('Tactical Ingenuity', function() {
 
             this.zentaro = this.player1.findCardByName('akodo-zentaro');
             this.matsuBerseker = this.player1.findCardByName('matsu-berserker');
+            this.honoredGeneral = this.player1.findCardByName('honored-general');
             this.tacticalIngenuity = this.player1.findCardByName('tactical-ingenuity');
+            this.sealOfTheLion = this.player1.findCardByName('seal-of-the-lion');
+            this.shosuroMiyakoConflict = this.player1.findCardByName('shosuro-miyako-2');
             this.wayOfTheLion = this.player1.findCardByName('way-of-the-lion', 'conflict deck');
 
             this.giftedTactician = this.player2.findCardByName('gifted-tactician');
@@ -55,6 +58,34 @@ describe('Tactical Ingenuity', function() {
 
             this.player1.clickPrompt('Way of the Lion');
             expect(this.wayOfTheLion.location).toBe('hand');
+        });
+
+        it('should fall off when disguised over by a non-commander', function() {
+            this.player1.playAttachment(this.tacticalIngenuity, this.honoredGeneral);
+            this.player2.pass();
+
+            this.player1.clickCard(this.shosuroMiyakoConflict);
+            this.player1.clickCard(this.honoredGeneral);
+            this.player1.clickPrompt('Pass');
+
+            expect(this.tacticalIngenuity.parent).toBe(null);
+            expect(this.tacticalIngenuity.location).toBe('conflict discard pile');
+        });
+
+        it('should not fall off when disguised over by a non-commander carrying seal of the lion', function() {
+            this.player1.playAttachment(this.tacticalIngenuity, this.honoredGeneral);
+            this.player2.pass();
+            this.player1.playAttachment(this.sealOfTheLion, this.honoredGeneral);
+            this.player2.pass();
+
+            this.player1.clickCard(this.shosuroMiyakoConflict);
+            this.player1.clickCard(this.honoredGeneral);
+            this.player1.clickPrompt('Pass');
+
+            expect(this.sealOfTheLion.parent).toBe(this.shosuroMiyakoConflict);
+            expect(this.sealOfTheLion.location).toBe('play area');
+            expect(this.tacticalIngenuity.parent).toBe(this.shosuroMiyakoConflict);
+            expect(this.tacticalIngenuity.location).toBe('play area');
         });
     });
 });
