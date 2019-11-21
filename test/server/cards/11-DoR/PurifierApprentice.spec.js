a2da7b752479eaba104dfad6c2836c320085c188
marc-soiferman <n/a>
2019-11-20 22:18:57 -0600

    Fixed the tests

--- test/server/cards/11-DoR/PurifierApprentice.spec.js
+++ test/server/cards/11-DoR/PurifierApprentice.spec.js
@@ -1,6 +1,6 @@
-describe('Hida O-Ushi', function() {
+describe('Purifier Apprentice', function() {
     integration(function() {
-        describe('Hida O-Ushi\'s Reaction', function() {
+        describe('Purifier Apprentice\'s Reaction', function() {
             beforeEach(function () {
                 this.setupTest({
                     phase: 'conflict',
@@ -8,8 +8,7 @@ describe('Hida O-Ushi', function() {
                         inPlay: ['doji-kuwanan']
                     },
                     player2: {
-                        inPlay: ['hida-kisada', 'purifier-apprentice'],
-                        hand: ['captive-audience']
+                        inPlay: ['hida-kisada', 'purifier-apprentice']
                     }
                 });
 
@@ -33,6 +32,7 @@ describe('Hida O-Ushi', function() {
                 expect(this.player2).toBeAbleToSelect(this.purifier);
                 this.player2.clickCard(this.purifier);
                 expect(this.player1.honor).toBe(honor - 1);
+                expect(this.getChatLogs(1)).toContain('player2 uses Purifier Apprentice to make player1 lose 1 honor');
             });
 
             it('should not trigger if you win on attack', function () {
@@ -49,7 +49,7 @@ describe('Hida O-Ushi', function() {
                 this.player2.pass();
 
                 let honor = this.player1.honor;
-                expect(this.player2).toBeAbleToSelect(this.purifier);
+                expect(this.player2).not.toBeAbleToSelect(this.purifier);
                 this.player2.clickCard(this.purifier);
                 expect(this.player1.honor).toBe(honor);
             });
@@ -66,7 +66,7 @@ describe('Hida O-Ushi', function() {
                 this.player1.pass();
 
                 let honor = this.player1.honor;
-                expect(this.player2).toBeAbleToSelect(this.purifier);
+                expect(this.player2).not.toBeAbleToSelect(this.purifier);
                 this.player2.clickCard(this.purifier);
                 expect(this.player1.honor).toBe(honor);
             });
