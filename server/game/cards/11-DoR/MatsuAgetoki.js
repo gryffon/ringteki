3a7c5c8ce191d472151aa172c46298b568c01212
marc-soiferman <n/a>
2019-11-20 21:56:00 -0600

    Base implementation and tests

---
+++ server/game/cards/11-DoR/MatsuAgetoki.js
@@ -0,0 +1,33 @@
+const DrawCard = require('../../drawcard.js');
+const { Locations, CardTypes, EventNames } = require('../../Constants');
+
+class MatsuAgetoki extends DrawCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Move the conflict to another eligible province',
+            condition: context => context.player && context.player.opponent && context.player.honor > context.player.opponent.honor && context.source.isAttacking(),
+            effect: 'move the conflict to a different province',
+            handler: context => this.game.promptForSelect(context.player, {
+                context: context,
+                cardType: CardTypes.Province,
+                location: Locations.Provinces,
+                cardCondition: card => !card.isConflictProvince() && card.canBeAttacked(),
+                onSelect: (player, card) => {
+                    this.game.addMessage('{0} moves the conflict to {1}', player, card);
+                    card.inConflict = true;
+                    this.game.currentConflict.conflictProvince.inConflict = false;
+                    this.game.currentConflict.conflictProvince = card;
+                    if(card.facedown) {
+                        card.facedown = false;
+                        this.game.raiseEvent(EventNames.OnCardRevealed, { context: context, card: card });
+                    }
+                    return true;
+                }
+            })
+        });
+    }
+}
+
+MatsuAgetoki.id = 'matsu-agetoki';
+
+module.exports = MatsuAgetoki;
