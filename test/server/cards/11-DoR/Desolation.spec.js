b83abc980b5c86b3238e5f51c6620365d115b6b7
marc-soiferman <n/a>
2019-11-24 17:12:48 -0600

    Added a test to make sure it doesn't blank your own provinces

--- test/server/cards/11-DoR/Desolation.spec.js
+++ test/server/cards/11-DoR/Desolation.spec.js
@@ -5,6 +5,7 @@ describe('Desolation', function() {
                 this.setupTest({
                     phase: 'conflict',
                     player1: {
+                        provinces: ['magistrate-station'],
                         inPlay: ['brash-samurai'],
                         hand: ['desolation', 'fine-katana'],
                         honor: 10
@@ -12,14 +13,17 @@ describe('Desolation', function() {
                     player2: {
                         role: 'keeper-of-void',
                         inPlay: ['shinjo-outrider'],
-                        hand: ['talisman-of-the-sun'],
+                        hand: ['talisman-of-the-sun', 'mirumoto-s-fury'],
                         provinces: ['rally-to-the-cause', 'secret-cache', 'fertile-fields','public-forum']
                     }
                 });
+                this.magistrateStation = this.player1.findCardByName('magistrate-station');
                 this.brashSamurai = this.player1.findCardByName('brash-samurai');
                 this.desolation = this.player1.findCardByName('desolation');
+                this.fury = this.player2.findCardByName('mirumoto-s-fury');
                 this.player1.playAttachment('fine-katana', 'brash-samurai');
                 this.talismanOfTheSun = this.player2.playAttachment('talisman-of-the-sun', 'shinjo-outrider');
+                this.magistrateStation.facedown = false;
                 this.player1.clickCard(this.desolation);
                 this.noMoreActions();
             });
@@ -27,6 +31,26 @@ describe('Desolation', function() {
             it('should spend 2 honor to play', function() {
                 expect(this.getChatLogs(3)).toContain('player1 plays Desolation, losing 2 honor to blank player2\'s provinces until the end of the phase');
                 expect(this.player1.honor).toBe(8);
+            });
+
+            it('should not blank own provinces', function() {
+                this.initiateConflict({
+                    type: 'military',
+                    province: 'fertile-fields',
+                    attackers: [this.brashSamurai],
+                    defenders: []
+                });
+                this.player2.clickCard(this.fury);
+                this.player2.clickCard(this.brashSamurai);
+                expect(this.brashSamurai.bowed).toBe(true);
+                this.player1.clickCard(this.brashSamurai);
+                expect(this.brashSamurai.isHonored).toBe(true);
+                this.player2.pass();
+                this.player1.clickCard(this.magistrateStation);
+                expect(this.player1).toHavePrompt('Magistrate Station');
+                this.player1.clickCard(this.brashSamurai);
+                expect(this.brashSamurai.bowed).toBe(false);
+                expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
             it('should work on provinces with a reveal reaction', function() {
@@ -65,9 +89,9 @@ describe('Desolation', function() {
                     attackers: [this.brashSamurai],
                     defenders: []
                 });
-                expect(this.player2.player.hand.size()).toBe(0);
+                expect(this.player2.player.hand.size()).toBe(1);
                 this.player2.clickCard('fertile-fields');
-                expect(this.player2.player.hand.size()).toBe(0);
+                expect(this.player2.player.hand.size()).toBe(1);
                 expect(this.player2).toHavePrompt('Conflict Action Window');
             });
 
