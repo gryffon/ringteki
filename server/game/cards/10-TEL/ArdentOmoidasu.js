26955bf497e76d0014d38d382a26f0c869137ea7
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2019-11-24 01:20:59 +0100

    Fix Omoidasu trigger condition
    It didn't check who was dishonored, only  by who.

--- server/game/cards/10-TEL/ArdentOmoidasu.js
+++ server/game/cards/10-TEL/ArdentOmoidasu.js
@@ -10,7 +10,10 @@ class ArdentOmoidasu extends DrawCard {
                     const dishonoredByOpponentsEffect = (context.player.opponent === event.context.player);
                     const dishonoredByRingEffect = (event.context.source.type === 'ring');
                     const dishonoredByCardEffect = event.context.ability.isCardAbility();
-                    return dishonoredByOpponentsEffect && (dishonoredByRingEffect || dishonoredByCardEffect);
+                    const dishonoredCharacterBelongsToOmoidasuOwner = targetIsControlledByPlayer(event.context.target, context);
+                    return dishonoredCharacterBelongsToOmoidasuOwner &&
+                        dishonoredByOpponentsEffect &&
+                        (dishonoredByRingEffect || dishonoredByCardEffect);
                 }
             },
             gameAction: AbilityDsl.actions.takeHonor({
@@ -18,6 +21,13 @@ class ArdentOmoidasu extends DrawCard {
             })
         });
     }
+}
+
+function targetIsControlledByPlayer(target, context) {
+    if(Array.isArray(target))
+        return target.some(target => target.controller === context.player);
+
+    return target.controller === context.player;
 }
 
 ArdentOmoidasu.id = 'ardent-omoidasu';
