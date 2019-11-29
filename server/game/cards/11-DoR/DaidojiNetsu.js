5c4d377f0eb8012dbe6717a77708d3beaac4a959
Marc Soiferman <marc.soiferman@cerebrahealth.com>
2019-11-29 16:10:22 -0600

    Unit tests are written

---
+++ server/game/cards/11-DoR/DaidojiNetsu.js
@@ -0,0 +1,19 @@
+const DrawCard = require('../../drawcard.js');
+const { Players, CardTypes, Phases } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class DaidojiNetsu extends DrawCard {
+    setupCardAbilities(ability) {
+        this.persistentEffect({
+            condition: context => this.game.currentPhase === Phases.Conflict,
+            targetController: Players.Any,
+            match: (card, context) => card.getType() === CardTypes.Character && card !== context.source,
+            effect: AbilityDsl.effects.cardCannot('discardFromPlay')
+        });
+    }
+}
+
+DaidojiNetsu.id = 'daidoji-netsu';
+
+module.exports = DaidojiNetsu;
+
