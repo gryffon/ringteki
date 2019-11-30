gryffon/ringteki

commit 7696224035265be70eb1d839b7bb066a533ad8da

Bug fix for Total Warfare & Let Go. (#3709)

server/game/CardSelectors/BaseCardSelector.js
@@ -37,6 +37,9 @@ class BaseCardSelector {
             return context.game.allCards.toArray();
         }
         let attachments = context.player.cardsInPlay.reduce((array, card) => array.concat(card.attachments.toArray()), []);
+        let allProvinceAttachments = context.player.getProvinces().reduce((array, card) => array.concat(card.attachments.toArray()), []);
+        allProvinceAttachments = allProvinceAttachments.concat(context.player.opponent.getProvinces().reduce((array, card) => array.concat(card.attachments.toArray()), []));
+        attachments = attachments.concat(allProvinceAttachments);
 
         if(context.source.game.rings) {
             let rings = Object.values(context.source.game.rings);

test/server/cards/10-TEL/TotalWarfare.spec.js
@@ -9,6 +9,7 @@ describe('Total Warfare', function() {
                 },
                 player2: {
                     inPlay: ['samurai-of-integrity', 'akodo-toturi'],
+                    hand: ['let-go', 'calling-in-favors'],
                     provinces: ['ancestral-lands']
                 }
             });
@@ -21,6 +22,8 @@ describe('Total Warfare', function() {
             this.samuraiOfIntegrity = this.player2.findCardByName('samurai-of-integrity');
             this.akodoToturi = this.player2.findCardByName('akodo-toturi');
             this.ancestralLands = this.player2.findCardByName('ancestral-lands');
+            this.letGo = this.player2.findCardByName('let-go');
+            this.cif = this.player2.findCardByName('calling-in-favors');
         });
 
         it('should be able to played on a province', function() {
@@ -105,6 +108,26 @@ describe('Total Warfare', function() {
 
             expect(this.totalWarfare.parent).toBe(this.ancestralLands);
             expect(this.getChatLogs(2)).toContain('player1 plays Total Warfare, attaching it to ' + this.ancestralLands.location);
+        });
+
+        it('should be able to be discarded by let go', function() {
+            this.player1.playAttachment(this.totalWarfare, this.ancestralLands);
+            expect(this.totalWarfare.parent).toBe(this.ancestralLands);
+            this.player2.clickCard(this.letGo);
+            expect(this.player2).toBeAbleToSelect(this.totalWarfare);
+            this.player2.clickCard(this.totalWarfare);
+            expect(this.totalWarfare.location).toBe('conflict discard pile');
+        });
+
+        it('should be able to be discarded by calling in favors', function() {
+            this.player1.playAttachment(this.totalWarfare, this.ancestralLands);
+            expect(this.totalWarfare.parent).toBe(this.ancestralLands);
+            this.player2.clickCard(this.cif);
+            expect(this.player2).toBeAbleToSelect(this.totalWarfare);
+            this.player2.clickCard(this.totalWarfare);
+            expect(this.player2).toBeAbleToSelect(this.akodoToturi);
+            this.player2.clickCard(this.akodoToturi);
+            expect(this.totalWarfare.location).toBe('conflict discard pile');
         });
     });
 });

commit 6e07f9943ab4a683372e5836b1a4c6d2c5286ab8

This fixes a gamenode crash during the regroup stage if you don't have an opponent
Introduced in PR #3709

server/game/CardSelectors/BaseCardSelector.js
@@ -38,7 +38,8 @@ class BaseCardSelector {
         }
         let attachments = context.player.cardsInPlay.reduce((array, card) => array.concat(card.attachments.toArray()), []);
         let allProvinceAttachments = context.player.getProvinces().reduce((array, card) => array.concat(card.attachments.toArray()), []);
-        allProvinceAttachments = allProvinceAttachments.concat(context.player.opponent.getProvinces().reduce((array, card) => array.concat(card.attachments.toArray()), []));
+        if (context.player.opponent)
+            allProvinceAttachments = allProvinceAttachments.concat(context.player.opponent.getProvinces().reduce((array, card) => array.concat(card.attachments.toArray()), []));
         attachments = attachments.concat(allProvinceAttachments);
 
         if(context.source.game.rings) {