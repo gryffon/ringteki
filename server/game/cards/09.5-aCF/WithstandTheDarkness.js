897c7d26d9e0f3090a4cbe49aa7e3e6285d7932d
marc-soiferman <n/a>
2019-12-02 21:21:44 -0600

    Linting cleanup

--- server/game/cards/09.5-aCF/WithstandTheDarkness.js
+++ server/game/cards/09.5-aCF/WithstandTheDarkness.js
@@ -1,8 +1,7 @@
 const _ = require('underscore');
 const DrawCard = require('../../drawcard.js');
 const AbilityDsl = require('../../abilitydsl');
-const EventRegistrar = require('../../eventregistrar.js');
-const { Players, Locations, CardTypes, EventNames, AbilityTypes } = require('../../Constants');
+const { Players, Locations, CardTypes } = require('../../Constants');
 
 class WithstandTheDarkness extends DrawCard {
     setupCardAbilities() {
@@ -11,7 +10,7 @@ class WithstandTheDarkness extends DrawCard {
         this.reaction({
             when: {
                 onCardPlayed: (event, context) => {
-                    if (event.card.type === CardTypes.Event && event.card.controller === context.player.opponent) {
+                    if(event.card.type === CardTypes.Event && event.card.controller === context.player.opponent) {
                         currentTargets = this.getLegalWithstandTargets(event, context);
                         return currentTargets.length > 0;
                     }
@@ -24,45 +23,15 @@ class WithstandTheDarkness extends DrawCard {
                 cardCondition: card => currentTargets.includes(card),
                 gameAction: AbilityDsl.actions.placeFate()
             },
-            max: AbilityDsl.limit.perPhase(1),
+            max: AbilityDsl.limit.perPhase(1)
         });
-
-        // this.reaction({
-        //     when: {
-        //         onCardPlayed: (event, context) => {
-        //             if (event.context) {
-        //                 let temp = _.flatten(_.values(event.context.targets));
-        //                 temp = temp.concat(_.flatten(_.values(event.context.selects)));
-        //                 currentTargets = temp;
-        //             }
-        //             if (!Array.isArray(currentTargets))
-        //                 currentTargets = [currentTargets];
-        //             return (event.card.controller === context.player.opponent &&
-        //             event.card.type === CardTypes.Event &&
-        //             currentTargets.some(card => (
-        //                 card.type === CardTypes.Character &&
-        //                 card.isFaction('crab') &&
-        //                 card.controller === context.player &&
-        //                 card.location === Locations.PlayArea))
-        //             );
-        //         }},
-        //     title: 'Place a fate on a character',
-        //     target: {
-        //         activePromptTitle: 'Choose a character to receive a fate',
-        //         cardType: CardTypes.Character,
-        //         controller: Players.Self,
-        //         cardCondition: card => card.isFaction('crab') && currentTargets.includes(card),
-        //         gameAction: AbilityDsl.actions.placeFate()
-        //     },
-        //     max: AbilityDsl.limit.perPhase(1),
-        // });
     }
 
     getLegalWithstandTargets(event, context) {
-        if (event.context) {
+        if(event.context) {
             let targets = _.flatten(_.values(event.context.targets));
             targets = targets.concat(_.flatten(_.values(event.context.selects)));
-            if (!Array.isArray(targets)) {
+            if(!Array.isArray(targets)) {
                 targets = [targets];
             }
 
@@ -70,7 +39,7 @@ class WithstandTheDarkness extends DrawCard {
                 card.type === CardTypes.Character &&
                 card.isFaction('crab') &&
                 card.controller === context.player &&
-                card.location === Locations.PlayArea))
+                card.location === Locations.PlayArea));
             return targets;
         }
         return [];
