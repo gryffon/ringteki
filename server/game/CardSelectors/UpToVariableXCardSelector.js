3f3b5cdb3d092a3fc441a13d20685453236201bd
marc-soiferman <n/a>
2019-12-11 00:57:19 -0600

    Need to add the cannotTargetFirst flag and re-arrange everything

--- server/game/CardSelectors/UpToVariableXCardSelector.js
+++ server/game/CardSelectors/UpToVariableXCardSelector.js
@@ -7,7 +7,7 @@ class UpToVariableXCardSelector extends BaseCardSelector {
     }
 
     defaultActivePromptTitle(context) {
-        return this.numCardsFunc(context) === 1 ? 'Select a character' : `Select ${this.numCardsFunc(context)} characters`;
+        return this.numCardsFunc(context) === 1 ? 'Select up to one character' : `Select up to ${this.numCardsFunc(context)} characters`;
     }
 
     hasReachedLimit(selectedCards, context) {
--- server/game/cards/09.2-BoB/DishonorableAssault.js
+++ server/game/cards/09.2-BoB/DishonorableAssault.js
@@ -7,52 +7,33 @@ class DishonorableAssault extends ProvinceCard {
         this.action({
             title: 'Discard cards to dishonor attackers',
             effect: 'discard {1} and dishonor {2}',
-            effectArgs: context => [context.costs.multipleCardDiscardCost.map(a => a.name).join(', '), context.target],
-            cost: AbilityDsl.costs.multipleCardDiscardCost(context => context.target.length),
+            effectArgs: context => [context.costs.multipleCardDiscardCost.map(a => a.name).sort().join(', '), context.target],
+            cost: AbilityDsl.costs.multipleCardDiscardCost(context => {
+                if (context && context.target) {
+                    return context.target.length;
+                }
+                return 1;
+            }),
             target: {
                 mode: TargetModes.UpToVariable,
-                numCardsFunc: (context) => context.player.hand.size(),
+                numCardsFunc: (context) => {
+                    let hand  = 0;
+                    let paid = 0;
+                    if (context && context.player && context.player.hand) {
+                        hand = context.player.hand.size();
+                    }
+                    if (context && context.costs && context.costs.multipleCardDiscardCost) {
+                        paid = context.costs.multipleCardDiscardCost.length;
+                    }
+
+                    return hand + paid;
+                },
                 cardType: CardTypes.Character,
                 cardCondition: card => card.isAttacking(),
                 gameAction: AbilityDsl.actions.dishonor()
-            }
+            },
+            cannotTargetFirst: true
         });
-
-        // this.action({
-        //     title: 'Discard cards to dishonor attackers',
-        //     effect: 'discard {1}',
-        //     effectArgs: context => context.costs.variableCardDiscardCost.map(a => a.name).join(', '),
-        //     cost: AbilityDsl.costs.variableCardDiscardCost(context => this.getNumberOfLegalTargets(context)),
-        //     handler: context => {
-        //         this.game.promptForSelect(context.player, {
-        //             activePromptTitle: 'Choose characters to dishonor',
-        //             mode: TargetModes.Exactly,
-        //             numCards: context.costs.variableCardDiscardCost.length,
-        //             context: context,
-        //             cardCondition: card => card.type === CardTypes.Character && card.isAttacking(),
-        //             onSelect: (player, cards) => {
-        //                 this.game.addMessage('{0} uses {1} to dishonor {2}', player, context.source, cards);
-        //                 this.game.applyGameAction(context, { dishonor: cards });
-        //                 return true;
-        //             }
-        //         });
-        //     }
-        // });
-    }
-
-    getNumberOfLegalTargets(context) {
-        if (this.game.isDuringConflict()) {
-            let cards = context.game.currentConflict.getParticipants(card => card.isAttacking());
-            let count = 0;
-            cards.forEach(card => {
-                if (card.allowGameAction('dishonor', context)) {
-                    count++;
-                }
-            });
-
-            return count;
-        }
-        return 0;
     }
 }
 
