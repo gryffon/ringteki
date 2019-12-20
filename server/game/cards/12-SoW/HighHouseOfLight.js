82b6023acbfeb8119b478b0a4311b8856e5b3c17
Marc Soiferman <t>
2019-12-19 23:07:50 -0600

    Tests and implementation for High House of Light

---
+++ server/game/cards/12-SoW/HighHouseOfLight.js
@@ -0,0 +1,43 @@
+const StrongholdCard = require('../../strongholdcard.js');
+const { CardTypes, Players } = require('../../Constants');
+const AbilityDsl = require('../../abilitydsl.js');
+
+class HighHouseOfLight extends StrongholdCard {
+    setupCardAbilities() {
+        this.action({
+            title: 'Give a character with attachments bonus skill',
+            cost: AbilityDsl.costs.bowSelf(),
+            condition: () => this.game.isDuringConflict(),
+            target: {
+                cardType: CardTypes.Character,
+                controller: Players.Self,
+                cardCondition: card => card.isParticipating() && card.hasTrait('monk'),
+                gameAction: AbilityDsl.actions.cardLastingEffect(() => ({
+                    effect: AbilityDsl.effects.cardCannot({
+                        cannot: 'target',
+                        restricts: 'opponentsEvents'
+                    })
+                }))
+            },
+            then: context => ({
+                condition: context => this.game.currentConflict.getNumberOfCardsPlayed(context.player) >= 5,
+                gameAction: AbilityDsl.actions.selectRing({
+                    activePromptTitle: 'Choose a ring to take a fate from',
+                    ringCondition: ring => ring.fate >= 1,
+                    message: '{0} moves a fate from the {1} to {2}',
+                    messageArgs: ring => [context.player, ring, context.target],
+                    gameAction: AbilityDsl.actions.multiple([
+                        AbilityDsl.actions.takeFateFromRing({ removeOnly: true }),
+                        AbilityDsl.actions.placeFate({ target: context.target })
+                    ])
+                })
+            }),
+            effect: 'make {0} unable to be targeted by opponent\'s events',
+            effectArgs: context => [Math.min(context.target.attachments.size(), 2), 'military', 'political']
+        });
+    }
+}
+
+HighHouseOfLight.id = 'high-house-of-light';
+
+module.exports = HighHouseOfLight;
