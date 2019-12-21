fd625fb0b2a9ce53d3840746db0133224f925f22
Marc Soiferman <t>
2019-12-20 23:08:43 -0600

    Updated to AbilityDsl

--- server/game/cards/12-SoW/WildfireKick.js
+++ server/game/cards/12-SoW/WildfireKick.js
@@ -1,7 +1,8 @@
 const DrawCard = require('../../drawcard.js');
+const AbilityDsl = require('../../abilitydsl.js');
 
 class WildfireKick extends DrawCard {
-    setupCardAbilities(ability) {
+    setupCardAbilities() {
         this.action({
             title: 'Give opponent\'s characters -2/-2',
             condition: context =>
@@ -10,9 +11,9 @@ class WildfireKick extends DrawCard {
             target: {
                 cardCondition: card => card.isParticipating() && card.hasTrait('monk')
             },
-            gameAction: ability.actions.cardLastingEffect(context => ({
+            gameAction: AbilityDsl.actions.cardLastingEffect(context => ({
                 target: this.game.currentConflict.getCharacters(context.player.opponent).filter(card => card.getMilitarySkill() <= context.target.getMilitarySkill() && card !== context.source),
-                effect: ability.effects.modifyBothSkills(-2)
+                effect: AbilityDsl.effects.modifyBothSkills(-2)
             })),
             effect: 'give {1}\'s participating characters -2{2}/-2{3} if their military skill is equal to or lower than {4}. This affects: {5}',
             effectArgs: context => [context.player.opponent, 'military', 'political', context.target.getMilitarySkill(), context.game.currentConflict.getCharacters(context.player.opponent).filter(card => card.getMilitarySkill() <= context.target.getMilitarySkill() && card !== context.source)]
