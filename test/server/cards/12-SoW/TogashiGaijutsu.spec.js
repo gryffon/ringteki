dfb88445d3432de7b1a24982e0fd3d896a733a61
Marc Soiferman <t>
2019-12-28 16:37:49 -0600

    Added a test case for non-tattoos

--- test/server/cards/12-SoW/TogashiGaijutsu.spec.js
+++ test/server/cards/12-SoW/TogashiGaijutsu.spec.js
@@ -5,7 +5,7 @@ describe('Togashi Gaijutsu', function() {
                 phase: 'conflict',
                 player1: {
                     inPlay: ['togashi-gaijutsu', 'togashi-mitsu'],
-                    hand: ['centipede-tattoo']
+                    hand: ['centipede-tattoo', 'fine-katana']
                 },
                 player2: {
                     inPlay: ['togashi-kazue'],
@@ -16,6 +16,7 @@ describe('Togashi Gaijutsu', function() {
             this.gaijutsu = this.player1.findCardByName('togashi-gaijutsu');
             this.mitsu = this.player1.findCardByName('togashi-mitsu');
             this.p1Tattoo = this.player1.findCardByName('centipede-tattoo');
+            this.katana = this.player1.findCardByName('fine-katana');
 
             this.kazue = this.player2.findCardByName('togashi-kazue');
             this.p2Tattoo = this.player2.findCardByName('centipede-tattoo');
@@ -55,6 +56,13 @@ describe('Togashi Gaijutsu', function() {
             this.player2.clickCard(this.mitsu);
             expect(this.player1).not.toHavePrompt('Triggered Abilities');
             expect(this.player1).toHavePrompt('Action Window');
+        });
+
+        it('should not react from non-tattoos', function() {
+            this.player1.clickCard(this.katana);
+            this.player1.clickCard(this.mitsu);
+            expect(this.player1).not.toHavePrompt('Triggered Abilities');
+            expect(this.player2).toHavePrompt('Action Window');
         });
     });
 });
