24891f66bfdca92a4c6976a79589282ba6e0cd05
Tom_Nys(the-oucast) <tom-nys@hotmail.com>
2019-12-31 21:39:27 +0100

    Apply new RRG changes around discard pile order
    Has functional impact on plagerist

--- server/game/cards/02.6-MotE/IllustriousPlagiarist.js
+++ server/game/cards/02.6-MotE/IllustriousPlagiarist.js
@@ -1,5 +1,5 @@
 const DrawCard = require('../../drawcard.js');
-const { Locations, Durations, Players, CardTypes, AbilityTypes } = require('../../Constants');
+const { Locations, Durations, Players, AbilityTypes } = require('../../Constants');
 
 class IllustriousPlagiarist extends DrawCard {
     setupCardAbilities(ability) {
@@ -7,10 +7,12 @@ class IllustriousPlagiarist extends DrawCard {
             title: 'Copy action abilty of opponent\'s top event',
             condition: context => context.player.opponent,
             target: {
+                player: Players.Opponent, // As per December 2019 RRG Conflict Discard Pile order is determined by the controller of the pile
                 location: Locations.ConflictDiscardPile,
                 controller: Players.Opponent,
-                cardCondition: (card, context) => card === context.player.opponent.conflictDiscardPile.find(card => card.type === CardTypes.Event) &&
-                                                  card.abilities.actions.length > 0,
+                cardCondition: (card, context) => card.location === Locations.ConflictDiscardPile &&
+                    card.controller === context.player.opponent &&
+                    card.abilities.actions.length > 0,
                 gameAction: ability.actions.cardLastingEffect(context => ({
                     duration: Durations.UntilEndOfPhase,
                     target: context.source,
