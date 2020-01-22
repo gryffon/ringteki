0fd811666923120164aa54085e727c62f468066d
Marc Soiferman <t>
2020-01-21 21:16:24 -0600

    Checked it with a "max 1 per conflict" event.  Time to try defend your honor

--- test/server/cards/12-SoW/DragonTattoo.spec.js
+++ test/server/cards/12-SoW/DragonTattoo.spec.js
@@ -5,7 +5,7 @@ describe('Dragon Tattoo', function() {
                 phase: 'conflict',
                 player1: {
                     inPlay: ['togashi-kazue', 'doji-challenger'],
-                    hand: ['way-of-the-scorpion', 'way-of-the-crane', 'dragon-tattoo', 'dragon-tattoo', 'mantra-of-fire'],
+                    hand: ['way-of-the-scorpion', 'way-of-the-crane', 'dragon-tattoo', 'dragon-tattoo', 'mantra-of-fire', 'rising-stars-kata'],
                     conflictDiscard: ['defend-your-honor']
                 },
                 player2: {
@@ -21,6 +21,7 @@ describe('Dragon Tattoo', function() {
             this.dragonTattoo1 = this.player1.filterCardsByName('dragon-tattoo')[0];
             this.dragonTattoo2 = this.player1.filterCardsByName('dragon-tattoo')[1];
             this.mantraOfFire = this.player1.findCardByName('mantra-of-fire');
+            this.kata = this.player1.findCardByName('rising-stars-kata');
 
             this.kuwanan = this.player2.findCardByName('doji-kuwanan');
             this.scorpion2 = this.player2.findCardByName('way-of-the-scorpion');
@@ -154,7 +155,7 @@ describe('Dragon Tattoo', function() {
             expect(this.getChatLogs(2)).toContain('Mantra of Fire is removed from the game by Dragon Tattoo\'s effect');
         });
 
-        it('should react after you target your own character with an event to remove from game even if the copy has no legal targets', function() {
+        it('should just remove an event from the game if the copy has no legal targets', function() {
             this.noMoreActions();
             this.initiateConflict({
                 attackers: [this.kazue, this.challenger],
@@ -181,6 +182,32 @@ describe('Dragon Tattoo', function() {
 
             expect(this.getChatLogs(4)).toContain('player1 plays Way of the Crane to honor Doji Challenger');
             expect(this.getChatLogs(3)).toContain('player1 uses Dragon Tattoo to remove Way of the Crane from the game');
+        });
+
+        it('should just remove a "max 1 per conflict" event from the game rather than resolve it again', function() {
+            this.noMoreActions();
+            this.initiateConflict({
+                attackers: [this.kazue, this.challenger],
+                defenders: [this.kuwanan],
+                type: 'military'
+            });
+
+            this.player2.pass();
+            this.player1.clickCard(this.kata);
+            expect(this.player1).toBeAbleToSelect(this.kazue);
+            expect(this.player1).not.toBeAbleToSelect(this.challenger);
+            expect(this.player1).toBeAbleToSelect(this.kuwanan);
+
+            this.player1.clickCard(this.kazue);
+            expect(this.player1).toHavePrompt('Triggered Abilities');
+            expect(this.player1).toBeAbleToSelect(this.dragonTattoo1);
+            this.player1.clickCard(this.dragonTattoo1);
+            expect(this.player2).toHavePrompt('Conflict Action Window');
+
+            expect(this.kata.location).toBe('removed from game');
+
+            expect(this.getChatLogs(4)).toContain('player1 plays Rising Stars Kata to give Togashi Kazue +3 military skill until the end of the conflict');
+            expect(this.getChatLogs(3)).toContain('player1 uses Dragon Tattoo to remove Rising Stars Kata from the game');
         });
     });
 });
